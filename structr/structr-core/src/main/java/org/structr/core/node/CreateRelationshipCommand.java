76af1c5ffa36c79cccfe2e5c25bb9cf38f9ac1fa
amorgner <am@structr.org>
2011-03-25 17:39:26 +0100

    Added switch for duplicates check

--- structr/structr-core/src/main/java/org/structr/core/node/CreateRelationshipCommand.java
+++ structr/structr-core/src/main/java/org/structr/core/node/CreateRelationshipCommand.java
@@ -4,6 +4,9 @@
  */
 package org.structr.core.node;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.neo4j.graphdb.DynamicRelationshipType;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
@@ -26,6 +29,8 @@ import org.structr.core.entity.StructrRelationship;
  * @author cmorgner
  */
 public class CreateRelationshipCommand extends NodeServiceCommand {
+
+    private static final Logger logger = Logger.getLogger(CreateRelationshipCommand.class.getName());
 
     @Override
     public Object execute(Object... parameters) {
@@ -61,8 +66,57 @@ public class CreateRelationshipCommand extends NodeServiceCommand {
             } else {
                 throw new UnsupportedArgumentError("Wrong argument type(s).");
             }
+
+        } else if (graphDb != null && parameters.length == 4) {
+
+            Object arg0 = parameters[0]; // start node
+            Object arg1 = parameters[1]; // end node
+            Object arg2 = parameters[2]; // relationship type
+            Object arg3 = parameters[3]; // check duplicates
+
+            RelationshipType relType = null;
+            if (arg2 instanceof String) {
+                relType = DynamicRelationshipType.withName((String) arg2);
+            } else if (arg2 instanceof RelationshipType) {
+                relType = (RelationshipType) arg2;
+            } else {
+                throw new UnsupportedArgumentError("Wrong argument type(s).");
+            }
+
+            boolean checkDuplicates = false;
+            if (arg3 instanceof Boolean) {
+                checkDuplicates = ((Boolean) arg3) == true;
+            }
+
+            if (arg0 instanceof AbstractNode && arg1 instanceof AbstractNode) {
+
+                AbstractNode startNode = (AbstractNode) arg0;
+                AbstractNode endNode = (AbstractNode) arg1;
+
+                if (checkDuplicates) {
+
+                    List<StructrRelationship> incomingRels = endNode.getIncomingLinkRelationships();
+
+                    for (StructrRelationship rel : incomingRels) {
+                        if (rel.getRelType().equals(relType) && rel.getStartNode().equals(startNode)) {
+                            logger.log(Level.WARNING, "Creation of duplicate relationship was blocked");
+                            return null;
+                        }
+                    }
+
+                }
+
+                Node node1 = graphDb.getNodeById(startNode.getId());
+                Node node2 = graphDb.getNodeById(endNode.getId());
+
+                return new StructrRelationship(node1.createRelationshipTo(node2, relType));
+
+            } else {
+                throw new UnsupportedArgumentError("Wrong argument type(s).");
+            }
         }
 
-        return (null);
+
+        return null;
     }
 }
