762789723fecd88c3388d097222f804c6a7d307f
amorgner <axel@morgner.de>
2011-05-09 09:39:20 +0200

    Example modified

--- structr/structr-core/importShapefiles.sh
+++ structr/structr-core/importShapefiles.sh
@@ -8,11 +8,7 @@
 #-layer world_regions" \
 #-Dexec.executable=/usr/lib/jvm/java-6-openjdk/bin/java -Dnetbeans.execution=true process-classes org.codehaus.mojo:exec-maven-plugin:1.1.1:exec
 
-mvn -Dexec.classpathScope=runtime \
--Dexec.args="-server -Xms64m -Xmx1024m -classpath %classpath \
-org.structr.tools.GeoDataTool \
--dbPath /opt/structr/t5s/db \
--file /tmp/TM_WORLD_BORDERS-0.3-polygons-ms1.shp \
--importShapefile \
--layer t5s_geodata_MapShaper3" \
--Dexec.executable=/usr/lib/jvm/java-6-openjdk/bin/java -Dnetbeans.execution=true process-classes org.codehaus.mojo:exec-maven-plugin:1.1.1:exec
+mvn -Dexec.classpathScope=runtime -Dexec.args="-server -Xms64m -Xmx1024m -classpath %classpath org.structr.tools.GeoDataTool \
+-dbPath /opt/structr/t5s/db -file /home/axel/MERGED/WORLD_REGIONS.shp \
+-importShapefile -layer world_regions_final" -Dexec.executable=/usr/lib/jvm/java-6-openjdk/bin/java -Dnetbeans.execution=true process-classes org.codehaus.mojo:exec-maven-plugin:1.1.1:exec
+
