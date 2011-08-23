55d299889593ca572d1a9dc21e10eae73f4d5acb
Axel Morgner <axel@morgner.de>
2011-08-23 09:32:41 +0200

    Missing class dependency added

--- structr/structr-core/src/main/java/org/structr/core/entity/AbstractNode.java
+++ structr/structr-core/src/main/java/org/structr/core/entity/AbstractNode.java
@@ -100,6 +100,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.structr.common.AbstractComponent;
 import org.structr.common.AccessControllable;
+import org.structr.core.node.DeleteRelationshipCommand;
 
 //~--- classes ----------------------------------------------------------------
 
