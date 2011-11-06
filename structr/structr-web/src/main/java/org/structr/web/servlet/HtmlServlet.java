bfd4a142e2123d60f00ecca5257487e3d6aa27f5
Axel Morgner <axel@morgner.de>
2011-11-06 13:55:43 +0100

    NP check added

--- structr/structr-web/src/main/java/org/structr/web/servlet/HtmlServlet.java
+++ structr/structr-web/src/main/java/org/structr/web/servlet/HtmlServlet.java
@@ -1,40 +1,26 @@
 /*
  *  Copyright (C) 2011 Axel Morgner
- * 
+ *
  *  This file is part of structr <http://structr.org>.
- * 
+ *
  *  structr is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  structr is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with structr.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+
+
 package org.structr.web.servlet;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Path;
@@ -45,6 +31,7 @@ import org.neo4j.graphdb.traversal.Evaluator;
 import org.neo4j.graphdb.traversal.TraversalDescription;
 import org.neo4j.kernel.Traversal;
 import org.neo4j.kernel.Uniqueness;
+
 import org.structr.common.SecurityContext;
 import org.structr.core.Command;
 import org.structr.core.Predicate;
@@ -62,6 +49,29 @@ import org.structr.web.common.RelType;
 import org.structr.web.entity.Content;
 import org.structr.web.entity.Resource;
 
+//~--- JDK imports ------------------------------------------------------------
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//~--- classes ----------------------------------------------------------------
+
 /**
  * A first proof of concept for the new graph concept. This class has two modes,
  * one to create an example structure and one to traverse over the created
@@ -75,7 +85,11 @@ public class HtmlServlet extends HttpServlet {
 
 	private static final Logger logger = Logger.getLogger(HtmlServlet.class.getName());
 
+	//~--- fields ---------------------------------------------------------
+
 	private TraversalDescription desc = null;
+
+	//~--- methods --------------------------------------------------------
 
 	@Override
 	public void init() {
@@ -87,92 +101,59 @@ public class HtmlServlet extends HttpServlet {
 	}
 
 	@Override
-	public void destroy() {
-	}
+	public void destroy() {}
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 
-		if(request.getParameter("create") != null) {
+		if (request.getParameter("create") != null) {
 
 			createTestStructure();
-
 			response.setStatus(HttpServletResponse.SC_CREATED);
+
 			return;
 		}
 
 		try {
+
 			request.setCharacterEncoding("UTF-8");
 
 			DecimalFormat decimalFormat = new DecimalFormat("0.000000000", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
-			double start = System.nanoTime();
+			double start                = System.nanoTime();
+			String idString             = request.getParameter("id");
 
-			// 1: find entry point (Resource)
-			long id = Long.parseLong(request.getParameter("id"));
-			Resource resource = (Resource)Services.command(SecurityContext.getSuperUserInstance(), FindNodeCommand.class).execute(id);
+			if (idString != null) {
 
-			// 2: do a traversal and collect content
-			String content = getContent(resource);
+				// 1: find entry point (Resource)
+				long id           = Long.parseLong(idString);
+				Resource resource = (Resource) Services.command(SecurityContext.getSuperUserInstance(), FindNodeCommand.class).execute(id);
 
-			double end = System.nanoTime();
-			logger.log(Level.INFO, "Content collected in {0} seconds", decimalFormat.format((end - start) / 1000000000.0));
+				if (resource == null) {
 
-			// 3: output content
-			response.getWriter().append(content);
+					response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+					return;
+				}
+
+				// 2: do a traversal and collect content
+				String content = getContent(resource);
+				double end     = System.nanoTime();
+
+				logger.log(Level.INFO, "Content collected in {0} seconds", decimalFormat.format((end - start) / 1000000000.0));
+
+				// 3: output content
+				response.getWriter().append(content);
+			}
+
 			response.getWriter().flush();
 			response.getWriter().close();
-
 			response.setStatus(HttpServletResponse.SC_OK);
 
-
-		} catch(Throwable t) {
+		} catch (Throwable t) {
 
 			logger.log(Level.WARNING, "Exception while processing request", t);
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 		}
-	}
-
-	private String getContent(Resource resource) {
-
-		final StringBuilder builder = new StringBuilder();
-
-		TraversalDescription localDesc = desc.expand(new ResourceExpander(resource.getIdString()));
-		localDesc = localDesc.evaluator(new Evaluator() {
-
-				@Override
-				public Evaluation evaluate(Path path) {
-
-					Node node = path.endNode();
-
-					try {
-
-						String type = (String)node.getProperty(AbstractNode.Key.type.name());
-
-						if("Content".equals(type)) {
-
-							// Content node reached, collect content and stop traversal here
-							builder.append(node.getProperty(Content.Key.content.name()));
-							return Evaluation.EXCLUDE_AND_PRUNE;
-
-						} else {
-
-							// continue traversal
-							return Evaluation.EXCLUDE_AND_CONTINUE;
-						}
-
-
-					} catch(Throwable t) {
-						// fail fast, no check
-					}
-
-					return Evaluation.EXCLUDE_AND_PRUNE;
-				}
-			}
-		);
-
-		// do traversal to retrieve paths
-		for(Path path : localDesc.traverse(resource.getNode())) {}
-
-		return builder.toString();
 	}
 
 	private void createTestStructure() {
@@ -187,22 +168,20 @@ public class HtmlServlet extends HttpServlet {
 				AbstractNode page1 = createNode("Resource", "page1");
 				AbstractNode page2 = createNode("Resource", "page2");
 
-				logger.log(Level.INFO, "Created page1 with id {0}, page2 with id {1}", new Object[] { page1.getId(), page2.getId() } );
+				logger.log(Level.INFO, "Created page1 with id {0}, page2 with id {1}", new Object[] { page1.getId(), page2.getId() });
 
-				AbstractNode doc = createNode("Element", "doc");
-				AbstractNode body = createNode("Element", "body");
-				AbstractNode article1 = createNode("Element", "article1");
-				AbstractNode article2 = createNode("Element", "article2");
-
-				AbstractNode header = createNode("Content", "header", new NodeAttribute("content", "<html>"));
+				AbstractNode doc       = createNode("Element", "doc");
+				AbstractNode body      = createNode("Element", "body");
+				AbstractNode article1  = createNode("Element", "article1");
+				AbstractNode article2  = createNode("Element", "article2");
+				AbstractNode header    = createNode("Content", "header", new NodeAttribute("content", "<html>"));
 				AbstractNode startBody = createNode("Content", "start_body", new NodeAttribute("content", "<body>"));
-				AbstractNode endBody = createNode("Content", "end_body", new NodeAttribute("content", "</body>"));
-				AbstractNode footer = createNode("Content", "footer", new NodeAttribute("content", "</html>"));
-				AbstractNode foo = createNode("Content", "content1", new NodeAttribute("content", "<h1>Dies ist Seite 1</h1>"));
-				AbstractNode bar = createNode("Content", "content2", new NodeAttribute("content", "<h1>Dies ist Seite 2</h1>"));
-
-				String idOfPage1 = page1.getIdString();
-				String idOfPage2 = page2.getIdString();
+				AbstractNode endBody   = createNode("Content", "end_body", new NodeAttribute("content", "</body>"));
+				AbstractNode footer    = createNode("Content", "footer", new NodeAttribute("content", "</html>"));
+				AbstractNode foo       = createNode("Content", "content1", new NodeAttribute("content", "<h1>Dies ist Seite 1</h1>"));
+				AbstractNode bar       = createNode("Content", "content2", new NodeAttribute("content", "<h1>Dies ist Seite 2</h1>"));
+				String idOfPage1       = page1.getIdString();
+				String idOfPage2       = page2.getIdString();
 
 				// page 1
 				linkNodes(page1, doc, idOfPage1, 0);
@@ -226,47 +205,97 @@ public class HtmlServlet extends HttpServlet {
 
 				return null;
 			}
+
 		});
 	}
 
 	private AbstractNode createNode(String type, String name, NodeAttribute... attributes) {
 
-		SecurityContext context = SecurityContext.getSuperUserInstance();
+		SecurityContext context   = SecurityContext.getSuperUserInstance();
 		Command createNodeCommand = Services.command(context, CreateNodeCommand.class);
-
 		Map<String, Object> attrs = new HashMap<String, Object>();
+
 		attrs.put(AbstractNode.Key.type.name(), type);
 		attrs.put(AbstractNode.Key.name.name(), name);
-		for(NodeAttribute attr : attributes) {
+
+		for (NodeAttribute attr : attributes) {
 			attrs.put(attr.getKey(), attr.getValue());
 		}
 
-		return (AbstractNode)createNodeCommand.execute(attrs);
+		return (AbstractNode) createNodeCommand.execute(attrs);
 	}
 
 	private StructrRelationship linkNodes(AbstractNode startNode, AbstractNode endNode, String resourceId, int index) {
 
-		SecurityContext context = SecurityContext.getSuperUserInstance();
+		SecurityContext context  = SecurityContext.getSuperUserInstance();
 		Command createRelCommand = Services.command(context, CreateRelationshipCommand.class);
+		StructrRelationship rel  = (StructrRelationship) createRelCommand.execute(startNode, endNode, RelType.CONTAINS);
 
-		StructrRelationship rel = (StructrRelationship)createRelCommand.execute(startNode, endNode, RelType.CONTAINS);
 		rel.setProperty(resourceId, index);
 
 		return rel;
 	}
 
+	//~--- get methods ----------------------------------------------------
+
+	private String getContent(Resource resource) {
+
+		final StringBuilder builder    = new StringBuilder();
+		TraversalDescription localDesc = desc.expand(new ResourceExpander(resource.getIdString()));
+
+		localDesc = localDesc.evaluator(new Evaluator() {
+
+			@Override
+			public Evaluation evaluate(Path path) {
+
+				Node node = path.endNode();
+
+				try {
+
+					String type = (String) node.getProperty(AbstractNode.Key.type.name());
+
+					if ("Content".equals(type)) {
+
+						// Content node reached, collect content and stop traversal here
+						builder.append(node.getProperty(Content.Key.content.name()));
+
+						return Evaluation.EXCLUDE_AND_PRUNE;
+					} else {
+
+						// continue traversal
+						return Evaluation.EXCLUDE_AND_CONTINUE;
+					}
+
+				} catch (Throwable t) {
+
+					// fail fast, no check
+				}
+
+				return Evaluation.EXCLUDE_AND_PRUNE;
+			}
+
+		});
+
+		// do traversal to retrieve paths
+		for (Path path : localDesc.traverse(resource.getNode())) {}
+
+		return builder.toString();
+	}
+
+	//~--- inner classes --------------------------------------------------
+
 	private static class ResourceExpander implements RelationshipExpander {
 
 		private Direction direction = Direction.OUTGOING;
-		private String resourceId = null;
+		private String resourceId   = null;
+
+		//~--- constructors -------------------------------------------
 
 		public ResourceExpander(final String resourceId) {
 			this.resourceId = resourceId;
 		}
 
-		public void setDirection(Direction direction) {
-			this.direction = direction;
-		}
+		//~--- methods ------------------------------------------------
 
 		@Override
 		public Iterable<Relationship> expand(Node node) {
@@ -277,14 +306,17 @@ public class HtmlServlet extends HttpServlet {
 			 * sorting for us and return sorted values from map.
 			 */
 			Map<Integer, Relationship> sortedRelationshipMap = new TreeMap<Integer, Relationship>();
-			for(Relationship rel : node.getRelationships(RelType.CONTAINS, direction)) {
+
+			for (Relationship rel : node.getRelationships(RelType.CONTAINS, direction)) {
 
 				try {
 
-					Integer position = (Integer)rel.getProperty(resourceId);
+					Integer position = (Integer) rel.getProperty(resourceId);
+
 					sortedRelationshipMap.put(position, rel);
 
-				} catch(Throwable t) {
+				} catch (Throwable t) {
+
 					// fail fast, no check
 				}
 			}
@@ -294,11 +326,18 @@ public class HtmlServlet extends HttpServlet {
 
 		@Override
 		public RelationshipExpander reversed() {
-			
+
 			ResourceExpander reversed = new ResourceExpander(resourceId);
+
 			reversed.setDirection(Direction.INCOMING);
 
 			return reversed;
+		}
+
+		//~--- set methods --------------------------------------------
+
+		public void setDirection(Direction direction) {
+			this.direction = direction;
 		}
 	}
 }
