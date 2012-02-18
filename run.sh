5444f9a2710be572b893378cd83296c522551c10
Axel Morgner <axel@morgner.de>
2012-02-18 01:28:45 +0100

    added -U switch to ensure that dependencies are always updated

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 mkdir /tmp/structr-ui
 rm -rf /tmp/structr-ui/*
 cd structr/structr-ui
-mvn clean package
+mvn -U clean package
 cp target/structr-ui.war /tmp/structr-ui
 cd /tmp/structr-ui
 java -jar structr-ui.war
