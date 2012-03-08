84407bf260b2e47113dc328d6ff267b272b0b075
amorgner <axel@morgner.de>
2012-03-08 19:25:31 +0100

    work in progress on geocoding

---
+++ structr/structr-core/src/main/java/org/structr/core/converter/DoubleConverter.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.structr.core.converter;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.lang.math.NumberUtils;
+import org.structr.core.PropertyConverter;
+import org.structr.core.Value;
+
+/**
+ *
+ * @author Axel Morgner
+ */
+public class DoubleConverter extends PropertyConverter {
+
+	private static final Logger logger = Logger.getLogger(DoubleConverter.class.getName());
+
+	@Override
+	public Object convertForSetter(Object source, Value value) {
+
+		if(source != null) {
+			
+			try {
+
+				if(source instanceof Double) {
+					return ((Double)source);
+				} else if(source instanceof String) {
+					return NumberUtils.createDouble((String) source);
+				}
+
+			} catch (Throwable t) {
+
+				logger.log(Level.WARNING, "Exception while parsing double", t);
+				return null;
+			}
+		}
+
+		return source;
+
+	}
+
+	@Override
+	public Object convertForGetter(Object source, Value value) {
+
+		if(source != null) {
+			return source.toString();
+		}
+
+		return source;
+	}
+}
---
+++ structr/structr-core/src/main/java/org/structr/core/entity/Location.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.structr.core.entity;
+
+import org.structr.common.error.ErrorBuffer;
+import org.structr.common.PropertyKey;
+import org.structr.common.PropertyView;
+import org.structr.common.ValidationHelper;
+import org.structr.core.EntityContext;
+import org.structr.core.converter.DoubleConverter;
+import org.structr.core.node.NodeService.NodeIndex;
+
+/**
+ * The Location entity.
+ * 
+ * @author Axel Morgner
+ */
+public class Location extends AbstractNode {
+
+
+	public enum Key implements PropertyKey {
+		latitude, longitude, altitude;
+	}
+
+	static {
+
+
+		// ----- initialize property sets -----
+		EntityContext.registerPropertySet(Location.class, PropertyView.All,		Key.values());
+
+		// ----- initialize property converters -----
+		EntityContext.registerPropertyConverter(Location.class, Location.Key.latitude,	DoubleConverter.class);
+		EntityContext.registerPropertyConverter(Location.class, Location.Key.longitude,	DoubleConverter.class);
+		EntityContext.registerPropertyConverter(Location.class, Location.Key.altitude,	DoubleConverter.class);
+		
+		// ----- initialize validators -----
+
+		// ----- initialize searchable properties
+		EntityContext.registerSearchablePropertySet(Location.class, NodeIndex.fulltext.name(), Key.values());
+		EntityContext.registerSearchablePropertySet(Location.class, NodeIndex.keyword.name(), Key.values());
+		
+	}
+
+	@Override
+	public boolean isValid(ErrorBuffer errorBuffer) {
+
+		boolean error = false;
+
+		error |= ValidationHelper.checkPropertyNotNull(this, Key.latitude, errorBuffer);
+		error |= ValidationHelper.checkPropertyNotNull(this, Key.longitude, errorBuffer);
+
+		return !error;
+	}
+	
+	@Override
+	public String getIconSrc() {
+		return null;
+	}
+}
