4b39324c933dda7896cdb90251e93318aa6df43c
Christian Morgner <christian@morgner.de>
2012-04-18 23:46:59 +0200

    Added nestable function calls in property replacement method.

--- structr/structr-ui/src/main/java/org/structr/web/entity/html/HtmlElement.java
+++ structr/structr-ui/src/main/java/org/structr/web/entity/html/HtmlElement.java
@@ -21,8 +21,6 @@
 
 package org.structr.web.entity.html;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.LinkedHashMap;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang.StringUtils;
@@ -43,6 +41,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.neo4j.graphdb.Direction;
+import org.structr.common.RelType;
+import org.structr.common.error.FrameworkException;
+import org.structr.core.Adapter;
+import org.structr.core.entity.AbstractRelationship;
 
 //~--- classes ----------------------------------------------------------------
 
@@ -66,17 +69,44 @@ public abstract class HtmlElement extends Element {
 	};
 	private static final Logger logger           = Logger.getLogger(HtmlElement.class.getName());
 	private static final Pattern templatePattern = Pattern.compile("\\$\\{[^}]*\\}");
+	private static final Pattern functionPattern = Pattern.compile("([a-zA-Z0-9]+)\\((.+)\\)");
 
-	private static final java.util.Map<String, Method> functions = new LinkedHashMap<String, Method>();
+	private static final java.util.Map<String, Adapter<String, String>> functions = new LinkedHashMap<String, Adapter<String, String>>();
 
 	static {
-		try {
-			functions.put("md5", HtmlElement.class.getMethod("md5", String.class));
-		} catch (NoSuchMethodException ex) {
-			Logger.getLogger(HtmlElement.class.getName()).log(Level.SEVERE, null, ex);
-		} catch (SecurityException ex) {
-			Logger.getLogger(HtmlElement.class.getName()).log(Level.SEVERE, null, ex);
-		}
+		
+		functions.put("md5", new Adapter<String, String>() {
+
+			@Override public String adapt(String s) throws FrameworkException {
+				return s != null ? DigestUtils.md5Hex(s) : null;
+			}
+			
+		});
+		
+		functions.put("upper", new Adapter<String, String>() {
+
+			@Override public String adapt(String s) throws FrameworkException {
+				return s != null ? s.toUpperCase() : null;
+			}
+			
+		});
+		
+		functions.put("lower", new Adapter<String, String>() {
+
+			@Override public String adapt(String s) throws FrameworkException {
+				return s != null ? s.toLowerCase() : null;
+			}
+			
+		});
+		
+		functions.put("capitalize", new Adapter<String, String>() {
+
+			@Override public String adapt(String s) throws FrameworkException {
+				return s != null ? StringUtils.capitalize(s) : null;
+			}
+			
+		});
+		
 	}
 
 	//~--- static initializers --------------------------------------------
@@ -117,7 +147,7 @@ public abstract class HtmlElement extends Element {
 		return htmlAttributes;
 	}
 
-	public String getReferencedProperty(String resourceId, String componentId, String key) {
+	public String getReferencedProperty(String resourceId, String componentId, String key) throws FrameworkException {
 
 		java.lang.Object rawValue = super.getProperty(key);
 		String value              = null;
@@ -131,44 +161,15 @@ public abstract class HtmlElement extends Element {
 			while (matcher.find()) {
 
 				String group  = matcher.group();
-				String refKey = group.substring(2, group.length() - 1);
-				boolean md5   = false;
-
-				if (refKey.startsWith("md5(") && refKey.endsWith(")")) {
-
-					refKey = StringUtils.stripStart(refKey, "md5(");
-					refKey = StringUtils.stripEnd(refKey, ")");
-					md5    = true;
-
-				}
+				String source = group.substring(2, group.length() - 1);
 
 				// fetch referenced property
-				String partValue = convertValueForHtml(getReferencedProperty(securityContext, this, resourceId, componentId, refKey));
-
-				// Eventually replace by MD5 hash
-				if (md5) {
-
-					Method function = functions.get("md5");
-					try {
-						partValue = (String) function.invoke(HtmlElement.class, partValue);
-					} catch (IllegalAccessException ex) {
-						Logger.getLogger(HtmlElement.class.getName()).log(Level.SEVERE, null, ex);
-					} catch (IllegalArgumentException ex) {
-						Logger.getLogger(HtmlElement.class.getName()).log(Level.SEVERE, null, ex);
-					} catch (InvocationTargetException ex) {
-						Logger.getLogger(HtmlElement.class.getName()).log(Level.SEVERE, null, ex);
-					}
-
-				}
-
+				String partValue = extractFunctions(resourceId, componentId, source);
 				if (partValue != null) {
 
 					value = value.replace(group, partValue);
-
 				}
-
 			}
-
 		}
 
 		return value;
@@ -203,6 +204,39 @@ public abstract class HtmlElement extends Element {
 
 			}
 
+			// special keyword "link"
+			if ("link".equals(part.toLowerCase())) {
+
+				for(AbstractRelationship rel : node.getRelationships(RelType.LINK, Direction.OUTGOING)) {
+					node = rel.getEndNode();
+					break;
+				}
+
+				continue;
+			}
+
+			// special keyword "parent"
+			if ("parent".equals(part.toLowerCase())) {
+
+				for(AbstractRelationship rel : node.getRelationships(RelType.CONTAINS, Direction.INCOMING)) {
+					node = rel.getStartNode();
+					break;
+				}
+
+				continue;
+			}
+
+			// special keyword "parent"
+			if ("owner".equals(part.toLowerCase())) {
+
+				for(AbstractRelationship rel : node.getRelationships(RelType.OWNS, Direction.INCOMING)) {
+					node = rel.getStartNode();
+					break;
+				}
+
+				continue;
+			}
+
 		}
 
 		if (node != null) {
@@ -231,5 +265,34 @@ public abstract class HtmlElement extends Element {
 		return null;
 	}
 
-	public static String md5(final String in) { return in != null ? DigestUtils.md5Hex(in) : null; }
+	public String extractFunctions(String resourceId, String componentId, String source) throws FrameworkException {
+		
+		Matcher functionMatcher = functionPattern.matcher(source);
+		if(functionMatcher.matches()) {
+
+			String functionGroup = functionMatcher.group(1);
+			String parameter     = functionMatcher.group(2);
+
+			String function = functionGroup.substring(0, functionGroup.length());
+
+			Adapter<String, String> adapter = functions.get(function);
+			if(adapter != null) {
+				
+				return adapter.adapt(extractFunctions(resourceId, componentId, parameter));
+			}
+		}
+		
+		if(source.startsWith("\"") && source.endsWith("\"")) {
+
+			return source.substring(1, source.length() - 1);
+			
+		} else if(source.startsWith("'") && source.endsWith("'")) {
+
+			return source.substring(1, source.length() - 1);
+			
+		} else {
+
+			return convertValueForHtml(getReferencedProperty(securityContext, this, resourceId, componentId, source));
+		}
+	}
 }
