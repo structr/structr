9d6ec9916165448fc86f8f1ca5b1cec7352a8dc6
Christian Morgner <christian@morgner.de>
2012-05-13 20:43:58 +0200

    Added class skeleton for Cache list entity, to be implemented.

---
+++ structr/structr-core/src/main/java/org/structr/core/entity/Cache.java
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (C) 2012 Axel Morgner
+ * 
+ *  This file is part of structr <http://structr.org>.
+ * 
+ *  structr is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  structr is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.structr.core.entity;
+
+import java.util.Collections;
+import java.util.List;
+import org.structr.common.PropertyKey;
+import org.structr.common.SecurityContext;
+
+/**
+ *
+ * @author Christian Morgner
+ */
+public class Cache extends AbstractNode {
+
+	public enum Key implements PropertyKey {
+		sortKey, size
+	}
+	
+	static {
+		
+	}
+
+	
+	private int size = 0;
+	
+	public List<AbstractNode> getCachedList(SecurityContext securityContext, int numResults) {
+		
+		return Collections.emptyList();
+	}
+	
+	public void insertNode(AbstractNode node) {
+		
+	}
+	
+	public void removeNode(AbstractNode node) {
+		
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	@Override
+	public void onNodeInstantiation() {
+		this.size = getIntProperty(Key.size);
+	}
+	
+	@Override
+	public String getIconSrc() {
+		return null;
+	}
+}
