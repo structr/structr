7db06e9f832533cb2d912cd260da876ae4aa797e
amorgner <axel@morgner.de>
2012-07-17 19:27:53 +0200

    added license header

--- structr/structr-rest/src/main/java/org/structr/rest/resource/TypeResource.java
+++ structr/structr-rest/src/main/java/org/structr/rest/resource/TypeResource.java
@@ -1,8 +1,22 @@
-
 /*
-* To change this template, choose Tools | Templates
-* and open the template in the editor.
+ *  Copyright (C) 2010-2012 Axel Morgner, structr <structr@structr.org>
+ * 
+ *  This file is part of structr <http://structr.org>.
+ * 
+ *  structr is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  structr is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with structr.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.structr.rest.resource;
 
 import org.structr.common.SecurityContext;
@@ -12,7 +26,6 @@ import org.structr.core.GraphObject;
 import org.structr.core.Services;
 import org.structr.core.entity.AbstractNode;
 import org.structr.core.node.CreateNodeCommand;
-import org.structr.core.node.CypherQueryCommand;
 import org.structr.core.node.StructrTransaction;
 import org.structr.core.node.TransactionCommand;
 import org.structr.core.node.search.*;
