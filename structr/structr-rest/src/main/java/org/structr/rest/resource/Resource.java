d7f385c6310bb459cf8a62e59d99e6c6579b44ab
amorgner <axel@morgner.de>
2012-07-17 19:27:53 +0200

    added license header

--- structr/structr-rest/src/main/java/org/structr/rest/resource/Resource.java
+++ structr/structr-rest/src/main/java/org/structr/rest/resource/Resource.java
@@ -1,8 +1,22 @@
-
 /*
-* To change this template, choose Tools | Templates
-* and open the template in the editor.
+ *  Copyright (C) 2010-2012 Axel Morgner, structr <structr@structr.org>
+ * 
+ *  This file is part of structr <http://structr.org>.
+ * 
+ *  structr is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  structr is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with structr.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.structr.rest.resource;
 
 import org.apache.commons.lang.StringUtils;
@@ -17,7 +31,6 @@ import org.structr.core.*;
 import org.structr.core.EntityContext;
 import org.structr.core.GraphObject;
 import org.structr.core.Services;
-import org.structr.core.StructrTransactionListener;
 import org.structr.core.Value;
 import org.structr.core.entity.AbstractNode;
 import org.structr.core.entity.AbstractRelationship;
