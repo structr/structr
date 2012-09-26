3eaa27009f4cc98af9b7bc8facbccc1fe8061db6
Christian Morgner <christian@morgner.de>
2012-09-26 14:04:39 +0200

    Allow more than one grant for resource access, log incident only, do not raise an error.

--- structr/structr-rest/src/main/java/org/structr/rest/resource/Resource.java
+++ structr/structr-rest/src/main/java/org/structr/rest/resource/Resource.java
@@ -337,21 +337,19 @@ public abstract class Resource {
 
 		} else {
 
-			if (result.size() == 1) {
+			AbstractNode node = (AbstractNode) result.get(0);
 
-				AbstractNode node = (AbstractNode) result.get(0);
+			if (node instanceof ResourceAccess) {
 
-				if (node instanceof ResourceAccess) {
-
-					grant = (ResourceAccess) node;
-
-				} else {
-
-					logger.log(Level.SEVERE, "Grant for URI {0} has wrong type!", new Object[] { uriPart, node.getClass().getName() });
-
-				}
+				grant = (ResourceAccess) node;
 
 			} else {
+
+				logger.log(Level.SEVERE, "Grant for URI {0} has wrong type!", new Object[] { uriPart, node.getClass().getName() });
+
+			}
+
+			if (result.size() > 1) {
 
 				logger.log(Level.SEVERE, "Found {0} grants for URI {1}!", new Object[] { result.size(), uriPart });
 
