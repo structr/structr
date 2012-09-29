490962d308b755ec058f917b98c0f9f2aaf02463
Axel Morgner <axel@morgner.de>
2012-09-29 11:06:11 +0200

    added sortType to inherting type resource

--- structr/structr-rest/src/main/java/org/structr/rest/resource/InheritingTypeResource.java
+++ structr/structr-rest/src/main/java/org/structr/rest/resource/InheritingTypeResource.java
@@ -40,8 +40,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
+import org.apache.lucene.search.SortField;
 import org.structr.common.GraphObjectComparator;
 import org.structr.common.PropertyKey;
+import org.structr.core.converter.DateConverter;
+import org.structr.core.converter.IntConverter;
+import org.structr.core.converter.PropertyConverter;
 
 //~--- classes ----------------------------------------------------------------
 
@@ -99,6 +103,16 @@ public class InheritingTypeResource extends TypeResource {
 				}
 			}
 			
+			Integer sortType = null;
+			PropertyConverter converter = EntityContext.getPropertyConverter(securityContext, entityClass, sortKey);
+			if (converter != null) {
+				if (converter instanceof IntConverter) {
+					sortType = SortField.INT;
+				} else if (converter instanceof DateConverter) {
+					sortType = SortField.LONG;
+				}
+			}
+			
 			// do search
 			Result results = (Result) Services.command(securityContext, SearchNodeCommand.class).execute(
 				topNode,
@@ -108,7 +122,8 @@ public class InheritingTypeResource extends TypeResource {
 				sortKey,
 				sortDescending,
 				pageSize,
-				page
+				page,
+				sortType
 			);
 			
 			// TODO: SORTING: remove default sorting below
