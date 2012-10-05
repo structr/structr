2ed893e525a982a589643f728a9e107dc1afa1a1
Christian Morgner <christian@morgner.de>
2012-10-05 09:48:04 +0200

    Added javadocs for some of the main framework classes.

--- structr/structr-core/src/main/java/org/structr/core/ViewTransformation.java
+++ structr/structr-core/src/main/java/org/structr/core/ViewTransformation.java
@@ -1,41 +1,29 @@
 /*
- *  Copyright (C) 2010-2012 Axel Morgner, structr <structr@structr.org>
- *
+ *  Copyright (C) 2012 Axel Morgner
+ * 
  *  This file is part of structr <http://structr.org>.
- *
+ * 
  *  structr is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
+ * 
  *  structr is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
+ * 
  *  You should have received a copy of the GNU General Public License
  *  along with structr.  If not, see <http://www.gnu.org/licenses/>.
  */
+package org.structr.core;
 
-
-
-package org.structr.core.entity;
-
-import java.util.Date;
-import org.structr.common.PropertyView;
-import org.structr.core.EntityContext;
-
-//~--- classes ----------------------------------------------------------------
+import java.util.List;
 
 /**
  *
- * @author amorgner
- *
+ * @author Christian Morgner
  */
-public class Category extends AbstractNode {
-
-	static {
-
-		EntityContext.registerPropertySet(Category.class, PropertyView.All, Key.values());
-	}
+public interface ViewTransformation extends Transformation<List<? extends GraphObject>> {
+	public boolean evaluateWrappedResource();
 }
