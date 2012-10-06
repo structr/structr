e331952104af597ac489823df558e2f7ced7ac96
Axel Morgner <axel@morgner.de>
2012-10-06 14:36:51 +0200

    added license header

--- structr/structr-ui/src/main/java/org/structr/Ui.java
+++ structr/structr-ui/src/main/java/org/structr/Ui.java
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (C) 2010-2012 Axel Morgner, structr <structr@structr.org>
+ *
+ *  This file is part of structr <http://structr.org>.
+ *
+ *  structr is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  structr is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 package org.structr;
 
 import java.util.HashMap;
