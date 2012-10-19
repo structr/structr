0eaad58cafa9a79d83d9ee1706ba5b059bb40e8b
Bastian Knerr <bastian.knerr@fortytools.com>
2012-10-19 17:20:18 +0200

    Restructured duplicate hasSearchableAttribute code in Resource class, changed method signature to reflect its purpose more clearly

--- structr/structr-rest/src/main/java/org/structr/rest/resource/InheritingTypeResource.java
+++ structr/structr-rest/src/main/java/org/structr/rest/resource/InheritingTypeResource.java
@@ -80,7 +80,7 @@ public class InheritingTypeResource extends TypeResource {
 			searchAttributes.add(Search.andExactTypeAndSubtypes(EntityContext.normalizeEntityName(rawType)));
 
 			// searchable attributes from EntityContext
-			hasSearchableAttributes(searchAttributes);
+			searchAttributes.addAll(extractSearchableAttributesFromRequest());
 
 			// default sort key & order
 			if(sortKey == null) {
