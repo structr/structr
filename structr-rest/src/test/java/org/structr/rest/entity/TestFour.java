7c2120a8e585b1fa2b1351af0c466e801cb7cb47
Axel Morgner <axel@morgner.de>
2012-12-25 15:29:05 +0100

    added REST tests for collection property (at least manyToMany for now

---
+++ structr-rest/src/test/java/org/structr/rest/entity/TestFour.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (C) 2012 Axel Morgner
+ * 
+ *  This file is part of structr <http://structr.org>.
+ * 
+ *  structr is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  structr is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.structr.rest.entity;
+
+import org.structr.core.entity.AbstractNode;
+import org.structr.core.property.CollectionProperty;
+import org.structr.core.property.EntityProperty;
+import org.structr.rest.common.TestRestRelType;
+
+/**
+ *
+ * @author Axel Morgner
+ */
+public class TestFour extends AbstractNode {
+	
+	public static final CollectionProperty<TestOne> manyToManyTestOnes = new CollectionProperty<TestOne>("manyToManyTestOnes", TestOne.class, TestRestRelType.MANY_TO_MANY, false);
+	public static final CollectionProperty<TestOne> oneToManyTestOnes  = new CollectionProperty<TestOne>("oneToManyTestOnes",  TestOne.class, TestRestRelType.ONE_TO_MANY,  true);
+	
+	public static final EntityProperty<TestThree>   oneToOneTestThree  = new EntityProperty<TestThree>("oneToOneTestThree",  TestThree.class, TestRestRelType.ONE_TO_ONE,  false);
+	public static final EntityProperty<TestThree>   manyToOneTestThree = new EntityProperty<TestThree>("manyToOneTestThree", TestThree.class, TestRestRelType.MANY_TO_ONE, true);
+}
---
+++ structr-rest/src/test/java/org/structr/rest/test/property/CollectionPropertyTest.java
@@ -0,0 +1,132 @@
+/*
+ *  Copyright (C) 2012 Axel Morgner
+ * 
+ *  This file is part of structr <http://structr.org>.
+ * 
+ *  structr is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  structr is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.structr.rest.test.property;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.filter.log.ResponseLoggingFilter;
+import org.structr.rest.common.StructrRestTest;
+import static org.hamcrest.Matchers.*;
+
+/**
+ *
+ * @author Axel Morgner
+ */
+public class CollectionPropertyTest extends StructrRestTest {
+
+	public void testManyToMany() throws Exception {
+
+		String[] testOneIds = new String[3];
+		
+		for (int i=0; i<3; i++) {
+		
+			String location = RestAssured.given()
+				.contentType("application/json; charset=UTF-8")
+				//.body(" { 'name' : 'TestOne-'" + i + "' } ")
+			.expect()
+				.statusCode(201)
+			.when()
+				.post("/test_ones")
+				.getHeader("Location");
+			
+			testOneIds[i] = getUuidFromLocation(location);
+		
+		}
+		
+		// POST to create a TestOne object with relationships to the three TestOnes
+		String location = RestAssured.given()
+			.contentType("application/json; charset=UTF-8")
+			.body(" { 'manyToManyTestOnes' : ['" + testOneIds[0] + "', '" + testOneIds[1] + "', '" + testOneIds[2] + "'] } ")
+		.expect()
+			.statusCode(201)
+		.when()
+			.post("/test_fives")
+			.getHeader("Location");
+		
+		String testFiveId = getUuidFromLocation(location);
+		
+		System.out.println("ID of TestFive node: " + testFiveId);
+		
+		RestAssured.given()
+			.contentType("application/json; charset=UTF-8")
+			.filter(ResponseLoggingFilter.logResponseIfStatusCodeIs(200))
+			.filter(ResponseLoggingFilter.logResponseIfStatusCodeIs(500))
+		.expect()
+			.statusCode(200)
+			.body("result.manyToManyTestOnes", hasItems(testOneIds[0], testOneIds[1], testOneIds[2]))
+		.when()
+			.get("/test_fives/" + testFiveId);
+		
+				
+	}
+
+	public void testOneToMany() throws Exception {
+		
+
+	}
+	
+	
+	/**
+	 * Test of typeName method, of class CollectionProperty.
+	 */
+	public void testTypeName() {
+
+	}
+
+	/**
+	 * Test of databaseConverter method, of class CollectionProperty.
+	 */
+	public void testDatabaseConverter() {
+
+	}
+
+	/**
+	 * Test of inputConverter method, of class CollectionProperty.
+	 */
+	public void testInputConverter() {
+
+	}
+
+	/**
+	 * Test of relatedType method, of class CollectionProperty.
+	 */
+	public void testRelatedType() {
+
+	}
+
+	/**
+	 * Test of isCollection method, of class CollectionProperty.
+	 */
+	public void testIsCollection() {
+
+	}
+
+	/**
+	 * Test of getNotion method, of class CollectionProperty.
+	 */
+	public void testGetNotion() {
+
+	}
+
+	/**
+	 * Test of isOneToMany method, of class CollectionProperty.
+	 */
+	public void testIsOneToMany() {
+
+	}
+}
