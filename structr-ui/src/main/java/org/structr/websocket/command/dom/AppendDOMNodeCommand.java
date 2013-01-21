da70b5b532ae9f11c4e32948ee73feccad19c815
Axel Morgner <axel@morgner.de>
2013-01-21 11:21:12 +0100

    new command to use appendChild method of new DOM API

---
+++ structr-ui/src/main/java/org/structr/websocket/command/dom/AppendDOMNodeCommand.java
@@ -0,0 +1,85 @@
+package org.structr.websocket.command.dom;
+
+import java.util.Map;
+
+import org.structr.web.entity.dom.DOMNode;
+import org.structr.websocket.StructrWebSocket;
+import org.structr.websocket.command.AbstractCommand;
+import org.structr.websocket.message.MessageBuilder;
+import org.structr.websocket.message.WebSocketMessage;
+
+import org.w3c.dom.DOMException;
+
+/**
+ *
+ * @author Axel Morgner
+ */
+public class AppendDOMNodeCommand extends AbstractCommand {
+
+	static {
+
+		StructrWebSocket.addCommand(AppendDOMNodeCommand.class);
+	}
+
+	@Override
+	public void processMessage(WebSocketMessage webSocketData) {
+
+		String id                    = webSocketData.getId();
+		Map<String, Object> nodeData = webSocketData.getNodeData();
+		String parentId              = (String) nodeData.get("parentId");
+
+		// check node to append
+		if (id == null) {
+
+			getWebSocket().send(MessageBuilder.status().code(422).message("Cannot no node to append").build(), true);
+
+			return;
+
+		}
+
+		// check for parent ID
+		if (parentId == null) {
+
+			getWebSocket().send(MessageBuilder.status().code(422).message("Cannot add node without parentId").build(), true);
+
+			return;
+
+		}
+
+		// check if parent node with given ID exists
+		DOMNode parentNode = getDOMNode(parentId);
+
+		if (parentNode == null) {
+
+			getWebSocket().send(MessageBuilder.status().code(404).message("Parent node not found").build(), true);
+
+			return;
+
+		}
+
+		DOMNode node = getDOMNode(id);
+
+		try {
+
+			// append node to parent
+			if (node != null) {
+
+				parentNode.appendChild(node);
+				
+			}
+		} catch (DOMException dex) {
+
+			// send DOM exception
+			getWebSocket().send(MessageBuilder.status().code(422).message(dex.getMessage()).build(), true);
+		}
+
+	}
+
+	@Override
+	public String getCommand() {
+
+		return "APPEND_DOM_NODE";
+
+	}
+
+}
