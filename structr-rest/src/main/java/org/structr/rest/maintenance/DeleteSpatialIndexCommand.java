26719757967367d6b9fd304e3cae3bbaaf18aa18
Christian Morgner <christian@morgner.de>
2013-04-24 17:10:49 +0200

    Implemented DeleteSpatialCommand.

--- structr-rest/src/main/java/org/structr/rest/maintenance/DeleteSpatialIndexCommand.java
+++ structr-rest/src/main/java/org/structr/rest/maintenance/DeleteSpatialIndexCommand.java
@@ -1,10 +1,14 @@
 package org.structr.rest.maintenance;
 
 import java.util.Map;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.traversal.TraversalDescription;
+import org.neo4j.graphdb.traversal.Traverser;
 import org.neo4j.kernel.Traversal;
 import org.neo4j.kernel.Uniqueness;
 import org.structr.common.error.FrameworkException;
@@ -21,6 +25,8 @@ import org.structr.rest.resource.MaintenanceParameterResource;
  */
 public class DeleteSpatialIndexCommand extends NodeServiceCommand implements MaintenanceCommand {
 
+	private static final Logger logger = Logger.getLogger(DeleteSpatialIndexCommand.class.getName());
+	
 	static {
 		
 		MaintenanceParameterResource.registerMaintenanceCommand("deleteSpatialIndex", DeleteSpatialIndexCommand.class);
@@ -40,10 +46,28 @@ public class DeleteSpatialIndexCommand extends NodeServiceCommand implements Mai
 			.relationships(org.neo4j.collections.rtree.RTreeRelationshipTypes.RTREE_REFERENCE, Direction.OUTGOING)
 			
 		;
-		
-		Node referenceNode = graphDb.getRelationshipById(0);
-		description.traverse(referenceNode);
 
+		Node referenceNode = graphDb.getNodeById(0);
+		Traverser traverser = description.traverse(referenceNode);
+
+		for (Node node: traverser.nodes()) {
+		
+			logger.log(Level.INFO, "Deleting node {0}", node);
+			
+			try {
+				
+				for (Relationship rel : node.getRelationships()) {
+
+					rel.delete();
+				}
+
+				node.delete();
+				
+			} catch (Throwable t) {
+				
+				t.printStackTrace();
+			}
+		}
 	}
 
 }
