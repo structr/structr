e64b2127ca11b45d844d83e7203f2b221b03db41
Axel Morgner <axel@morgner.de>
2013-06-02 23:57:49 +0200

    fixed run scripts to run 0.7 instead of 0.7-SNAPSHOT

--- structr-ui/run.sh
+++ structr-ui/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Dfile.encoding=utf-8 -Xms1g -Xmx1g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseNUMA -cp target/lib/*:target/structr-ui-0.7-SNAPSHOT.jar org.structr.Ui
+java -Dfile.encoding=utf-8 -Xms1g -Xmx1g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseNUMA -cp target/lib/*:target/structr-ui-0.7.jar org.structr.Ui
--- structr-ui/run_debug.sh
+++ structr-ui/run_debug.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n -Dfile.encoding=utf-8 -Xms1g -Xmx1g -XX:+UseNUMA -cp target/lib/*:target/structr-ui-0.7-SNAPSHOT.jar org.structr.Ui
+java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n -Dfile.encoding=utf-8 -Xms1g -Xmx1g -XX:+UseNUMA -cp target/lib/*:target/structr-ui-0.7.jar org.structr.Ui
