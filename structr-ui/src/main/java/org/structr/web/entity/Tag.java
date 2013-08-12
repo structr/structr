8ca6578a04693b09510b5d692e91e7e40047e2cc
Axel Morgner <axel@morgner.de>
2013-08-12 11:10:08 +0200

    added pictures and tags properties to Widget class

---
+++ structr-ui/src/main/java/org/structr/web/entity/Tag.java
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (C) 2013 Axel Morgner
+ * 
+ *  This file is part of structr <http://structr.org>.
+ * 
+ *  structr is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ *  structr is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.structr.web.entity;
+
+import org.structr.core.entity.ValidatedNode;
+import org.structr.common.PropertyView;
+import org.structr.common.SecurityContext;
+import org.structr.common.View;
+import org.structr.common.error.ErrorBuffer;
+import org.structr.common.error.FrameworkException;
+import org.structr.core.entity.AbstractNode;
+import org.structr.core.property.CollectionProperty;
+import org.structr.web.common.RelType;
+import org.structr.web.property.UiNotion;
+
+/**
+ *
+ * @author Christian Morgner
+ */
+public class Tag extends ValidatedNode {
+
+	public static final CollectionProperty<Taggable> taggables = new CollectionProperty<Taggable>("taggables", Taggable.class, RelType.TAG, new UiNotion(), false);
+	
+	public static final View defaultView = new View(Tag.class, PropertyView.Public, name, taggables);
+	public static final View uiView      = new View(Tag.class, PropertyView.Ui, name, taggables);
+	
+	@Override
+	public boolean isValid(ErrorBuffer errorBuffer) {
+		
+		boolean valid = true;
+		
+		valid &= nonEmpty(AbstractNode.name, errorBuffer);
+		
+		return valid;
+	}
+	
+	@Override
+	public boolean onCreation(SecurityContext securityContext, ErrorBuffer errorBuffer) throws FrameworkException {
+
+		// Make tags visible to anyone upon creation
+		setProperty(visibleToPublicUsers, true);
+		setProperty(visibleToAuthenticatedUsers, true);
+		
+		return super.onModification(securityContext, errorBuffer);
+	}
+
+
+}
--- structr-ui/src/main/java/org/structr/web/entity/Widget.java
+++ structr-ui/src/main/java/org/structr/web/entity/Widget.java
@@ -1,37 +1,62 @@
+/**
+ * Copyright (C) 2010-2013 Axel Morgner, structr <structr@structr.org>
+ *
+ * This file is part of structr <http://structr.org>.
+ *
+ * structr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * structr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.structr.web.entity;
 
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
+import org.neo4j.graphdb.Direction;
 import org.structr.common.PropertyView;
 import org.structr.common.SecurityContext;
 import org.structr.common.error.EmptyPropertyToken;
 import org.structr.common.error.ErrorBuffer;
 import org.structr.common.error.FrameworkException;
 import org.structr.core.entity.AbstractNode;
+import org.structr.core.property.CollectionProperty;
 import org.structr.core.property.Property;
 import org.structr.core.property.StringProperty;
 import org.structr.web.Importer;
+import org.structr.web.common.RelType;
 import org.structr.web.common.ThreadLocalMatcher;
 import org.structr.web.entity.dom.DOMNode;
 import org.structr.web.entity.dom.Page;
+import org.structr.web.property.UiNotion;
 
 /**
  *
  * @author Christian Morgner
  */
-public class Widget extends AbstractNode {
+public class Widget extends AbstractNode implements Taggable {
 
-	private static final ThreadLocalMatcher threadLocalTemplateMatcher = new ThreadLocalMatcher("\\[[a-zA-Z]+\\]");
-	public static final Property<String> source                        = new StringProperty("source");
+	private static final ThreadLocalMatcher		threadLocalTemplateMatcher	= new ThreadLocalMatcher("\\[[a-zA-Z]+\\]");
+	public static final Property<String>		source				= new StringProperty("source");
+	public static final Property<String>		description			= new StringProperty("description");
+	public static final CollectionProperty<Image>	pictures				= new CollectionProperty("pictures", Image.class, RelType.PICTURE_OF, Direction.INCOMING, new UiNotion(), true);
 	
 	public static final org.structr.common.View uiView = new org.structr.common.View(User.class, PropertyView.Ui,
-		type, name, source
+		type, name, source, description, pictures, tags
 	);
 	
 	public static final org.structr.common.View publicView = new org.structr.common.View(User.class, PropertyView.Public,
-		type, name, source
+		type, name, source, description, pictures, tags
 	);
 	
 	public static void expandWidget(SecurityContext securityContext, Page page, DOMNode parent, Map<String, Object> parameters) throws FrameworkException {
@@ -65,7 +90,7 @@ public class Widget extends AbstractNode {
 				} else {
 					
 					// replace and restart matching process
-					_source = _source.replaceAll(group, value.toString());
+					_source = _source.replace(group, value.toString());
 					matcher.reset(_source);
 				}
 				
