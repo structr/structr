4c379408d2d851cea6972fbf143b12bff4da9e15
Axel Morgner <axel@morgner.de>
2014-02-09 20:10:29 +0100

    Up to 1.0-SNAPSHOT

--- structr-ui/run.sh
+++ structr-ui/run.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-<<<<<<< HEAD
-java -Dfile.encoding=utf-8 -Xms1g -Xmx1g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseNUMA -cp target/lib/*:target/structr-ui-0.8.2.jar org.structr.Ui
-=======
-java -Dfile.encoding=utf-8 -Xms1g -Xmx1g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseNUMA -cp target/lib/*:target/structr-ui-0.9-SNAPSHOT.jar org.structr.Server
->>>>>>> develop
+java -Dfile.encoding=utf-8 -Xms1g -Xmx1g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseNUMA -cp target/lib/*:target/structr-ui-1.0-SNAPSHOT.jar org.structr.Server
