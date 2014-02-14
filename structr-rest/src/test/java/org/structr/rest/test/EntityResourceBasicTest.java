14f99dcdbb2899f7cb78001275856b3671ed6dd6
Axel Morgner <axel@morgner.de>
2014-02-14 15:45:47 +0100

    Increased test time interval to make test pass on slower CI environments

--- structr-rest/src/test/java/org/structr/rest/test/EntityResourceBasicTest.java
+++ structr-rest/src/test/java/org/structr/rest/test/EntityResourceBasicTest.java
@@ -131,7 +131,7 @@ public class EntityResourceBasicTest extends StructrRestTest {
 				.statusCode(200)
 				.body("result_count",		equalTo(1))
 				.body("query_time",		lessThan("0.1"))
-				.body("serialization_time",	lessThan("0.005"))
+				.body("serialization_time",	lessThan("0.01"))
 				.body("result",			isEntity(TestObject.class))
 			.when()
 				.get("/test_objects/" + uuid);
