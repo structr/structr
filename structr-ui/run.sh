8e390de0e8e5c7043e3fcf1e26b6ca0d4e607c04
Axel Morgner <axel@morgner.de>
2014-02-16 11:11:40 +0100

    Fixed wrong base path in run script

--- structr-ui/run.sh
+++ structr-ui/run.sh
@@ -3,7 +3,7 @@ JAVA=`which java`
 STRUCTR="-cp target/lib/*:target/structr-ui-1.0-SNAPSHOT.jar org.structr.Server"
 STRUCTR_ARGS="-server -d64 -Xms512m -Xmx512m -XX:+UseNUMA -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -Dinstance=structr.com"
 
-BASE_DIR=/opt/structr/structr.com
+BASE_DIR=.
 PIDFILE=$BASE_DIR/structr-ui.pid
 LOGS_DIR=$BASE_DIR/logs
 SERVER_LOG=$BASE_DIR/logs/server.log
