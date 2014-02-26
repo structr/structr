1fdacc8690e30855a8eca1b1aa41dfb89a3f526a
Axel Morgner <axel@morgner.de>
2014-02-26 15:48:04 +0100

    Ignore comment lines in structr.conf

--- structr-ui/run.sh
+++ structr-ui/run.sh
@@ -5,7 +5,7 @@ STRUCTR_ARGS="-server -d64 -Xms512m -Xmx512m -XX:+UseNUMA -XX:+UseParNewGC -XX:+
 
 STRUCTR_CONF=`find . -name structr.conf`
 echo "Starting Structr with config file $STRUCTR_CONF"
-BASE_DIR=`grep -m1 "base\.path" $STRUCTR_CONF | awk '{ print $3 }' | tr -d [:cntrl:]`
+BASE_DIR=`grep -v '^#' $STRUCTR_CONF | grep -m1 "base\.path" | awk '{ print $3 }' | tr -d [:cntrl:]`
 
 PIDFILE=$BASE_DIR/structr-ui.pid
 LOGS_DIR=$BASE_DIR/logs
@@ -20,17 +20,23 @@ echo $! >$PIDFILE
 
 { tail -q -n0 --pid=$! -F $SERVER_LOG 2>/dev/null & } | sed -n '/Initialization complete/q'
 tail -200 $SERVER_LOG 2> /dev/null | grep 'Starting'
-echo "       _                          _         "
-echo " ___  | |_   ___   _   _   ____  | |_   ___ "
-echo "(  _| | __| |  _| | | | | |  __| | __| |  _|"
-echo " \ \  | |_  | |   | |_| | | |__  | |_  | |  "
-echo "|___) |___| |_|   |_____| |____| |___| |_|  "
-echo                              
-echo "Structr started successfully (PID $!)"
 
-#echo "        _                          _         "
-#echo " ____  | |_   ___   _   _   ____  | |_   ___ "
-#echo "(  __| | __| |  _| | | | | |  __| | __| |  _|"
-#echo " \ \   | |   | |   | | | | | |    | |   | |  "
-#echo " _\ \  | |_  | |   | |_| | | |__  | |_  | |  "
-#echo "|____) |___| |_|   |_____| |____| |___| |_|  "
+# If your console font is rather slim, you can change the ascii art message to
+# better fit the structr logo ;-) (you know, details matter...)
+
+#echo "       _                          _         "
+#echo " ___  | |_   ___   _   _   ____  | |_   ___ "
+#echo "(  _| | __| |  _| | | | | |  __| | __| |  _|"
+#echo " \ \  | |_  | |   | |_| | | |__  | |_  | |  "
+#echo "|___) |___| |_|   |_____| |____| |___| |_|  "
+
+
+echo "        _                          _         "
+echo " ____  | |_   ___   _   _   ____  | |_   ___ "
+echo "(  __| | __| |  _| | | | | |  __| | __| |  _|"
+echo " \ \   | |   | |   | | | | | |    | |   | |  "
+echo " _\ \  | |_  | |   | |_| | | |__  | |_  | |  "
+echo "|____) |___| |_|   |_____| |____| |___| |_|  "
+
+echo
+echo "Structr started successfully (PID $!)"
