322a3982309c8bdbfc627caf0f100cc77f889eac
Axel Morgner <axel@morgner.de>
2014-03-14 16:05:28 +0100

    Found a way to re-introduce early flushing which works with new async output.

--- structr-ui/src/main/java/org/structr/web/common/AsyncBuffer.java
+++ structr-ui/src/main/java/org/structr/web/common/AsyncBuffer.java
@@ -1,26 +1,25 @@
-/*
- *  Copyright (C) 2014 Axel Morgner
- * 
- *  This file is part of structr <http://structr.org>.
- * 
- *  structr is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as
- *  published by the Free Software Foundation, either version 3 of the
- *  License, or (at your option) any later version.
- * 
- *  structr is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- * 
- *  You should have received a copy of the GNU Affero General Public License
- *  along with structr.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Copyright (C) 2010-2014 Morgner UG (haftungsbeschränkt)
+ *
+ * This file is part of Structr <http://structr.org>.
+ *
+ * Structr is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Structr is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Structr. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.structr.web.common;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.AsyncContext;
@@ -29,39 +28,33 @@ import javax.servlet.WriteListener;
 
 /**
  * Special buffer for asynchronous streaming of chunked output.
- * 
+ *
  * @author Axel Morgner
  */
-public class AsyncBuffer extends InputStream implements WriteListener, Appendable {
+public class AsyncBuffer implements WriteListener {
 
 	private static final Logger logger = Logger.getLogger(AsyncBuffer.class.getName());
 
-	private static final int COPY_BUFFER_SIZE = 4096;
-	private byte[] buffer = new byte[COPY_BUFFER_SIZE];
-	
-	private final StringBuilder sb = new StringBuilder(8192);
-	private ByteArrayInputStream content    = null;
+	private final Queue<String> queue = new LinkedList<>();
 
 	private AsyncContext async;
 	private ServletOutputStream out;
 
 	private boolean completed = false;
-	
+
 	public void prepare(final AsyncContext async, final ServletOutputStream out) {
 
 		this.async = async;
 		this.out = out;
-		
+
 		out.setWriteListener(this);
 	}
 
 	public void flush() {
-		
+
 		try {
-			
-			content = new ByteArrayInputStream(sb.toString().getBytes("UTF-8"));
-			sb.delete(0, sb.length());
-			onWritePossible();
+
+			write();
 
 		} catch (IOException ex) {
 
@@ -71,61 +64,50 @@ public class AsyncBuffer extends InputStream implements WriteListener, Appendabl
 
 	}
 
-	public void finish() {
-	
-		flush();
-		completed = true;
-		async.complete();
-		
-	}
-	
-	@Override
-	public void onWritePossible() throws IOException {
+	private void write() throws IOException {
 
 		// Write to client if buffer is filled and client is ready
 		while (out.isReady()) {
 
-			int len = content.read(buffer);
+			String s = queue.poll();
 
-			if (len < 0) {
+			if (s == null) {
+				out.flush();
 				return;
 			}
 
-			if (!completed && len > 0) {
+			if (!completed) {
 				// write out the copy buffer.  
-				out.write(buffer, 0, len);
+				out.print(s);
 			}
 
 		}
 
 	}
 
+	public void finish() {
+
+		flush();
+		completed = true;
+		async.complete();
+
+	}
+
+	@Override
+	public void onWritePossible() throws IOException {
+
+		write();
+
+	}
+
 	@Override
 	public void onError(Throwable t) {
-		logger.log(Level.SEVERE, "Async error", t);
+		logger.log(Level.FINE, "Async error", t);
 		async.complete();
 	}
-	
-	@Override
-	public int read() throws IOException {
-		return (content != null ? content.read() : -1);
-	}
 
-	@Override
-	public AsyncBuffer append(CharSequence csq) {
-		sb.append(csq);
-		return this;
-	}
-
-	@Override
-	public AsyncBuffer append(CharSequence csq, int start, int end) {
-		sb.append(csq, start, end);
-		return this;
-	}
-
-	@Override
-	public AsyncBuffer append(char c) {
-		sb.append(c);
+	public AsyncBuffer append(final String s) {
+		queue.add(s);
 		return this;
 	}
 
