35f3def216206b2004d64b796537e1b04b93268a
amorgner <axel@morgner.de>
2014-05-21 22:44:07 +0200

    Upgraded CodeMirror to 4.2. Scroll position in file editor is now stored in localStorage.

--- structr-ui/src/main/resources/structr/codemirror/addon/edit/matchtags.js
+++ structr-ui/src/main/resources/structr/codemirror/addon/edit/matchtags.js
@@ -1,4 +1,14 @@
-(function() {
+// CodeMirror, copyright (c) by Marijn Haverbeke and others
+// Distributed under an MIT license: http://codemirror.net/LICENSE
+
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"), require("../fold/xml-fold"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror", "../fold/xml-fold"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
   "use strict";
 
   CodeMirror.defineOption("matchTags", false, function(cm, val, old) {
@@ -8,6 +18,7 @@
       clear(cm);
     }
     if (val) {
+      cm.state.matchBothTags = typeof val == "object" && val.bothTags;
       cm.on("cursorActivity", doMatchTags);
       cm.on("viewportChange", maybeUpdateMatch);
       doMatchTags(cm);
@@ -15,23 +26,27 @@
   });
 
   function clear(cm) {
-    if (cm.state.matchedTag) {
-      cm.state.matchedTag.clear();
-      cm.state.matchedTag = null;
-    }
+    if (cm.state.tagHit) cm.state.tagHit.clear();
+    if (cm.state.tagOther) cm.state.tagOther.clear();
+    cm.state.tagHit = cm.state.tagOther = null;
   }
 
   function doMatchTags(cm) {
     cm.state.failedTagMatch = false;
     cm.operation(function() {
       clear(cm);
+      if (cm.somethingSelected()) return;
       var cur = cm.getCursor(), range = cm.getViewport();
       range.from = Math.min(range.from, cur.line); range.to = Math.max(cur.line + 1, range.to);
       var match = CodeMirror.findMatchingTag(cm, cur, range);
       if (!match) return;
+      if (cm.state.matchBothTags) {
+        var hit = match.at == "open" ? match.open : match.close;
+        if (hit) cm.state.tagHit = cm.markText(hit.from, hit.to, {className: "CodeMirror-matchingtag"});
+      }
       var other = match.at == "close" ? match.open : match.close;
       if (other)
-        cm.state.matchedTag = cm.markText(other.from, other.to, {className: "CodeMirror-matchingtag"});
+        cm.state.tagOther = cm.markText(other.from, other.to, {className: "CodeMirror-matchingtag"});
       else
         cm.state.failedTagMatch = true;
     });
@@ -45,7 +60,7 @@
     var found = CodeMirror.findMatchingTag(cm, cm.getCursor());
     if (found) {
       var other = found.at == "close" ? found.open : found.close;
-      if (other) cm.setSelection(other.to, other.from);
+      if (other) cm.extendSelection(other.to, other.from);
     }
   };
-})();
+});
