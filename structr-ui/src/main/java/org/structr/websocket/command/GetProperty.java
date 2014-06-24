c1ebee925d24897e1fe4f5010c6717f5e3248227
Axel Morgner <axel@morgner.de>
2014-06-24 11:35:22 +0200

    Use converter for output formatting of single values

--- structr-ui/src/main/java/org/structr/websocket/command/GetProperty.java
+++ structr-ui/src/main/java/org/structr/websocket/command/GetProperty.java
@@ -3,50 +3,67 @@
  *
  * This file is part of Structr <http://structr.org>.
  *
- * Structr is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * Structr is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  *
- * Structr is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Structr is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Structr. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.structr.websocket.command;
 
+import org.structr.common.error.FrameworkException;
 import org.structr.core.app.StructrApp;
+import org.structr.core.converter.PropertyConverter;
 import org.structr.core.entity.AbstractNode;
+import org.structr.core.property.PropertyKey;
 import org.structr.websocket.StructrWebSocket;
 import org.structr.websocket.message.MessageBuilder;
 import org.structr.websocket.message.WebSocketMessage;
 
 //~--- classes ----------------------------------------------------------------
-
 /**
  *
  * @author Axel Morgner
  */
 public class GetProperty extends AbstractCommand {
-	
+
 	static {
-		
+
 		StructrWebSocket.addCommand(GetProperty.class);
-		
+
 	}
 
 	@Override
 	public void processMessage(final WebSocketMessage webSocketData) {
 
 		AbstractNode node = getNode(webSocketData.getId());
-		String key        = (String) webSocketData.getNodeData().get("key");
+		String key = (String) webSocketData.getNodeData().get("key");
 
 		if (node != null) {
 
-			webSocketData.setNodeData(key, node.getProperty(StructrApp.getConfiguration().getPropertyKeyForJSONName(node.getClass(), key)));
+			PropertyKey propertyKey = StructrApp.getConfiguration().getPropertyKeyForJSONName(node.getClass(), key);
+			PropertyConverter converter = propertyKey.inputConverter(getWebSocket().getSecurityContext());
+
+			Object value = node.getProperty(propertyKey);
+			if (converter != null) {
+
+				try {
+					value = converter.revert(value);
+
+				} catch (FrameworkException ex) {
+					
+					getWebSocket().send(MessageBuilder.status().code(400).message(ex.getMessage()).build(), true);
+
+				}
+			}
+
+			webSocketData.setNodeData(key, value);
 
 			// send only over local connection (no broadcast)
 			getWebSocket().send(webSocketData, true);
@@ -59,7 +76,6 @@ public class GetProperty extends AbstractCommand {
 	}
 
 	//~--- get methods ----------------------------------------------------
-
 	@Override
 	public String getCommand() {
 		return "GET_PROPERTY";
