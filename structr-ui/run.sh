c995b732e79594e5df6349243846f12d0b4df928
Axel Morgner <axel@morgner.de>
2015-01-20 13:03:34 +0100

    Set JVM options -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false to avoid slow DNS lookup on networks not supporting IPv6

--- structr-ui/run.sh
+++ structr-ui/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 JAVA=`which java`
-STRUCTR="-Duser.timezone=Europe/Berlin -Duser.country=US -Duser.language=en -cp target/lib/*:target/structr-ui-1.1-SNAPSHOT-$(git rev-parse --short=5 HEAD).jar org.structr.Server"
+STRUCTR="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false -Duser.timezone=Europe/Berlin -Duser.country=US -Duser.language=en -cp target/lib/*:target/structr-ui-1.1-SNAPSHOT-$(git rev-parse --short=5 HEAD).jar org.structr.Server"
 STRUCTR_ARGS="-d64 -Xms1g -Xmx1g -XX:+UseG1GC -XX:MaxPermSize=128m -XX:+UseNUMA -Dinstance=your_instance_name"
 
 STRUCTR_CONF=`find . -name structr.conf`
