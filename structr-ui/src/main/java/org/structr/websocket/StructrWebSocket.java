structr/structr

commit e47d3f2bf64d3201405ed9c118bb9dd2ee0abbd2

changeset for frontend websocket functionality
- add a configuration option "WebSocketServlet.frontendAccess" (disabled by default)
- changed node visibility on creation to check for the accessing user (otherwise it would be broadcast to all WS-listeners)
- changed WS broadcast message generation: a client only receives messages if it is allowed to access the object

structr-ui/src/main/java/org/structr/websocket/StructrWebSocket.java
@@ -31,6 +31,7 @@ import org.structr.common.AccessMode;
 import org.structr.common.SecurityContext;
 import org.structr.common.error.FrameworkException;
 import org.structr.core.GraphObject;
+import org.structr.core.Services;
 import org.structr.core.app.App;
 import org.structr.core.app.StructrApp;
 import org.structr.core.auth.AuthHelper;
@@ -452,7 +453,8 @@ public class StructrWebSocket implements WebSocketListener {
 	public boolean isAuthenticated() {
 
 		final Principal user = getCurrentUser();
-		return (user != null && (user.getProperty(Principal.isAdmin) || user.getProperty(User.backendUser)));
+		final boolean allowWSInFrontend = Boolean.parseBoolean(StructrApp.getConfigurationValue(Services.WEBSOCKET_FRONTEND_ACCESS, "false"));
+		return (user != null && (allowWSInFrontend || user.getProperty(Principal.isAdmin) || user.getProperty(User.backendUser)));
 
 	}
 

structr-ui/src/main/java/org/structr/websocket/WebsocketController.java
@@ -102,20 +102,20 @@ public class WebsocketController implements StructrTransactionListener {
 
 			webSocketData.setCallback(socket.getCallback());
 
-			if ((session != null)) { //&& socket.isAuthenticated()) {
+			if (session != null && socket.isAuthenticated()) {
 
 				List<? extends GraphObject> result = webSocketData.getResult();
+				SecurityContext securityContext = socket.getSecurityContext();
+
+				// if the client has no access to the node then skip sending a message to it
+				if (!securityContext.isVisible((AbstractNode) webSocketData.getGraphObject())) {
+					continue;
+				}
 
 				if ((result != null) && (result.size() > 0)
 					&& (webSocketData.getCommand().equals("UPDATE") || webSocketData.getCommand().equals("ADD") || webSocketData.getCommand().equals("CREATE"))) {
 
 					WebSocketMessage clientData = webSocketData.copy();
-					SecurityContext securityContext = socket.getSecurityContext();
-
-					// For non-authenticated clients, construct a security context without user
-					if (securityContext == null) {
-						securityContext = SecurityContext.getInstance(null, AccessMode.Frontend);
-					}
 
 					clientData.setResult(filter(securityContext, result));
 

structr-core/src/main/java/org/structr/core/Services.java
@@ -107,6 +107,7 @@ public class Services {
 	public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "access.control.allow.credentials";
 	public static final String ACCESS_CONTROL_EXPOSE_HEADERS    = "access.control.expose.headers";
 	public static final String APPLICATION_SESSION_TIMEOUT      = "application.session.timeout";
+	public static final String WEBSOCKET_FRONTEND_ACCESS        = "WebSocketServlet.frontendAccess";
 
 	// singleton instance
 	private static int globalSessionTimeout            = -1;

structr-core/src/main/java/org/structr/core/entity/AbstractNode.java
@@ -745,7 +745,7 @@ public abstract class AbstractNode implements NodeInterface, AccessControllable 
 	private boolean isGranted(final Permission permission, final Principal accessingUser) {
 
 		// use quick checks for maximum performance
-		if (isCreation) {
+		if (isCreation && (accessingUser == null || accessingUser.equals(getOwnerNode()) ) ) {
 			
 			return true;
 		}

structr-ui/structr.conf_templ
@@ -140,6 +140,9 @@ WebSocketServlet.resourceprovider = org.structr.web.common.UiResourceProvider
 WebSocketServlet.user.autocreate = false
 WebSocketServlet.user.class = org.structr.web.entity.User
 
+# Uncomment this if you want to use WebSocket functionality for frontend users
+# WebSocketServlet.frontendAccess = true
+
 # CSV servlet
 CsvServlet.authenticator = org.structr.web.auth.UiAuthenticator
 CsvServlet.class = org.structr.rest.servlet.CsvServlet

commit 0d9f801321676c0e0701d32cd231ba4d4da068e7

fixed the ws-functionality i broke in e47d3f2bf64d3201405ed9c118bb9dd2ee0abbd2
- fixed how visibility of changes is determined (this was broken)
- split isAuthenticated in two methods in order to check if the user is priviledged

structr-ui/src/main/java/org/structr/websocket/StructrWebSocket.java
@@ -453,8 +453,19 @@ public class StructrWebSocket implements WebSocketListener {
 	public boolean isAuthenticated() {
 
 		final Principal user = getCurrentUser();
-		final boolean allowWSInFrontend = Boolean.parseBoolean(StructrApp.getConfigurationValue(Services.WEBSOCKET_FRONTEND_ACCESS, "false"));
-		return (user != null && (allowWSInFrontend || user.getProperty(Principal.isAdmin) || user.getProperty(User.backendUser)));
+		return (user != null && (isPriviledgedUser(user) || isFrontendWebsocketAccessEnabled()));
+
+	}
+
+	public boolean isPriviledgedUser(Principal user) {
+
+		return (user != null && (user.getProperty(Principal.isAdmin) || user.getProperty(User.backendUser)));
+
+	}
+
+	public boolean isFrontendWebsocketAccessEnabled() {
+
+		return Boolean.parseBoolean(StructrApp.getConfigurationValue(Services.WEBSOCKET_FRONTEND_ACCESS, "false"));
 
 	}
 

structr-ui/src/main/java/org/structr/websocket/WebsocketController.java
@@ -26,6 +26,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.lang3.Range;
 import org.eclipse.jetty.util.ConcurrentHashSet;
 import org.eclipse.jetty.util.URIUtil;
 import org.eclipse.jetty.websocket.api.Session;
@@ -107,8 +108,14 @@ public class WebsocketController implements StructrTransactionListener {
 				List<? extends GraphObject> result = webSocketData.getResult();
 				SecurityContext securityContext = socket.getSecurityContext();
 
-				// if the client has no access to the node then skip sending a message to it
-				if (!securityContext.isVisible((AbstractNode) webSocketData.getGraphObject())) {
+				// if the object IS NOT of type AbstractNode AND the client is NOT priviledged
+				// OR
+				// if the object IS of type AbstractNode AND the client has no access to the node
+				// THEN skip sending a message
+				if (
+						( !(webSocketData.getGraphObject() instanceof AbstractNode) && !socket.isPriviledgedUser(socket.getCurrentUser()) )
+						|| (webSocketData.getGraphObject() instanceof AbstractNode && !securityContext.isVisible((AbstractNode) webSocketData.getGraphObject()))
+					) {
 					continue;
 				}
 