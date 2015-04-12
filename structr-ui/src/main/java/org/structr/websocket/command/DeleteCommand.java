b9e535f937d1f7034f5501e043a058fc06720085
Axel Morgner <axel@morgner.de>
2015-04-12 22:49:52 +0200

    Check if user has right to delete a node.

--- structr-ui/src/main/java/org/structr/websocket/command/DeleteCommand.java
+++ structr-ui/src/main/java/org/structr/websocket/command/DeleteCommand.java
@@ -3,18 +3,18 @@
  *
  * This file is part of Structr <http://structr.org>.
  *
- * Structr is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * Structr is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  *
- * Structr is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * Structr is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Structr. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.structr.websocket.command;
 
@@ -22,6 +22,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.structr.common.Permission;
 import org.structr.common.SecurityContext;
 import org.structr.common.error.FrameworkException;
 import org.structr.core.app.App;
@@ -29,6 +30,7 @@ import org.structr.core.app.StructrApp;
 import org.structr.core.entity.AbstractNode;
 import org.structr.core.entity.LinkedTreeNode;
 import org.structr.core.graph.NodeInterface;
+import org.structr.core.graph.Tx;
 import org.structr.web.entity.dom.DOMNode;
 import org.structr.websocket.StructrWebSocket;
 import org.structr.websocket.message.MessageBuilder;
@@ -57,16 +59,31 @@ public class DeleteCommand extends AbstractCommand {
 		final SecurityContext securityContext = getWebSocket().getSecurityContext();
 
 		final Boolean recursive = (Boolean) webSocketData.getNodeData().get("recursive");
-		final AbstractNode obj  = getNode(webSocketData.getId());
+		final AbstractNode obj = getNode(webSocketData.getId());
 
 		if (obj != null) {
 
 			final App app = StructrApp.getInstance(securityContext);
 
-			if (Boolean.TRUE.equals(recursive)) {
-				
-				// Remove all child nodes first
+			try (final Tx tx = app.tx()) {
 
+				if (!(obj.isGranted(Permission.delete, getWebSocket().getSecurityContext()))) {
+
+					logger.log(Level.WARNING, "No delete permission for {0} on {1}", new Object[]{getWebSocket().getCurrentUser().toString(), obj.toString()});
+					getWebSocket().send(MessageBuilder.status().message("No delete permission").code(400).build(), true);
+					tx.success();
+
+					return;
+
+				}
+
+			} catch (FrameworkException ex) {
+				ex.printStackTrace();
+			}
+
+			if (Boolean.TRUE.equals(recursive)) {
+
+				// Remove all child nodes first
 				try {
 
 					final List<AbstractNode> filteredResults = new LinkedList();
@@ -77,11 +94,11 @@ public class DeleteCommand extends AbstractCommand {
 						filteredResults.addAll(DOMNode.getAllChildNodes(node));
 
 					} else if (obj instanceof LinkedTreeNode) {
-						
+
 						LinkedTreeNode node = (LinkedTreeNode) obj;
 
 						filteredResults.addAll(node.getAllChildNodes());
-						
+
 					}
 
 					for (NodeInterface node : filteredResults) {
@@ -92,7 +109,7 @@ public class DeleteCommand extends AbstractCommand {
 
 					logger.log(Level.WARNING, "Exception occured", fex);
 					getWebSocket().send(MessageBuilder.status().code(fex.getStatus()).message(fex.getMessage()).build(), true);
-	
+
 				} catch (DOMException dex) {
 
 					logger.log(Level.WARNING, "DOMException occured.", dex);
