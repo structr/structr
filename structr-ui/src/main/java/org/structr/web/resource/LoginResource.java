bc1a6e7762bb103870481313727d7898efc89a17
Axel Morgner <axel@morgner.de>
2015-04-16 14:43:31 +0200

    Let LoginResource support doOptions method

--- structr-ui/src/main/java/org/structr/web/resource/LoginResource.java
+++ structr-ui/src/main/java/org/structr/web/resource/LoginResource.java
@@ -118,13 +118,6 @@ public class LoginResource extends Resource {
 	}
 
 	@Override
-	public RestMethodResult doOptions() throws FrameworkException {
-
-		throw new IllegalMethodException();
-
-	}
-
-	@Override
 	public Resource tryCombineWith(Resource next) throws FrameworkException {
 
 		return null;
