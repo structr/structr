61a5ebd0c02a5853a72fab81cee482155dd79f84
vigorouscoding <kai@vigorouscoding.com>
2015-07-07 17:32:41 +0200

    Change the way "children" of a node are resolved. Looking up the closest template is unnecessary and is now skipped completely

--- structr-ui/src/main/java/org/structr/web/common/RenderContext.java
+++ structr-ui/src/main/java/org/structr/web/common/RenderContext.java
@@ -408,11 +408,8 @@ public class RenderContext extends ActionContext {
 
 						if (entity instanceof DOMNode) {
 
-							final Template template = ((DOMNode) entity).getClosestTemplate(getPage());
-							if (template != null) {
+							return ((DOMNode) entity).getChildNodes();
 
-								return template.getChildNodes();
-							}
 						}
 						break;
 
