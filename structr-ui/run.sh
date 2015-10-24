86642e7ef519729d3a4add809e50dbf5720237db
vigorouscoding <kai@vigorouscoding.com>
2015-10-23 19:33:06 -0700

    removed deprecated option MaxPermsize which results in a warning in Java 8 and an error in Java 9

--- structr-ui/run.sh
+++ structr-ui/run.sh
@@ -17,7 +17,7 @@ JAVA=`which java`
 LATEST=`ls target/structr-ui-*.jar | grep -v 'sources.jar' | grep -v 'javadoc.jar' | sort | tail -1`
 VERSION=${LATEST#target/structr-ui-};VERSION=${VERSION%%.jar}
 STRUCTR="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false -Duser.timezone=Europe/Berlin -Duser.country=US -Duser.language=en -cp target/lib/*:$LATEST org.structr.Server"
-STRUCTR_ARGS="-server -d64 -Xms${HEAPSIZE}g -Xmx${HEAPSIZE}g -XX:MaxPermSize=128m -XX:+UseNUMA -XX:+UseConcMarkSweepGC -Dinstance=$NAME"
+STRUCTR_ARGS="-server -d64 -Xms${HEAPSIZE}g -Xmx${HEAPSIZE}g -XX:+UseNUMA -XX:+UseConcMarkSweepGC -Dinstance=$NAME"
 
 PIDFILE=$BASE_DIR/structrdb-$NAME.pid
 LOGS_DIR=$BASE_DIR/logs
