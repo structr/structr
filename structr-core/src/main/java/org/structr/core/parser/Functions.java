f630fef1e7129e1856362925e5af0cde0ea4d064
Christian Morgner <christian@morgner.de>
2015-12-04 11:21:17 +0100

    Added merge_unique() builtin function that removes duplicates from merged sources, added tests.

--- structr-core/src/main/java/org/structr/core/parser/Functions.java
+++ structr-core/src/main/java/org/structr/core/parser/Functions.java
@@ -159,6 +159,7 @@ public class Functions {
 	public static final String ERROR_MESSAGE_EXTRACT                             = "Usage: ${extract(list, propertyName)}. Example: ${extract(this.children, \"amount\")}";
 	public static final String ERROR_MESSAGE_FILTER                              = "Usage: ${filter(list, expression)}. Example: ${filter(this.children, gt(size(data.children), 0))}";
 	public static final String ERROR_MESSAGE_MERGE                               = "Usage: ${merge(list1, list2, list3, ...)}. Example: ${merge(this.children, this.siblings)}";
+	public static final String ERROR_MESSAGE_MERGE_UNIQUE                        = "Usage: ${merge_unique(list1, list2, list3, ...)}. Example: ${merge_unique(this.children, this.siblings)}";
 	public static final String ERROR_MESSAGE_COMPLEMENT                          = "Usage: ${complement(list1, list2, list3, ...)}. (The resulting list contains no duplicates) Example: ${complement(allUsers, me)} => List of all users except myself";
 	public static final String ERROR_MESSAGE_UNWIND                              = "Usage: ${unwind(list1, ...)}. Example: ${unwind(this.children)}";
 	public static final String ERROR_MESSAGE_SORT                                = "Usage: ${sort(list1, key [, true])}. Example: ${sort(this.children, \"name\")}";
@@ -1745,6 +1746,50 @@ public class Functions {
 			@Override
 			public String shortDescription() {
 				return "Merges the given collections / objects into a single collection";
+			}
+
+		});
+		functions.put("merge_unique", new Function<Object, Object>() {
+
+			@Override
+			public String getName() {
+				return "merge_unique()";
+			}
+
+			@Override
+			public Object apply(final ActionContext ctx, final GraphObject entity, final Object[] sources) throws FrameworkException {
+
+				final Set result = new LinkedHashSet<>();
+				for (final Object source : sources) {
+
+					if (source instanceof Collection) {
+
+						// filter null objects
+						for (Object obj : (Collection)source) {
+
+							if (obj != null) {
+
+								result.add(obj);
+							}
+						}
+
+					} else if (source != null) {
+
+						result.add(source);
+					}
+				}
+
+				return new LinkedList<>(result);
+			}
+
+			@Override
+			public String usage(boolean inJavaScriptContext) {
+				return ERROR_MESSAGE_MERGE_UNIQUE;
+			}
+
+			@Override
+			public String shortDescription() {
+				return "Merges the given collections / objects into a single collection, removing duplicates";
 			}
 
 		});
