90c25096bcc19ecc586c992c699dc92e194cc118
Christian Morgner <christian@morgner.de>
2016-01-11 14:48:26 +0100

    Added concurrency test.

--- structr-core/src/test/java/org/structr/common/TransactionTest.java
+++ structr-core/src/test/java/org/structr/common/TransactionTest.java
@@ -1,9 +1,22 @@
 package org.structr.common;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.fail;
+import org.apache.commons.lang3.StringUtils;
 import org.structr.common.error.FrameworkException;
+import org.structr.core.app.StructrApp;
+import org.structr.core.entity.AbstractNode;
+import org.structr.core.entity.SchemaNode;
+import org.structr.core.entity.SchemaProperty;
 import org.structr.core.entity.TestOne;
+import org.structr.core.graph.NodeAttribute;
+import org.structr.core.graph.NodeInterface;
 import org.structr.core.graph.Tx;
 import org.structr.core.script.Scripting;
 import org.structr.schema.action.ActionContext;
@@ -65,4 +78,103 @@ public class TransactionTest extends StructrTest {
 		}
 	}
 
+	public void testConstraintsConcurrently() {
+
+		/**
+		 * This test concurrently creates 1000 nodes in
+		 * batches of 10, with 10 threads simultaneously.
+		 */
+
+		try (final Tx tx = app.tx()) {
+
+			app.create(SchemaNode.class,
+				new NodeAttribute(SchemaNode.name, "Item"),
+				new NodeAttribute(SchemaNode.schemaProperties,
+					Arrays.asList(app.create(
+						SchemaProperty.class,
+						new NodeAttribute(SchemaProperty.name, "name"),
+						new NodeAttribute(SchemaProperty.propertyType, "String"),
+						new NodeAttribute(SchemaProperty.unique, true),
+						new NodeAttribute(SchemaProperty.indexed, true)
+					)
+				))
+			);
+
+			tx.success();
+
+		} catch (FrameworkException ex) {
+			fail("Error creating schema node");
+		}
+
+		final Class itemType = StructrApp.getConfiguration().getNodeEntityClass("Item");
+
+		assertNotNull("Error creating schema node", itemType);
+
+		final Runnable worker = new Runnable() {
+
+			@Override
+			public void run() {
+
+				int i = 0;
+
+				while (i < 1000) {
+
+					try (final Tx tx = app.tx()) {
+
+						for (int j=0; j<10 && i<1000; j++) {
+
+							app.create(itemType, "Item" + StringUtils.leftPad(Integer.toString(i++), 5, "0"));
+						}
+
+						tx.success();
+
+					} catch (FrameworkException expected) {
+					}
+				}
+			}
+		};
+
+		final ExecutorService service = Executors.newFixedThreadPool(10);
+		final List<Future> futures    = new LinkedList<>();
+
+		for (int i=0; i<10; i++) {
+
+			futures.add(service.submit(worker));
+		}
+
+		// wait for result of async. operations
+		for (final Future future : futures) {
+
+			try {
+				future.get();
+
+			} catch (Throwable t) {
+
+				t.printStackTrace();
+				fail("Unexpected exception");
+			}
+		}
+
+
+		try (final Tx tx = app.tx()) {
+
+			final List<NodeInterface> items = app.nodeQuery(itemType).sort(AbstractNode.name).getAsList();
+			int i                           = 0;
+
+			assertEquals("Invalid concurrent constraint test result", 1000, items.size());
+
+			for (final NodeInterface item : items) {
+
+				assertEquals("Invalid name detected", "Item" + StringUtils.leftPad(Integer.toString(i++), 5, "0"), item.getName());
+			}
+
+			tx.success();
+
+		} catch (FrameworkException ex) {
+			fail("Unexpected exception");
+		}
+
+
+
+	}
 }
