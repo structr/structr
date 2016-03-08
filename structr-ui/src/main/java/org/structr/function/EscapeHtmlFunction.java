392f35bc152e878234f9ba6fbc34b2331218a208
Christian Morgner <christian@morgner.de>
2016-03-08 17:46:01 +0100

    Fixed wrong license header in EscapeHtmlFunction.

--- structr-ui/src/main/java/org/structr/function/EscapeHtmlFunction.java
+++ structr-ui/src/main/java/org/structr/function/EscapeHtmlFunction.java
@@ -4,16 +4,16 @@
  * This file is part of Structr <http://structr.org>.
  *
  * Structr is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
+ * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
  * Structr is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.structr.function;
@@ -29,7 +29,8 @@ import org.structr.web.entity.dom.DOMNode;
  */
 public class EscapeHtmlFunction extends Function<Object, Object> {
 
-	public static final String ERROR_MESSAGE_ESCAPE_JSON = "Usage: ${escape_html(string)}. Example: ${escape_html(this.name)}";
+	public static final String ERROR_MESSAGE_ESCAPE_HTML    = "Usage: ${escape_html(text)}. Example: ${escape_html(\"test & test\")}";
+	public static final String ERROR_MESSAGE_ESCAPE_HTML_JS = "Usage: ${{Structr.escape_html(text)}}. Example: ${{Structr.escape_html(\"test & test\")}}";
 
 	@Override
 	public String getName() {
@@ -42,16 +43,18 @@ public class EscapeHtmlFunction extends Function<Object, Object> {
 		return (arrayHasMinLengthAndAllElementsNotNull(sources, 1))
 			? DOMNode.escapeForHtmlAttributes(sources[0].toString())
 			: "";
+
 	}
 
 	@Override
 	public String usage(boolean inJavaScriptContext) {
-		return ERROR_MESSAGE_ESCAPE_JSON;
+		return (inJavaScriptContext ? ERROR_MESSAGE_ESCAPE_HTML_JS : ERROR_MESSAGE_ESCAPE_HTML);
 	}
 
 	@Override
 	public String shortDescription() {
-		return "Escapes HTML entities within the given string.";
+		return "";
 	}
+
 
 }
