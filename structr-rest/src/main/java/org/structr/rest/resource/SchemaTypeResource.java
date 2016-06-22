structr/structr

commit 5d839e4236ffc6f9bcc36120b2c00a610c77821a

Moved code to retrieve type info from the schema to a static method to be able to use it in a WebSocket command.

structr-rest/src/main/java/org/structr/rest/resource/SchemaTypeResource.java
@@ -50,10 +50,10 @@ import org.structr.schema.SchemaHelper;
 public class SchemaTypeResource extends Resource {
 
 	protected Class entityClass = null;
-	protected String rawType = null;
+	private static String           rawType = null;
 	protected HttpServletRequest request = null;
 	protected TypeResource typeResource = null;
-	private String propertyView = null;
+	private static           String propertyView = null;
 
 	//~--- methods --------------------------------------------------------
 	public SchemaTypeResource(SecurityContext securityContext, TypeResource typeResource) {
@@ -71,58 +71,8 @@ public class SchemaTypeResource extends Resource {
 
 	@Override
 	public Result doGet(PropertyKey sortKey, boolean sortDescending, int pageSize, int page, String offsetId) throws FrameworkException {
-
-		List<GraphObjectMap> resultList = new LinkedList<>();
-
-		// create & add schema information
 		Class type = typeResource.getEntityClass();
-		if (type != null) {
-
-			if (propertyView != null) {
-
-				for (final Map.Entry<String, Object> entry : getPropertiesForView(type, propertyView).entrySet()) {
-
-					final GraphObjectMap property = new GraphObjectMap();
-
-					for (final Map.Entry<String, Object> prop : ((Map<String, Object>) entry.getValue()).entrySet()) {
-
-						property.setProperty(new GenericProperty(prop.getKey()), prop.getValue());
-					}
-
-					resultList.add(property);
-				}
-
-			} else {
-
-				final GraphObjectMap schema = new GraphObjectMap();
-
-				resultList.add(schema);
-
-				String url = "/".concat(rawType);
-
-				schema.setProperty(new StringProperty("url"), url);
-				schema.setProperty(new StringProperty("type"), type.getSimpleName());
-				schema.setProperty(new StringProperty("className"), type.getName());
-				schema.setProperty(new BooleanProperty("isRel"), AbstractRelationship.class.isAssignableFrom(type));
-				schema.setProperty(new LongProperty("flags"), SecurityContext.getResourceFlags(rawType));
-
-				Set<String> propertyViews = new LinkedHashSet<>(StructrApp.getConfiguration().getPropertyViews());
-
-				// list property sets for all views
-				Map<String, Map<String, Object>> views = new TreeMap();
-				schema.setProperty(new GenericProperty("views"), views);
-
-				for (String view : propertyViews) {
-
-					if (!View.INTERNAL_GRAPH_VIEW.equals(view)) {
-
-						views.put(view, getPropertiesForView(type, view));
-					}
-				}
-			}
-		}
-
-		return new Result(resultList, resultList.size(), false, false);
+		return getSchemaTypeResult(securityContext, type);
 
 	}
 
@@ -177,9 +127,9 @@ public class SchemaTypeResource extends Resource {
 
 	}
 
-	private Map<String, Object> getPropertiesForView(final Class type, final String view) throws FrameworkException {
+	private static Map<String, Object> getPropertiesForView(final SecurityContext securityContext, final Class type) throws FrameworkException {
 
-		final Set<PropertyKey> properties = new LinkedHashSet<>(StructrApp.getConfiguration().getPropertySet(type, view));
+		final Set<PropertyKey> properties = new LinkedHashSet<>(StructrApp.getConfiguration().getPropertySet(type, propertyView));
 		final Map<String, Object> propertyConverterMap = new LinkedHashMap<>();
 
 		for (PropertyKey property : properties) {
@@ -189,4 +139,61 @@ public class SchemaTypeResource extends Resource {
 
 		return propertyConverterMap;
 	}
+	
+	// ----- public static methods -----
+	public static Result getSchemaTypeResult(final SecurityContext securityContext, final Class type) throws FrameworkException {
+	
+		List<GraphObjectMap> resultList = new LinkedList<>();
+
+		// create & add schema information
+		
+		if (type != null) {
+
+			if (propertyView != null) {
+
+				for (final Map.Entry<String, Object> entry : getPropertiesForView(securityContext, type).entrySet()) {
+
+					final GraphObjectMap property = new GraphObjectMap();
+
+					for (final Map.Entry<String, Object> prop : ((Map<String, Object>) entry.getValue()).entrySet()) {
+
+						property.setProperty(new GenericProperty(prop.getKey()), prop.getValue());
+					}
+
+					resultList.add(property);
+				}
+
+			} else {
+
+				final GraphObjectMap schema = new GraphObjectMap();
+
+				resultList.add(schema);
+
+				String url = "/".concat(rawType);
+
+				schema.setProperty(new StringProperty("url"), url);
+				schema.setProperty(new StringProperty("type"), type.getSimpleName());
+				schema.setProperty(new StringProperty("className"), type.getName());
+				schema.setProperty(new BooleanProperty("isRel"), AbstractRelationship.class.isAssignableFrom(type));
+				schema.setProperty(new LongProperty("flags"), SecurityContext.getResourceFlags(rawType));
+
+				Set<String> propertyViews = new LinkedHashSet<>(StructrApp.getConfiguration().getPropertyViewsForType(type));
+
+				// list property sets for all views
+				Map<String, Map<String, Object>> views = new TreeMap();
+				schema.setProperty(new GenericProperty("views"), views);
+
+				for (String view : propertyViews) {
+
+					if (!View.INTERNAL_GRAPH_VIEW.equals(view)) {
+
+						views.put(view, getPropertiesForView(securityContext, type));
+					}
+				}
+			}
+		}
+
+		return new Result(resultList, resultList.size(), false, false);
+	}
+	
 }

structr-ui/src/main/java/org/structr/websocket/command/GetSchemaInfoCommand.java
@@ -21,7 +21,9 @@ package org.structr.websocket.command;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.structr.common.error.FrameworkException;
+import org.structr.core.app.StructrApp;
 import org.structr.rest.resource.SchemaResource;
+import org.structr.rest.resource.SchemaTypeResource;
 import org.structr.websocket.StructrWebSocket;
 import org.structr.websocket.message.MessageBuilder;
 import org.structr.websocket.message.WebSocketMessage;
@@ -47,7 +49,18 @@ public class GetSchemaInfoCommand extends AbstractCommand {
 
 		try {
 
-			webSocketData.setResult(SchemaResource.getSchemaOverviewResult().getResults());
+			final String type = (String) webSocketData.getNodeData().get("type");
+
+			if (type != null) {
+				
+				final Class typeClass = StructrApp.getConfiguration().getNodeEntityClass(type);
+				
+				webSocketData.setResult(SchemaTypeResource.getSchemaTypeResult(getWebSocket().getSecurityContext(), typeClass).getResults());
+				
+			} else {
+
+				webSocketData.setResult(SchemaResource.getSchemaOverviewResult().getResults());
+			}
 
 			// send only over local connection (no broadcast)
 			getWebSocket().send(webSocketData, true);

commit 6cf331884bd3c8c9176082eb4ac5383cad735b6b

Fixed JSON output of SchemaTypeResource I broke with commit 5d839e4.

structr-rest/src/main/java/org/structr/rest/resource/SchemaTypeResource.java
@@ -49,11 +49,11 @@ import org.structr.schema.SchemaHelper;
  */
 public class SchemaTypeResource extends Resource {
 
-	protected Class entityClass = null;
-	private static String           rawType = null;
+	protected Class entityClass          = null;
+	private static String        rawType = null;
 	protected HttpServletRequest request = null;
-	protected TypeResource typeResource = null;
-	private static           String propertyView = null;
+	protected TypeResource typeResource  = null;
+	private   String propertyView        = null;
 
 	//~--- methods --------------------------------------------------------
 	public SchemaTypeResource(SecurityContext securityContext, TypeResource typeResource) {
@@ -72,7 +72,7 @@ public class SchemaTypeResource extends Resource {
 	@Override
 	public Result doGet(PropertyKey sortKey, boolean sortDescending, int pageSize, int page, String offsetId) throws FrameworkException {
 		Class type = typeResource.getEntityClass();
-		return getSchemaTypeResult(securityContext, type);
+		return getSchemaTypeResult(securityContext, type, propertyView);
 
 	}
 
@@ -127,7 +127,7 @@ public class SchemaTypeResource extends Resource {
 
 	}
 
-	private static Map<String, Object> getPropertiesForView(final SecurityContext securityContext, final Class type) throws FrameworkException {
+	private static Map<String, Object> getPropertiesForView(final SecurityContext securityContext, final Class type, final String propertyView) throws FrameworkException {
 
 		final Set<PropertyKey> properties = new LinkedHashSet<>(StructrApp.getConfiguration().getPropertySet(type, propertyView));
 		final Map<String, Object> propertyConverterMap = new LinkedHashMap<>();
@@ -141,7 +141,7 @@ public class SchemaTypeResource extends Resource {
 	}
 	
 	// ----- public static methods -----
-	public static Result getSchemaTypeResult(final SecurityContext securityContext, final Class type) throws FrameworkException {
+	public static Result getSchemaTypeResult(final SecurityContext securityContext, final Class type, final String propertyView) throws FrameworkException {
 	
 		List<GraphObjectMap> resultList = new LinkedList<>();
 
@@ -151,7 +151,7 @@ public class SchemaTypeResource extends Resource {
 
 			if (propertyView != null) {
 
-				for (final Map.Entry<String, Object> entry : getPropertiesForView(securityContext, type).entrySet()) {
+				for (final Map.Entry<String, Object> entry : getPropertiesForView(securityContext, type, propertyView).entrySet()) {
 
 					final GraphObjectMap property = new GraphObjectMap();
 
@@ -183,11 +183,11 @@ public class SchemaTypeResource extends Resource {
 				Map<String, Map<String, Object>> views = new TreeMap();
 				schema.setProperty(new GenericProperty("views"), views);
 
-				for (String view : propertyViews) {
+				for (final String view : propertyViews) {
 
 					if (!View.INTERNAL_GRAPH_VIEW.equals(view)) {
 
-						views.put(view, getPropertiesForView(securityContext, type));
+						views.put(view, getPropertiesForView(securityContext, type, view));
 					}
 				}
 			}

structr-ui/src/main/java/org/structr/websocket/command/GetSchemaInfoCommand.java
@@ -20,6 +20,7 @@ package org.structr.websocket.command;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.structr.common.PropertyView;
 import org.structr.common.error.FrameworkException;
 import org.structr.core.app.StructrApp;
 import org.structr.rest.resource.SchemaResource;
@@ -55,7 +56,7 @@ public class GetSchemaInfoCommand extends AbstractCommand {
 				
 				final Class typeClass = StructrApp.getConfiguration().getNodeEntityClass(type);
 				
-				webSocketData.setResult(SchemaTypeResource.getSchemaTypeResult(getWebSocket().getSecurityContext(), typeClass).getResults());
+				webSocketData.setResult(SchemaTypeResource.getSchemaTypeResult(getWebSocket().getSecurityContext(), typeClass, PropertyView.Ui).getResults());
 				
 			} else {
 