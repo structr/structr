abfebf57b7533263c2ab3568dc6105101e745fde
vigorouscoding <kai@vigorouscoding.com>
2016-08-25 12:18:00 +0200

    nicer errors/warnings for javascript minification. source is not supplied as one string but instead as the files themselves

--- structr-ui/src/main/java/org/structr/web/entity/MinifiedJavaScriptFile.java
+++ structr-ui/src/main/java/org/structr/web/entity/MinifiedJavaScriptFile.java
@@ -18,15 +18,16 @@
  */
 package org.structr.web.entity;
 
-import com.google.common.collect.ImmutableList;
 import com.google.javascript.jscomp.CommandLineRunner;
 import com.google.javascript.jscomp.CompilationLevel;
 import com.google.javascript.jscomp.Compiler;
 import com.google.javascript.jscomp.CompilerOptions;
-import com.google.javascript.jscomp.JSError;
 import com.google.javascript.jscomp.SourceFile;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Logger;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.structr.common.PropertyView;
 import org.structr.common.View;
 import org.structr.common.error.FrameworkException;
@@ -34,14 +35,15 @@ import org.structr.core.property.EnumProperty;
 import org.structr.core.property.Property;
 import org.structr.core.property.StringProperty;
 import org.structr.web.common.FileHelper;
+import org.structr.web.entity.relation.MinificationNeighbor;
 
 public class MinifiedJavaScriptFile extends AbstractMinifiedFile {
 
 	private static final Logger logger = Logger.getLogger(MinifiedJavaScriptFile.class.getName());
 
 	public static final Property<CompilationLevel> optimizationLevel = new EnumProperty<>("optimizationLevel", CompilationLevel.class, CompilationLevel.WHITESPACE_ONLY);
-	public static final Property<String> warnings = new StringProperty("warnings");
-	public static final Property<String> errors = new StringProperty("errors");
+	public static final Property<String> warnings                    = new StringProperty("warnings");
+	public static final Property<String> errors                      = new StringProperty("errors");
 
 	public static final View defaultView = new View(MinifiedJavaScriptFile.class, PropertyView.Public, minificationSources, optimizationLevel, warnings, errors);
 	public static final View uiView      = new View(MinifiedJavaScriptFile.class, PropertyView.Ui, minificationSources, optimizationLevel, warnings, errors);
@@ -49,32 +51,36 @@ public class MinifiedJavaScriptFile extends AbstractMinifiedFile {
 	@Override
 	public void minify() throws FrameworkException, IOException {
 
-		final String filename = getProperty(name);
 		final Compiler compiler = new Compiler();
 		final CompilerOptions options = new CompilerOptions();
-		final SourceFile input = SourceFile.fromCode(filename, getConcatenatedSource());
 		final CompilationLevel selectedLevel = getProperty(optimizationLevel);
-
 		selectedLevel.setOptionsForCompilationLevel(options);
 
-		compiler.compile(CommandLineRunner.getBuiltinExterns(options), ImmutableList.of(input), options);
+		compiler.compile(CommandLineRunner.getBuiltinExterns(options), getSourceFileList(), options);
 
 		FileHelper.setFileData(this, compiler.toSource().getBytes(), null);
 
-		// document warnings
-		final StringBuilder warningsSB = new StringBuilder();
-		for (JSError warning : compiler.getWarnings()) {
-			warningsSB.append(warning.toString());
-		}
-		setProperty(warnings, warningsSB.toString());
+		final String separator = System.lineSeparator().concat("----").concat(System.lineSeparator());
 
-		// document errors
-		final StringBuilder errorsSB = new StringBuilder();
-		for (JSError error : compiler.getErrors()) {
-			errorsSB.append(error.toString());
-		}
-		setProperty(errors, errorsSB.toString());
-
+		setProperty(warnings, StringUtils.join(compiler.getWarnings(), separator));
+		setProperty(errors, StringUtils.join(compiler.getErrors(), separator));
 	}
 
+	private ArrayList<SourceFile> getSourceFileList() throws FrameworkException, IOException {
+
+		ArrayList<SourceFile> sourceList = new ArrayList();
+
+		int cnt = 0;
+		for (MinificationNeighbor rel : getSortedRelationships()) {
+
+			final FileBase src = rel.getTargetNode();
+
+			sourceList.add(SourceFile.fromCode(src.getProperty(FileBase.name), FileUtils.readFileToString(src.getFileOnDisk())));
+
+			// compact the relationships (if necessary)
+			rel.setProperty(MinificationNeighbor.position, cnt++);
+		}
+
+		return sourceList;
+	}
 }
