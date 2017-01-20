structr/structr

commit 7df96df5d6e622cfa9936be1c5796173cbdf9feb

Fixed left slideout drag mechanism and generalized a lot of code. Also cleaned up quite a bit.

structr-ui/src/main/resources/structr/js/crud.js
@@ -166,10 +166,9 @@ var _Crud = {
 		left = left || LSWrapper.getItem(crudResizerLeftKey) || 210;
 		$('.column-resizer', main).css({ left: left });
 
-		var w = $(window).width();
 		$('#crud-types').css({width: left - 12 + 'px'});
 		$('#crud-recent-types').css({width: left - 12 + 'px'});
-		$('#crud-right').css({left: left - 222 + 'px', width: w - left - 58 + 'px'});
+		$('#crud-right').css({left: left - 222 + 'px', width: $(window).width() - left - 58 + 'px'});
 	},
 	init: function() {
 
@@ -1491,7 +1490,7 @@ var _Crud = {
 		var fields = $('input', form);
 		form.attr('data-id', node.id);
 		$.each(fields, function(f, field) {
-			var value = formatValue(node[field.name], field.name, node.type, node.id);
+			var value = formatValue(node[field.name]);
 			$('input[name="' + field.name + '"]').val(value);
 		});
 	},
@@ -2282,45 +2281,8 @@ var _Crud = {
 			$.blockUI.defaults.applyPlatformOpacityRules = false;
 			$.blockUI.defaults.css.cursor = 'default';
 
-
-			var w = $(window).width();
-			var h = $(window).height();
-
-			var ml = 0;
-			var mt = 24;
-
-			// Calculate dimensions of dialog
-			var dw = Math.min(900, w - ml);
-			var dh = Math.min(600, h - mt);
-			//            var dw = (w-24) + 'px';
-			//            var dh = (h-24) + 'px';
-
-			var l = parseInt((w - dw) / 2);
-			var t = parseInt((h - dh) / 2);
-
-			$.blockUI({
-				fadeIn: 25,
-				fadeOut: 25,
-				message: dialogBox,
-				css: {
-					border: 'none',
-					backgroundColor: 'transparent',
-					width: dw + 'px',
-					height: dh + 'px',
-					top: t + 'px',
-					left: l + 'px'
-				},
-				themedCSS: {
-					width: dw + 'px',
-					height: dh + 'px',
-					top: t + 'px',
-					left: l + 'px'
-				},
-				width: dw + 'px',
-				height: dh + 'px',
-				top: t + 'px',
-				left: l + 'px'
-			});
+			var dimensions = Structr.getDialogDimensions(0, 24);
+			Structr.blockUI(dimensions);
 
 		}
 	},
@@ -2328,40 +2290,26 @@ var _Crud = {
 
 		Structr.resize();
 
-		var w = $(window).width();
-		var h = $(window).height();
-
-		var ml = 0;
-		var mt = 24;
-
-		// Calculate dimensions of dialog
-		var dw = Math.min(900, w - ml);
-		var dh = Math.min(600, h - mt);
-
-		var l = parseInt((w - dw) / 2);
-		var t = parseInt((h - dh) / 2);
+		var dimensions = Structr.getDialogDimensions(0, 24);
 
 		if (dialogBox && dialogBox.is(':visible')) {
 
 			$('.blockPage').css({
-				width: dw + 'px',
-				height: dh + 'px',
-				top: t + 'px',
-				left: l + 'px'
+				width: dimensions.width + 'px',
+				height: dimensions.height + 'px',
+				top: dimensions.top + 'px',
+				left: dimensions.left + 'px'
+			});
+
+			$('#dialogBox .dialogTextWrapper').css({
+				width: (dimensions.width - 28) + 'px',
+				height: (dimensions.height - 106) + 'px'
 			});
 
 		}
 
-		var bw = (dw - 28) + 'px';
-		var bh = (dh - 106) + 'px';
-
-		$('#dialogBox .dialogTextWrapper').css({
-			width: bw,
-			height: bh
-		});
-
 		$('.searchResults').css({
-			height: h - 103 + 'px'
+			height: ($(window).height() - 103) + 'px'
 		});
 
 		_Crud.moveResizer();

structr-ui/src/main/resources/structr/css/main.css
@@ -1113,11 +1113,6 @@ button i {
     vertical-align: middle;
 }
 
-#json_input {
-    width: 1000px;
-    height: 500px;
-}
-
 .dialog {
     display: none;
     font-size: 11pt;

structr-ui/src/main/resources/structr/js/app-builder.js
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
  */
-var win = $(window);
 var pagesArea, widgetsArea, statusArea, paWidth, paHeight, currentPage, block = 0, activePreviewDoc, widgetGroups = {};
 var appBuilderActiveWidgetTabRightKey = 'structrAppBuilderActiveWidgetTabRightKey' + port;
 
@@ -159,7 +158,8 @@ var _AppBuilder = {
 			});
 		}
 
-		var windowWidth = win.width(), windowHeight = win.height();
+		var windowWidth = $(window).width();
+		var windowHeight = $(window).height();
 		var offsetWidth = 160, headerOffsetHeight = 124;
 
 		$('#app-preview_' + page.id).css({
@@ -390,7 +390,7 @@ var _AppBuilder = {
 		var w;
 		Command.list('Widget', true, 1000, 1, 'name', 'asc', 'id,name,type,source,treePath,isWidget,previewWidth,previewHeight', function(widgets) {
 			w = widgets;
-			_Widgets.getRemoteWidgets(widgetsUrl, function(widget) {
+			_Widgets.getRemoteWidgets(_Widgets.url, function(widget) {
 				w.push(widget);
 			}, function() {
 				_AppBuilder.appendWidgets(w);
@@ -592,7 +592,6 @@ var _AppBuilder = {
 
 			window.setTimeout(function() {
 				css = {
-					//width: Math.min(widget.previewWidth, firstEl.outerWidth()),
 					width: firstEl.outerWidth(),
 					height: firstEl.outerHeight(),
 					overflow: 'hidden',
@@ -605,7 +604,7 @@ var _AppBuilder = {
 			}, 100);
 		});
 	},
-	resize: function(offsetLeft, offsetRight) {
+	resize: function() {
 
 		Structr.resize();
 
@@ -613,9 +612,10 @@ var _AppBuilder = {
 			position: 'fixed'
 		});
 
-		var windowWidth = win.width(), windowHeight = win.height();
+		var windowWidth = $(window).width();
+		var windowHeight = $(window).height();
 		var offsetWidth = 160;
-		var headerOffsetHeight = 124, previewOffset = 22;
+		var headerOffsetHeight = 124;
 
 		$('#app-builder').css({
 			width: windowWidth,
@@ -649,8 +649,7 @@ var _AppBuilder = {
 	onload: function() {
 		_AppBuilder.init();
 
-		win.off('resize');
-		win.resize(function() {
+		$(window).off('resize').resize(function() {
 			_AppBuilder.resize();
 		});
 

structr-ui/src/main/resources/structr/js/contents.js
@@ -17,7 +17,6 @@
  * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
  */
 var main, contentsMain, contentTree, contentsContents;
-var win = $(window);
 var selectedElements = [];
 var currentContentContainer;
 var containerPageSize = 10000, containerPage = 1;
@@ -63,20 +62,17 @@ var _Contents = {
 	},
 	resize: function() {
 
-		var windowWidth = win.width();
-		var windowHeight = win.height();
+		var windowHeight = $(window).height();
 		var headerOffsetHeight = 100;
 
 		if (contentTree) {
 			contentTree.css({
-//				width: Math.max(180, Math.min(windowWidth / 3, 360)) + 'px',
 				height: windowHeight - headerOffsetHeight + 'px'
 			});
 		}
 
 		if (contentsContents) {
 			contentsContents.css({
-//				width: windowWidth - 400 - 64 + 'px',
 				height: windowHeight - headerOffsetHeight - 55 + 'px'
 			});
 		}
@@ -89,9 +85,8 @@ var _Contents = {
 		left = left || LSWrapper.getItem(contentsResizerLeftKey) || 300;
 		$('.column-resizer', contentsMain).css({ left: left });
 
-		var w = $(window).width();
 		$('#contents-tree').css({width: left - 14 + 'px'});
-		$('#contents-contents').css({left: left + 8 + 'px', width: w - left - 58 + 'px'});
+		$('#contents-contents').css({left: left + 8 + 'px', width: $(window).width() - left - 58 + 'px'});
 	},
 	onload: function() {
 
@@ -111,9 +106,7 @@ var _Contents = {
 		$('#contents-contents-container').prepend(' <select id="add-content-item"><option value="">Add Content Item</option></select>');
 
 		var itemTypesSelector = $('#add-content-item', main);
-		// query: function(type, pageSize, page, sort, order, properties, callback, exact, view) {
 		Command.query('SchemaNode', 1000, 1, 'name', 'asc', { extendsClass: 'org.structr.web.entity.ContentItem' }, function(schemaNodes) {
-			//console.log(schemaNodes);
 			schemaNodes.forEach(function(schemaNode) {
 				var type = schemaNode.name;
 				itemTypesSelector.append('<option value="' + type + '">' + type + '</option>');
@@ -138,7 +131,6 @@ var _Contents = {
 
 		var containerTypesSelector = $('#add-content-container', main);
 		Command.query('SchemaNode', 1000, 1, 'name', 'asc', { extendsClass: 'org.structr.web.entity.ContentContainer' }, function(schemaNodes) {
-			//console.log(schemaNodes);
 			schemaNodes.forEach(function(schemaNode) {
 				var type = schemaNode.name;
 				containerTypesSelector.append('<option value="' + type + '">' + type + '</option>');
@@ -185,8 +177,7 @@ var _Contents = {
 
 		_TreeHelper.initTree(contentTree, _Contents.treeInitFunction, 'structr-ui-contents');
 
-		win.off('resize');
-		win.resize(function() {
+		$(window).off('resize').resize(function() {
 			_Contents.resize();
 		});
 

structr-ui/src/main/resources/structr/js/crawler.js
@@ -17,7 +17,6 @@
  * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
  */
 var main, crawlerMain, crawlerTree, crawlerList;
-var win = $(window);
 var selectedElements = [];
 var currentSite;
 var sitePageSize = 10000, sitePage = 1;
@@ -66,20 +65,17 @@ var _Crawler = {
 	},
 	resize: function() {
 
-		var windowWidth = win.width();
-		var windowHeight = win.height();
+		var windowHeight = $(window).height();
 		var headerOffsetHeight = 100;
 
 		if (crawlerTree) {
 			crawlerTree.css({
-//				width: Math.max(180, Math.min(windowWidth / 3, 360)) + 'px',
 				height: windowHeight - headerOffsetHeight + 'px'
 			});
 		}
 
 		if (crawlerList) {
 			crawlerList.css({
-//				width: windowWidth - 400 - 64 + 'px',
 				height: windowHeight - headerOffsetHeight - 55 + 'px'
 			});
 
@@ -98,9 +94,8 @@ var _Crawler = {
 		left = left || LSWrapper.getItem(crawlerResizerLeftKey) || 300;
 		$('.column-resizer', crawlerMain).css({ left: left });
 
-		var w = $(window).width();
 		$('#crawler-tree').css({width: left - 14 + 'px'});
-		$('#crawler-list').css({left: left + 8 + 'px', width: w - left - 58 + 'px'});
+		$('#crawler-list').css({left: left + 8 + 'px', width: $(window).width() - left - 58 + 'px'});
 	},
 	onload: function() {
 
@@ -167,8 +162,7 @@ var _Crawler = {
 
 		_TreeHelper.initTree(crawlerTree, _Crawler.treeInitFunction, 'structr-ui-crawler');
 
-		win.off('resize');
-		win.resize(function() {
+		$(window).off('resize').resize(function() {
 			_Crawler.resize();
 		});
 

structr-ui/src/main/resources/structr/js/dragndrop.js
@@ -427,7 +427,7 @@ var _Dragndrop = {
 						});
 
 						e.stopPropagation();
-						Command.appendWidget(widgetSource, target.id, pageId, widgetsUrl, attrs);
+						Command.appendWidget(widgetSource, target.id, pageId, _Widgets.url, attrs);
 
 						dialogCancelButton.click();
 						return false;
@@ -438,7 +438,7 @@ var _Dragndrop = {
 			} else {
 
 				// If no matches, directly append widget
-				Command.appendWidget(widgetSource, target.id, pageId, widgetsUrl);
+				Command.appendWidget(widgetSource, target.id, pageId, _Widgets.url);
 
 			}
 

structr-ui/src/main/resources/structr/js/entities.js
@@ -17,11 +17,11 @@
  * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
  */
 var buttonClicked;
-var activeElements = {};
-var activeQueryTabPrefix = 'structrActiveQueryTab_' + port;
-var activeEditTabPrefix = 'structrActiveEditTab_' + port;
 
 var _Entities = {
+	activeElements: {},
+	activeQueryTabPrefix: 'structrActiveQueryTab_' + port,
+	activeEditTabPrefix: 'structrActiveEditTab_' + port,
 	numberAttrs: ['position', 'size'],
 	readOnlyAttrs: ['lastModifiedDate', 'createdDate', 'createdBy', 'id', 'checksum', 'size', 'version', 'relativeFilePath'],
 	changeBooleanAttribute: function(attrElement, value, activeLabel, inactiveLabel) {
@@ -184,8 +184,6 @@ var _Entities = {
 
 		_Entities.activateTabs(entity.id, '#data-tabs', '#content-tab-rest');
 
-		//_Entities.appendInput(dialog, entity, 'partialUpdateKey', 'Types to trigger partial update', '');
-
 	},
 	activateTabs: function(nodeId, elId, activeId) {
 		var el = $(elId);
@@ -197,12 +195,12 @@ var _Entities = {
 				tab.addClass('active');
 				el.children('div').hide();
 				var id = tab.prop('id').substring(4);
-				LSWrapper.setItem(activeQueryTabPrefix  + '_' + nodeId, id);
+				LSWrapper.setItem(_Entities.activeQueryTabPrefix  + '_' + nodeId, id);
 				var content = $('#content-tab-' + id);
 				content.show();
 			});
 		});
-		var id = LSWrapper.getItem(activeQueryTabPrefix  + '_' + nodeId) || activeId.substring(13);
+		var id = LSWrapper.getItem(_Entities.activeQueryTabPrefix  + '_' + nodeId) || activeId.substring(13);
 		var tab = $('#tab-' + id);
 		if (!tab.hasClass('active')) {
 			tab.click();
@@ -452,7 +450,7 @@ var _Entities = {
      		contentEl.append('<div class="propTabContent" id="tabView-' + name + '"></div>');
 			$('#tabView-' + name).show();
 			self.addClass('active');
-			LSWrapper.setItem(activeEditTabPrefix  + '_' + entity.id, name);
+			LSWrapper.setItem(_Entities.activeEditTabPrefix  + '_' + entity.id, name);
 
 			if (typeof initCallback === "function") {
 				initCallback();
@@ -494,7 +492,7 @@ var _Entities = {
 				var tabView = $('#tabView-' + view);
 				fastRemoveAllChildren(tabView[0]);
 				tabView.show();
-				LSWrapper.setItem(activeEditTabPrefix  + '_' + entity.id, view);
+				LSWrapper.setItem(_Entities.activeEditTabPrefix  + '_' + entity.id, view);
 
 				Command.getSchemaInfo(entity.type, function(schemaInfo) {
 					var typeInfo = {};
@@ -505,7 +503,7 @@ var _Entities = {
 				});
 			});
 		});
-		activeView = activeViewOverride || LSWrapper.getItem(activeEditTabPrefix  + '_' + entity.id) || activeView;
+		activeView = activeViewOverride || LSWrapper.getItem(_Entities.activeEditTabPrefix  + '_' + entity.id) || activeView;
 		$('#tab-' + activeView).click();
 
 	},
@@ -1559,9 +1557,9 @@ var _Entities = {
 
 			var idString = 'id_' + entity.id;
 
-			if (!activeElements.hasOwnProperty(idString)) {
+			if (!_Entities.activeElements.hasOwnProperty(idString)) {
 
-				activeElements[idString] = entity;
+				_Entities.activeElements[idString] = entity;
 
 				var parent = $('#activeElements div.inner');
 				var id = entity.id;
@@ -1574,7 +1572,6 @@ var _Entities = {
 
 				var div = $('#active_' + id);
 				var query = entity.query;
-				//var dataKey     = (entity.dataKey.split(',')[entity.recursionDepth] || '');
 				var expand = entity.state === 'Query';
 				var icon = _Icons.brick_icon;
 				var name = '', content = '', action = '';
@@ -1599,8 +1596,6 @@ var _Entities = {
 					default:
 						content = entity.type;
 				}
-
-				console.log(icon);
 
 				div.append('<img class="typeIcon" src="' + icon + '">'
 					+ '<b title="' + name + '">' + fitStringToWidth(name, 180, 'slideOut') + '</b>'

structr-ui/src/main/resources/structr/js/files.js
@@ -21,7 +21,6 @@ var drop;
 var fileList;
 var chunkSize = 1024 * 64;
 var sizeLimit = 1024 * 1024 * 1024;
-var win = $(window);
 var selectedElements = [];
 var activeFileId, fileContents = {};
 var currentWorkingDir;
@@ -74,7 +73,7 @@ var _Files = {
 	},
 	resize: function() {
 
-		var windowHeight = win.height();
+		var windowHeight = $(window).height();
 		var headerOffsetHeight = 100;
 
 		if (fileTree) {
@@ -132,9 +131,8 @@ var _Files = {
 		left = left || LSWrapper.getItem(filesResizerLeftKey) || 300;
 		$('.column-resizer', filesMain).css({ left: left });
 
-		var w = $(window).width();
 		$('#file-tree').css({width: left - 14 + 'px'});
-		$('#folder-contents').css({left: left + 8 + 'px', width: w - left - 58 + 'px'});
+		$('#folder-contents').css({left: left + 8 + 'px', width: $(window).width() - left - 58 + 'px'});
 	},
 	onload: function() {
 
@@ -241,8 +239,7 @@ var _Files = {
 
 		_Files.activateUpload();
 
-		win.off('resize');
-		win.resize(function() {
+		$(window).off('resize').resize(function() {
 			_Files.resize();
 		});
 

commit 40ca985fc108ad71ec9a8a5bc4c82e4749e771d9

Bugfix: Opening the crud edit mask in a maximized state now functions correctly again (my change in 7df96df introduced this bug)

structr-ui/src/main/resources/structr/js/crud.js
@@ -1137,12 +1137,11 @@ var _Crud = {
 			type: 'GET',
 			dataType: 'json',
 			contentType: 'application/json; charset=utf-8',
-			//async: false,
-			success: function(data) {
-				if (!data)
-					return;
-				_Crud.dialog('Edit ' + t + ' ' + id, function() {}, function() {});
-				_Crud.showDetails(data.result, t);
+			success: function (data) {
+				if (data) {
+					_Crud.dialog('Edit ' + t + ' ' + id, function() {}, function() {});
+					_Crud.showDetails(data.result, t);
+				}
 			}
 		});
 	},
@@ -2284,11 +2283,11 @@ var _Crud = {
 			var dimensions = Structr.getDialogDimensions(0, 24);
 			Structr.blockUI(dimensions);
 
+			_Crud.resize();
+
 		}
 	},
 	resize: function() {
-
-		Structr.resize();
 
 		var dimensions = Structr.getDialogDimensions(0, 24);
 
@@ -2313,6 +2312,8 @@ var _Crud = {
 		});
 
 		_Crud.moveResizer();
+
+		Structr.resize();
 
 	},
 	error: function(text, confirmationRequired) {