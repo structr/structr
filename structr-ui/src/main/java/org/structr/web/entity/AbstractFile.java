structr/structr

commit 1e8d8cde5bfab25b1d6e5ff39221c237374957f7

Improvement: The auto-renaming of newly created files/folders now happens onCreation instead of in multiple places. This solves multiple cases where creating files caused problems.

structr-ui/src/main/java/org/structr/web/entity/AbstractFile.java
@@ -20,6 +20,8 @@ package org.structr.web.entity;
 
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.structr.common.PropertyView;
 import org.structr.common.SecurityContext;
 import org.structr.common.ValidationHelper;
@@ -39,6 +41,7 @@ import org.structr.core.property.EndNodes;
 import org.structr.core.property.EntityIdProperty;
 import org.structr.core.property.Property;
 import org.structr.core.property.StartNode;
+import org.structr.web.common.FileHelper;
 import org.structr.web.entity.relation.FileChildren;
 import org.structr.web.entity.relation.FileSiblings;
 import org.structr.web.entity.relation.FolderChildren;
@@ -50,6 +53,8 @@ import org.structr.web.property.PathProperty;
  *
  */
 public class AbstractFile extends LinkedTreeNode<FileChildren, FileSiblings, AbstractFile> {
+
+	private static final Logger logger = LoggerFactory.getLogger(AbstractFile.class.getName());
 
 	public static final Property<Folder> parent                    = new StartNode<>("parent", FolderChildren.class);
 	public static final Property<List<AbstractFile>> children      = new EndNodes<>("children", FileChildren.class);
@@ -75,7 +80,12 @@ public class AbstractFile extends LinkedTreeNode<FileChildren, FileSiblings, Abs
 	@Override
 	public boolean onCreation(final SecurityContext securityContext, final ErrorBuffer errorBuffer) throws FrameworkException {
 
-		final boolean valid = validatePath(securityContext, errorBuffer);
+		boolean valid = true;
+
+		if (validatePathUniqueness) {
+			valid = validateAndRenameFileOnce(securityContext, errorBuffer);
+		}
+
 		return valid && super.onCreation(securityContext, errorBuffer);
 	}
 
@@ -88,31 +98,53 @@ public class AbstractFile extends LinkedTreeNode<FileChildren, FileSiblings, Abs
 
 	public boolean validatePath(final SecurityContext securityContext, final ErrorBuffer errorBuffer) throws FrameworkException {
 
-		if (validatePathUniqueness) {
+		final String filePath = getProperty(path);
+		if (filePath != null) {
 
-			final String filePath = getProperty(path);
-			if (filePath != null) {
+			final List<AbstractFile> files = StructrApp.getInstance().nodeQuery(AbstractFile.class).and(path, filePath).getAsList();
+			for (final AbstractFile file : files) {
 
-				final List<AbstractFile> files = StructrApp.getInstance().nodeQuery(AbstractFile.class).and(path, filePath).getAsList();
-				for (final AbstractFile file : files) {
+				if (!file.getUuid().equals(getUuid())) {
 
-					if (!file.getUuid().equals(getUuid())) {
+					if (errorBuffer != null) {
 
-						if (errorBuffer != null) {
+						final UniqueToken token = new UniqueToken(AbstractFile.class.getSimpleName(), path, file.getUuid());
+						token.setValue(filePath);
 
-							final UniqueToken token = new UniqueToken(AbstractFile.class.getSimpleName(), path, file.getUuid());
-							token.setValue(filePath);
-
-							errorBuffer.add(token);
-						}
-
-						return false;
+						errorBuffer.add(token);
 					}
+
+					return false;
 				}
 			}
 		}
 
 		return true;
+	}
+
+	public boolean validateAndRenameFileOnce(final SecurityContext securityContext, final ErrorBuffer errorBuffer) throws FrameworkException {
+
+		boolean valid = validatePath(securityContext, null);
+
+		if (!valid) {
+
+			final String originalPath = getProperty(AbstractFile.path);
+			final String newName = getProperty(AbstractFile.name).concat("_").concat(FileHelper.getDateString());
+
+			setProperty(AbstractFile.name, newName);
+
+			valid = validatePath(securityContext, errorBuffer);
+
+			if (valid) {
+				logger.warn("File {} already exists, renaming to {}", new Object[] { originalPath, newName });
+			} else {
+				logger.warn("File {} already existed. Tried renaming to {} and failed. Aborting.", new Object[] { originalPath, newName });
+			}
+
+		}
+
+		return valid;
+
 	}
 
 	@Override

structr-ui/src/main/java/org/structr/web/importer/Importer.java
@@ -54,7 +54,6 @@ import org.structr.common.CaseHelper;
 import org.structr.common.PathHelper;
 import org.structr.common.PropertyView;
 import org.structr.common.SecurityContext;
-import org.structr.common.error.ErrorBuffer;
 import org.structr.common.error.FrameworkException;
 import org.structr.core.GraphObject;
 import org.structr.core.app.App;
@@ -1133,12 +1132,11 @@ public class Importer {
 
 	private FileBase createFileNode(final String uuid, final String path, final String contentType, final long size, final long checksum, final Class fileClass) throws FrameworkException {
 
-		final String name = PathHelper.getName(path);
 		final String relativeFilePath = File.getDirectoryPath(uuid) + "/" + uuid;
 
 		final FileBase fileNode = app.create(fileClass != null ? fileClass : File.class,
 			new NodeAttribute(GraphObject.id, uuid),
-			new NodeAttribute(AbstractNode.name, name),
+			new NodeAttribute(AbstractNode.name, PathHelper.getName(path)),
 			new NodeAttribute(File.relativeFilePath, relativeFilePath),
 			new NodeAttribute(File.contentType, contentType),
 			new NodeAttribute(File.size, size),
@@ -1149,15 +1147,6 @@ public class Importer {
 
 		final Folder parentFolder = FileHelper.createFolderPath(securityContext, PathHelper.getFolderPath(path));
 		fileNode.setProperty(FileBase.parent, parentFolder);
-
-		if (!fileNode.validatePath(securityContext, new ErrorBuffer())) {
-
-			final String newName = name.concat("_").concat(FileHelper.getDateString());
-
-			logger.warn("File {} already exists, renaming to {}", new Object[] { path, newName });
-
-			fileNode.setProperty(AbstractNode.name, newName);
-		}
 
 		return fileNode;
 

structr-ui/src/main/java/org/structr/web/servlet/UploadServlet.java
@@ -20,7 +20,6 @@ package org.structr.web.servlet;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -299,10 +298,6 @@ public class UploadServlet extends HttpServlet implements HttpServiceServlet {
 
 								newFile.setProperties(securityContext, changedProperties);
 
-								if (!newFile.validatePath(securityContext, null)) {
-									newFile.setProperty(AbstractNode.name, PathHelper.getName(name).concat("_").concat(FileHelper.getDateString()));
-								}
-
 								uuid = newFile.getUuid();
 
 								tx.success();
@@ -348,19 +343,19 @@ public class UploadServlet extends HttpServlet implements HttpServiceServlet {
 		} catch (Throwable t) {
 
 			final String content;
-			
+
 			if (t instanceof FrameworkException) {
-				
+
 				final FrameworkException fex = (FrameworkException) t;
 				logger.error(fex.toString());
 				content = errorPage(fex);
-				
+
 			} else {
-			
+
 				logger.error("Exception while processing upload request", t);
 				content = errorPage(t);
 			}
-			
+
 			try {
 				final ServletOutputStream out = response.getOutputStream();
 				IOUtils.write(content, out);

structr-ui/src/main/java/org/structr/websocket/command/CreateCommand.java
@@ -31,7 +31,6 @@ import org.structr.core.graph.TransactionCommand;
 import org.structr.core.property.PropertyMap;
 import org.structr.dynamic.File;
 import org.structr.schema.SchemaHelper;
-import org.structr.web.common.FileHelper;
 import org.structr.web.entity.FileBase;
 import org.structr.websocket.StructrWebSocket;
 import org.structr.websocket.message.MessageBuilder;
@@ -85,10 +84,6 @@ public class CreateCommand extends AbstractCommand {
 				changedProperties.put(File.contentType, contentType);
 				changedProperties.put(AbstractNode.name, name);
 				fileNode.setProperties(securityContext, changedProperties);
-
-				if (!fileNode.validatePath(securityContext, null)) {
-					fileNode.setProperty(AbstractNode.name, name.concat("_").concat(FileHelper.getDateString()));
-				}
 
 				getWebSocket().createFileUploadHandler(fileNode);
 

structr-ui/src/test/java/org/structr/web/common/RenderContextTest.java
@@ -540,9 +540,6 @@ public class RenderContextTest extends StructrUiTest {
 			// test POST with invalid name containing curly braces to provoke 422
 			assertEquals("Invalid POST result", "422",                             Scripting.replaceVariables(ctx, page, "${POST('http://localhost:" + httpPort + "/structr/rest/folders', '{name:\"ShouldFail/xyz\"}').status}"));
 
-			// test that duplicate folder names are not allowed
-			assertEquals("Invalid POST result", "422",                             Scripting.replaceVariables(ctx, page, "${POST('http://localhost:" + httpPort + "/structr/rest/folders', '{name:status}').status}"));
-
 			// test login and sessions
 			final String sessionIdCookie = Scripting.replaceVariables(ctx, page, "${POST('http://localhost:" + httpPort + "/structr/rest/login', '{name:admin,password:admin}').headers.Set-Cookie}");
 			final String sessionId       = HttpCookie.parse(sessionIdCookie).get(0).getValue();

commit 26ea7484ee8203e3f2985d3496980404d42435ae

Bugfix: Correctly respects configuration switch `application.filesystem.unique.paths` when renaming files/folders (bug introduced in commit 1e8d8cd)

structr-ui/src/main/java/org/structr/web/entity/AbstractFile.java
@@ -92,7 +92,12 @@ public class AbstractFile extends LinkedTreeNode<FileChildren, FileSiblings, Abs
 	@Override
 	public boolean onModification(final SecurityContext securityContext, final ErrorBuffer errorBuffer, final ModificationQueue modificationQueue) throws FrameworkException {
 
-		final boolean valid = validatePath(securityContext, errorBuffer);
+		boolean valid = true;
+
+		if (validatePathUniqueness) {
+			valid = validatePath(securityContext, errorBuffer);
+		}
+
 		return valid && super.onModification(securityContext, errorBuffer, modificationQueue);
 	}
 