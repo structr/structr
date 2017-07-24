53396f36853243940ea95e38f341df30e9060d79
vigorouscoding <kai.schwaiger@structr.com>
2017-07-24 15:46:42 +0200

    Optimization: Don't iterate over all relationships when ensuring the cardinality. Rather just use cypher to look for/delete existing relationships

--- structr-core/src/main/java/org/structr/core/entity/ManyToOne.java
+++ structr-core/src/main/java/org/structr/core/entity/ManyToOne.java
@@ -18,13 +18,15 @@
  */
 package org.structr.core.entity;
 
+import java.util.LinkedHashMap;
 import org.structr.api.graph.Direction;
 import org.structr.common.SecurityContext;
 import org.structr.common.error.FrameworkException;
+import org.structr.core.GraphObject;
 import org.structr.core.app.App;
 import org.structr.core.app.StructrApp;
 import org.structr.core.graph.NodeInterface;
-import org.structr.core.graph.search.SearchCommand;
+import org.structr.core.graph.RelationshipInterface;
 import org.structr.core.notion.Notion;
 import org.structr.core.notion.RelationshipNotion;
 
@@ -67,19 +69,16 @@ public abstract class ManyToOne<S extends NodeInterface, T extends NodeInterface
 	@Override
 	public void ensureCardinality(final SecurityContext securityContext, final NodeInterface sourceNode, final NodeInterface targetNode) throws FrameworkException {
 
-		final App app                          = StructrApp.getInstance();
-		final Class<? extends ManyToOne> clazz = this.getClass();
-		final Class<T> targetType              = getTargetType();
+		final App app = StructrApp.getInstance();
 
 		if (sourceNode != null) {
 
-			// check existing relationships
-			final Relation<?, T, ?, ?> outgoingRel = sourceNode.getOutgoingRelationship(clazz);
-//			if (outgoingRel != null && targetType.isAssignableFrom(outgoingRel.getTargetType())) {
-//			if (outgoingRel != null && targetType.isInstance(outgoingRel.getTargetNode())) {
-			if (outgoingRel != null && SearchCommand.isTypeAssignableFromOtherType(targetType, outgoingRel.getTargetType())) {
+			final LinkedHashMap<String, Object> props = new LinkedHashMap();
+			props.put("sourceId",       sourceNode.getUuid());
+			props.put("structrRelType", getClass().getSimpleName());
 
-				app.delete(outgoingRel);
+			for (GraphObject outgoingRel : app.cypher("MATCH (s:" + sourceNode.getType() + ")-[r]->() WHERE s.id = {sourceId} AND r.type = {structrRelType} RETURN r", props)) {
+				app.delete((RelationshipInterface) outgoingRel);
 			}
 		}
 	}
