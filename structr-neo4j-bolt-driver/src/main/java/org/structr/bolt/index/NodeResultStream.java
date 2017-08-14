bc38e8fdf0d72d186528d5fb852e462099ce3059
Christian Morgner <christian@morgner.de>
2017-08-14 19:18:16 +0200

    Improvement: remove code duplication by using an abstract base class.

--- structr-neo4j-bolt-driver/src/main/java/org/structr/bolt/index/NodeResultStream.java
+++ structr-neo4j-bolt-driver/src/main/java/org/structr/bolt/index/NodeResultStream.java
@@ -18,7 +18,6 @@
  */
 package org.structr.bolt.index;
 
-import java.util.Iterator;
 import java.util.Map;
 import org.neo4j.driver.v1.types.Node;
 import org.structr.api.QueryResult;
@@ -26,79 +25,14 @@ import org.structr.bolt.SessionTransaction;
 
 /**
  */
-public class NodeResultStream implements QueryResult<Node> {
-
-	private QueryResult<Node> result = null;
-	private PageableQuery query      = null;
-	private Iterator<Node> current   = null;
-	private SessionTransaction tx    = null;
+public class NodeResultStream extends AbstractResultStream<Node> {
 
 	public NodeResultStream(final SessionTransaction tx, final PageableQuery query) {
-
-		this.query  = query;
-		this.tx     = tx;
+		super(tx, query);
 	}
 
 	@Override
-	public void close() {
-		
-		if (result != null) {
-			result.close();
-		}
-	}
-
-	@Override
-	public Iterator<Node> iterator() {
-
-		return new Iterator<Node>() {
-
-			private int remaining = 0;
-
-			@Override
-			public boolean hasNext() {
-
-				if (current == null || !current.hasNext()) {
-
-					// close previous result
-					if (result != null) {
-						result.close();
-					}
-
-					// fetch more?
-					if (remaining == 0) {
-
-						// reset count
-						remaining = query.pageSize();
-
-						final String statement            = query.getStatement();
-						final Map<String, Object> params  = query.getParameters();
-
-						result = tx.getNodes(statement, params);
-						if (result != null) {
-
-							current = result.iterator();
-
-							// advance page
-							query.nextPage();
-
-							// does the next result have elements?
-							if (!current.hasNext()) {
-
-								// no more elements
-								return false;
-							}
-						}
-					}
-				}
-
-				return current != null && current.hasNext();
-			}
-
-			@Override
-			public Node next() {
-				remaining--;
-				return current.next();
-			}
-		};
+	protected QueryResult<Node> fetchData(final SessionTransaction tx, final String statement, final Map<String, Object> data) {
+		return tx.getNodes(statement, data);
 	}
 }
