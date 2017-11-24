ba8c033595a50e97aa20d4041b8d2cd3c109c551
Axel Morgner <axel@morgner.de>
2017-11-24 11:17:48 +0100

    New module Java parser. Wraps GitHub's JavaParser (see http://javaparser.org).

---
+++ structr-modules/structr-java-parser-module/src/main/java/org/structr/javaparser/JavaParserModule.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (C) 2010-2017 Structr GmbH
+ *
+ * This file is part of Structr <http://structr.org>.
+ *
+ * Structr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Structr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.structr.javaparser;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.structr.api.service.LicenseManager;
+import org.structr.core.entity.AbstractSchemaNode;
+import org.structr.core.function.Functions;
+import org.structr.module.StructrModule;
+import org.structr.schema.action.Actions;
+
+/**
+ *
+ */
+public class JavaParserModule implements StructrModule {
+
+	@Override
+	public void onLoad(final LicenseManager licenseManager) {
+
+//		final boolean basicEdition         = licenseManager == null || licenseManager.isEdition(LicenseManager.Basic);
+//		final boolean smallBusinessEdition = licenseManager == null || licenseManager.isEdition(LicenseManager.SmallBusiness);
+		final boolean enterpriseEdition    = licenseManager == null || licenseManager.isEdition(LicenseManager.Enterprise);
+		
+		// Enterprise only
+		Functions.put(enterpriseEdition, LicenseManager.Enterprise, "parse_java",                      new ParseJavaFunction());
+	}
+
+	@Override
+	public String getName() {
+		return "java-parser";
+	}
+
+	@Override
+	public Set<String> getDependencies() {
+		return new LinkedHashSet<>();
+	}
+
+	@Override
+	public Set<String> getFeatures() {
+		return null;
+	}
+
+	@Override
+	public void insertImportStatements(final AbstractSchemaNode schemaNode, final StringBuilder buf) {
+	}
+
+	@Override
+	public void insertSourceCode(final AbstractSchemaNode schemaNode, final StringBuilder buf) {
+	}
+
+	@Override
+	public Set<String> getInterfacesForType(final AbstractSchemaNode schemaNode) {
+		return null;
+	}
+
+	@Override
+	public void insertSaveAction(final AbstractSchemaNode schemaNode, final StringBuilder buf, final Actions.Type type) {
+	}
+}
