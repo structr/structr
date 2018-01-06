structr/structr

commit a6841dcc67ca641699c5b6d39db52265ebd95d46

Improvement: Take advantage of existing AsyncObjectCache to retrieve templates - reduces the number of requests dramatically

structr-ui/src/main/resources/structr/js/crud.js
@@ -2004,7 +2004,7 @@ var _Crud = {
 		if (preloadedNode) {
 			nodeHandler(preloadedNode);
 		} else {
-			_Crud.crudCache.registerCallbackForId(id, nodeHandler);
+			_Crud.crudCache.registerCallback(id, id, nodeHandler);
 		}
 
 	},

structr-ui/src/main/resources/structr/js/entities.js
@@ -706,7 +706,7 @@ var _Entities = {
 
 									var nodeId = res[key].id || res[key];
 
-									tempNodeCache.registerCallbackForId(nodeId, function(node) {
+									tempNodeCache.registerCallback(nodeId, nodeId, function(node) {
 
 										_Entities.appendRelatedNode(cell, node, function(nodeEl) {
 											$('.remove', nodeEl).on('click', function(e) {
@@ -731,7 +731,7 @@ var _Entities = {
 
 										var nodeId = obj.id || obj;
 
-										tempNodeCache.registerCallbackForId(nodeId, function(node) {
+										tempNodeCache.registerCallback(nodeId, nodeId, function(node) {
 
 											_Entities.appendRelatedNode(cell, node, function(nodeEl) {
 												$('.remove', nodeEl).on('click', function(e) {

structr-ui/src/main/resources/structr/js/helper.js
@@ -1070,7 +1070,7 @@ var _Favorites = new (function () {
 });
 
 /**
- * A cache for async GET requests which allows ensures that certain requests are only made once - if used correctly.<br>
+ * A cache for async GET requests which ensures that certain requests are only made once - if used correctly.<br>
  * The cache has one argument - the fetch function - which handles fetching a single object.<br>
  * Upon successfully loading the object, it must be added to the cache via the `addObject` method.<br>
  * It is possible to attach callbacks to the ID we ware waiting for. After the object is loaded<br>
@@ -1084,34 +1084,33 @@ var AsyncObjectCache = function(fetchFunction) {
 	var _cache = {};
 
 	/**
-	 * This methods registers a callback for an object ID.<br>
-	 * If the ID has not been requested before, the fetch function is executed.<br>
-	 * If the ID has been requested before, the callback is added to the callbacks list.<br>
-	 * If the object associated with the ID is present in the cache, the callback is executed directly.
+	 * This methods registers a callback for a resource.<br>
+	 * If the resource has not been requested before, the fetch function is executed.<br>
+	 * If the resource has been requested before, the callback is added to the callbacks list.<br>
+	 * If the result for the cacheId is present in the cache, the callback is executed directly.
 	 *
-	 * @param {string} id The ID to fetch
+	 * @param {string} resource The parameter to pass in the fetchFunction
+	 * @param {string} cacheId The ID under which to cache the result
 	 * @param {function} callback The callback to execute with the fetched object. Needs to take the object as single paramter.
 	 */
-	this.registerCallbackForId = function(id, callback) {
+	this.registerCallback = function(resource, cacheId, callback) {
 
-		if (_cache[id] === undefined) {
+		if (_cache[cacheId] === undefined) {
 
-			_cache[id] = {
+			_cache[cacheId] = {
 				callbacks: [callback]
 			};
 
-			fetchFunction(id);
+			fetchFunction(resource);
 
-		} else if (_cache[id].value === undefined) {
+		} else if (_cache[cacheId].value === undefined) {
 
-			_cache[id].callbacks.push(callback);
+			_cache[cacheId].callbacks.push(callback);
 
 		} else {
 
-			_runSingleCallback(id, callback);
-
+			_runSingleCallback(cacheId, callback, true);
 		}
-
 	};
 
 	/**
@@ -1119,49 +1118,45 @@ var AsyncObjectCache = function(fetchFunction) {
 	 * Callbacks associated with that object will be executed afterwards.
 	 *
 	 * @param {object} obj The object to store in the cache
+	 * @param {string} cacheId The cache identifier for the object - usually a UUID, but also used as a URI
 	 */
-	this.addObject = function(obj) {
+	this.addObject = function(obj, cacheId) {
 
-		if (_cache[obj.id] === undefined) {
+		if (_cache[cacheId] === undefined) {
 
 			// no registered callbacks - simply set the cache object
-			_cache[obj.id] = {
+			_cache[cacheId] = {
 				value: obj
 			};
 
-		} else if (_cache[obj.id].value === undefined) {
+		} else if (_cache[cacheId].value === undefined) {
 
 			// set the cache object and run all registered callbacks
-			_cache[obj.id].value = obj;
-			_runRegisteredCallbacks(obj.id);
-
+			_cache[cacheId].value = obj;
+			_runRegisteredCallbacks(cacheId);
 		}
-
 	};
 
 	this.clear = function() {
 		_cache = {};
 	};
 
-	function _runRegisteredCallbacks (id) {
+	function _runRegisteredCallbacks (cacheId) {
 
-		if (_cache[id] !== undefined && _cache[id].callbacks) {
+		if (_cache[cacheId] !== undefined && _cache[cacheId].callbacks) {
 
-			_cache[id].callbacks.forEach(function(callback) {
-				_runSingleCallback(id, callback);
+			_cache[cacheId].callbacks.forEach(function(callback) {
+				_runSingleCallback(cacheId, callback, false);
 			});
 
-			_cache[id].callbacks = [];
+			_cache[cacheId].callbacks = [];
 		}
-
 	};
 
-	function _runSingleCallback(id, callback) {
+	function _runSingleCallback(cacheId, callback, cacheHit) {
 
 		if (typeof callback === "function") {
-			callback(_cache[id].value);
+			callback(_cache[cacheId].value, cacheHit);
 		}
-
 	};
-
 };

structr-ui/src/main/resources/structr/js/init.js
@@ -210,7 +210,13 @@ var Structr = {
 	expanded: {},
 	msgCount: 0,
 	currentlyActiveSortable: undefined,
-	templateCache: {},
+	templateCache: new AsyncObjectCache(function(templateName) {
+
+		Promise.resolve($.ajax('templates/' + templateName + '.html')).then(function(templateHtml) {
+			Structr.templateCache.addObject(templateHtml, templateName);
+		});
+
+	}),
 
 	reconnect: function() {
 		_Logger.log(_LogType.INIT, 'deactivated ping');
@@ -1313,22 +1319,6 @@ var Structr = {
 				return ['Community'].indexOf(requiredEdition) !== -1;
 		};
 	},
-	guardExecution:function (callbackToGuard) {
-		var didRun = false;
-
-		var guardedFunction = function () {
-			if (didRun) {
-				return;
-			}
-			didRun = true;
-
-			if (typeof callbackToGuard === "function") {
-				callbackToGuard();
-			}
-		};
-
-		return guardedFunction;
-	},
 	updateMainHelpLink: function (newUrl) {
 		$('#main-help a').attr('href', newUrl);
 	},
@@ -1691,15 +1681,11 @@ var Structr = {
 	},
 	fetchHtmlTemplate: function(templateName, templateConfig, callback) {
 
-		if (Structr.templateCache[templateName]) {
-			callback(Structr.templateCache[templateName](templateConfig), true);
-		} else {
-
-			Promise.resolve($.ajax('templates/' + templateName + '.html')).then(function(result) {
-				Structr.templateCache[templateName] = new Function("config", "return `" + result + "`;");
-				callback(Structr.templateCache[templateName](templateConfig), false);
-			});
-		}
+		Structr.templateCache.registerCallback(templateName, templateName, function(templateHtml, cacheHit) {
+			var convertTemplateToLiteral = new Function("config", "return `" + templateHtml + "`;");
+			var parameterizedTemplate = convertTemplateToLiteral(templateConfig);
+			callback(parameterizedTemplate, cacheHit);
+		});
 	}
 };
 

structr-ui/src/main/resources/structr/js/schema.js
@@ -3481,8 +3481,7 @@ var _Schema = {
 		});
 	},
 	getMethodsInitFunction: function(container) {
-		return Structr.guardExecution(function() {
-
+		return (function() {
 			$('textarea.property-code', container).each(function(i, el) {
 				_Schema.initCodeMirrorForMethodCode(el);
 			});
@@ -3490,7 +3489,6 @@ var _Schema = {
 			$(' textarea.property-comment', container).each(function(i, el) {
 				_Schema.initCodeMirrorForMethodComment(el);
 			});
-
 			_Schema.restoreSchemaMethodsRowHeights(container);
 		});
 	},
@@ -3498,43 +3496,49 @@ var _Schema = {
 		return (contentText.substring(0, 1) === "{") ? 'javascript' : 'none';
 	},
 	initCodeMirrorForMethodCode: function(el) {
-		var cm = CodeMirror.fromTextArea(el, {
-			lineNumbers: true,
-			mode: _Schema.senseCodeMirrorMode($(el).val()),
-			lineWrapping: LSWrapper.getItem(lineWrappingKey),
-			extraKeys: {
-				"'.'":        _Contents.autoComplete,
-				"Ctrl-Space": _Contents.autoComplete
-			},
-			indentUnit: 4,
-			tabSize: 4,
-			indentWithTabs: true
-		});
-		$(cm.getWrapperElement()).addClass('cm-schema-methods');
-		cm.refresh();
+		var existingCodeMirror = $('.CodeMirror', $(el).parent())[0];
+		if (!existingCodeMirror) {
+			var cm = CodeMirror.fromTextArea(el, {
+				lineNumbers: true,
+				mode: _Schema.senseCodeMirrorMode($(el).val()),
+				lineWrapping: LSWrapper.getItem(lineWrappingKey),
+				extraKeys: {
+					"'.'":        _Contents.autoComplete,
+					"Ctrl-Space": _Contents.autoComplete
+				},
+				indentUnit: 4,
+				tabSize: 4,
+				indentWithTabs: true
+			});
+			$(cm.getWrapperElement()).addClass('cm-schema-methods');
+			cm.refresh();
 
-		cm.on('change', function(cm, changeset) {
-			cm.save();
-			cm.setOption('mode', _Schema.senseCodeMirrorMode(cm.getValue()));
-			$(cm.getTextArea()).trigger('change');
-		});
+			cm.on('change', function(cm, changeset) {
+				cm.save();
+				cm.setOption('mode', _Schema.senseCodeMirrorMode(cm.getValue()));
+				$(cm.getTextArea()).trigger('change');
+			});
+		}
 	},
 	initCodeMirrorForMethodComment: function(el) {
-		var cm = CodeMirror.fromTextArea(el, {
-			theme: "no-lang",
-			lineNumbers: true,
-			lineWrapping: LSWrapper.getItem(lineWrappingKey),
-			indentUnit: 4,
-			tabSize: 4,
-			indentWithTabs: true
-		});
-		$(cm.getWrapperElement()).addClass('cm-schema-methods');
-		cm.refresh();
+		var existingCodeMirror = $('.CodeMirror', $(el).parent())[0];
+		if (!existingCodeMirror) {
+			var cm = CodeMirror.fromTextArea(el, {
+				theme: "no-lang",
+				lineNumbers: true,
+				lineWrapping: LSWrapper.getItem(lineWrappingKey),
+				indentUnit: 4,
+				tabSize: 4,
+				indentWithTabs: true
+			});
+			$(cm.getWrapperElement()).addClass('cm-schema-methods');
+			cm.refresh();
 
-		cm.on('change', function(cm, changeset) {
-			cm.save();
-			$(cm.getTextArea()).trigger('change');
-		});
+			cm.on('change', function(cm, changeset) {
+				cm.save();
+				$(cm.getTextArea()).trigger('change');
+			});
+		}
 	},
 	makeSchemaMethodRowResizable: function(tr) {
 		var initialRowHeight;

commit 4391efa58e280713f14540b98ea8b057f1277435

Bugfix: Fixes bug introduced in a6841dcc67ca641699c5b6d39db52265ebd95d46 where API of AsyncObjectCache was changed

structr-ui/src/main/resources/structr/js/crud.js
@@ -101,7 +101,7 @@ var _Crud = {
 					return;
 
 				var node = data.result;
-				_Crud.crudCache.addObject(node);
+				_Crud.crudCache.addObject(node, node.id);
 			}
 		});
 	}),

structr-ui/src/main/resources/structr/js/entities.js
@@ -544,7 +544,7 @@ var _Entities = {
 
 				var tempNodeCache = new AsyncObjectCache(function(id) {
 					Command.getProperties(id, 'id,name,type,tag,isContent,content', function (node) {
-						tempNodeCache.addObject(node);
+						tempNodeCache.addObject(node, node.id);
 					});
 				});
 