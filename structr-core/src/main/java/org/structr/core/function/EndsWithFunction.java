7746a522c15c31f502f480b30e06f791481bb949
vigorouscoding <kai.schwaiger@structr.com>
2018-02-28 16:24:50 +0100

    Feature: Added two new builtin functions `starts_with()` and `ends_with()`

---
+++ structr-core/src/main/java/org/structr/core/function/EndsWithFunction.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2010-2018 Structr GmbH
+ *
+ * This file is part of Structr <http://structr.org>.
+ *
+ * Structr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Structr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.structr.core.function;
+
+import org.structr.common.error.FrameworkException;
+import org.structr.schema.action.ActionContext;
+import org.structr.schema.action.Function;
+
+
+public class EndsWithFunction extends Function<Object, Object> {
+
+	public static final String ERROR_MESSAGE_ENDS_WITH = "Usage: ${ends_with(string, suffix)}. Example: ${ends_with(locale, \"de\")}";
+
+	@Override
+	public String getName() {
+		return "ends_with()";
+	}
+
+	@Override
+	public Object apply(final ActionContext ctx, final Object caller, final Object[] sources) throws FrameworkException {
+
+		if (arrayHasLengthAndAllElementsNotNull(sources, 2)) {
+
+			final String searchString = sources[0].toString();
+			final String suffix       = sources[1].toString();
+
+			return searchString.endsWith(suffix);
+
+		} else {
+
+			logParameterError(caller, sources, ctx.isJavaScriptContext());
+
+		}
+
+		return usage(ctx.isJavaScriptContext());
+	}
+
+	@Override
+	public String usage(boolean inJavaScriptContext) {
+		return ERROR_MESSAGE_ENDS_WITH;
+	}
+
+	@Override
+	public String shortDescription() {
+		return "Returns true if the given string ends with the given suffix";
+	}
+}
