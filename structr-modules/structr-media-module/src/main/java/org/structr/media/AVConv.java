20e8ce4dcba2d3c6142b66403009c5531f6949e2
vigorouscoding <kai.schwaiger@structr.com>
2018-05-03 09:13:48 +0200

    Feature: Enable to detect if avconv is installed

--- structr-modules/structr-media-module/src/main/java/org/structr/media/AVConv.java
+++ structr-modules/structr-media-module/src/main/java/org/structr/media/AVConv.java
@@ -18,6 +18,9 @@
  */
 package org.structr.media;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -117,5 +120,24 @@ public class AVConv implements VideoHelper {
 		}
 
 		return null;
+	}
+
+	public static boolean isAVConvInstalled() {
+
+		final String[] args = {"/bin/sh", "-c", "which avconv"};
+
+		try {
+
+			Process proc = Runtime.getRuntime().exec(args);
+
+			final String res = new BufferedReader(new InputStreamReader(proc.getInputStream())).readLine();
+
+			return ((res != null) && !("".equals(res.trim())));
+
+		} catch (IOException ex) {
+
+			logger.warn("", ex);
+			return false;
+		}
 	}
 }
