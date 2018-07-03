5726181f71655c4a2868884cafb327d7b6875c56
Christian Morgner <christian.morgner@structr.com>
2018-07-03 17:11:23 +0200

    Bugfix: modify initialization order of services in test classes to make sure that HttpService always starts _after_ SchemaService.

--- structr-rest/src/test/java/org/structr/csv/test/StructrCsvTest.java
+++ structr-rest/src/test/java/org/structr/csv/test/StructrCsvTest.java
@@ -308,7 +308,7 @@ public class StructrCsvTest {
 
 		basePath = "/tmp/structr-test-" + timestamp;
 
-		Settings.Services.setValue("NodeService LogService HttpService SchemaService");
+		Settings.Services.setValue("NodeService SchemaService HttpService");
 		Settings.ConnectionUrl.setValue(Settings.TestingConnectionUrl.getValue());
 
 		// example for new configuration setup
