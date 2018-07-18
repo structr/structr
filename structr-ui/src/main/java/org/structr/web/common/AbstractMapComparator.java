35e44d19d2966ccd6fe8a50b0138089cd528935b
vigorouscoding <kai.schwaiger@structr.com>
2018-07-18 13:22:35 +0200

    Bugfix: Add missing file and added null check

---
+++ structr-ui/src/main/java/org/structr/web/common/AbstractMapComparator.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2010-2018 Structr GmbH
+ *
+ * This file is part of Structr <http://structr.org>.
+ *
+ * Structr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Structr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.structr.web.common;
+
+import java.util.Comparator;
+import java.util.Map;
+
+public abstract class AbstractMapComparator<T> implements Comparator<Map<String, T>> {
+
+	@Override
+	public int compare(Map<String, T> o1, Map<String, T> o2) {
+		return getKey(o1).compareTo(getKey(o2));
+	}
+
+	public abstract String getKey (Map<String, T> map);
+}
