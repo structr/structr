485dc378200159a19d6b47137b8ff3d6265a0e65
vigorouscoding <kai.schwaiger@structr.com>
2018-07-31 17:26:59 +0200

    added original license for 2FA utility

--- structr-ui/src/main/java/org/structr/web/common/TimeBasedOneTimePasswordHelper.java
+++ structr-ui/src/main/java/org/structr/web/common/TimeBasedOneTimePasswordHelper.java
@@ -28,6 +28,22 @@ import javax.crypto.spec.SecretKeySpec;
 /**
  * Based on https://github.com/j256/two-factor-auth but extended to support more than 6 digits for the OTP length
  *
+ * ISC License
+ *
+ * Copyright 2017, Gray Watson
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
  * Java implementation for the Time-based One-Time Password (TOTP) two factor authentication algorithm. To get this to
  * work you:
  *
