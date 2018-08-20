structr/structr

commit 9095f8c75eb5f30d2a5e6dd0cee0c5bebae634d8

Next iteration of flow editor integration: Adds CSS overwrites, buttons with basic functionality like add, delete, run, reset view

structr-ui/src/main/resources/structr/js/icons.js
@@ -20,6 +20,7 @@ var _Icons = {
 	add_icon: 'icon/add.png',
 	delete_icon: 'icon/delete.png',
 	delete_disabled_icon: 'icon/delete_gray.png',
+	add_brick_icon: 'icon/brick_add.png',
 	delete_brick_icon: 'icon/brick_delete.png',
 	edit_icon: 'icon/pencil.png',
 	wrench_icon: 'icon/wrench.png',
@@ -52,8 +53,8 @@ var _Icons = {
 	accept_icon: 'icon/accept.png',
 	push_file_icon: 'icon/page_white_get.png',
 	pull_file_icon: 'icon/page_white_put.png',
-	exec_cypher_icon: 'icon/control_play_blue.png',
-	exec_rest_icon: 'icon/control_play.png',
+	exec_icon: 'icon/control_play_blue.png',
+	exec_blue_icon: 'icon/control_play.png',
 	arrow_undo_icon: 'icon/arrow_undo.png',
 	information_icon: 'icon/information.png',
 	help_icon: 'icon/help.png',
@@ -126,6 +127,7 @@ var _Icons = {
 			case _Icons.add_icon:                     return 'sprite-add';
 			case _Icons.delete_icon:                  return 'sprite-delete';
 			case _Icons.delete_disabled_icon:         return 'sprite-delete_gray';
+			case _Icons.add_brick_icon:               return 'sprite-brick_add';
 			case _Icons.delete_brick_icon:            return 'sprite-brick_delete';
 			case _Icons.edit_icon:                    return 'sprite-pencil';
 			case _Icons.wrench_icon:                  return 'sprite-wrench';
@@ -158,8 +160,8 @@ var _Icons = {
 			case _Icons.accept_icon:                  return 'sprite-accept';
 			case _Icons.push_file_icon:               return 'sprite-page_white_get';
 			case _Icons.pull_file_icon:               return 'sprite-page_white_put';
-			case _Icons.exec_cypher_icon:             return 'sprite-control_play_blue';
-			case _Icons.exec_rest_icon:               return 'sprite-control_play';
+			case _Icons.exec_icon:                    return 'sprite-control_play_blue';
+			case _Icons.exec_icon:                    return 'sprite-control_play';
 			case _Icons.arrow_undo_icon:              return 'sprite-arrow_undo';
 			case _Icons.information_icon:             return 'sprite-information';
 			case _Icons.refresh_icon:                 return 'sprite-arrow_refresh';

structr-ui/src/main/resources/structr/css/flow-editor.css
@@ -1,0 +1,159 @@
+/**
+ * Copyright (C) 2010-2018 Structr GmbH
+ *
+ * This file is part of Structr <http://structr.org>.
+ *
+ * Structr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Structr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+@charset "UTF-8";
+
+#flows-canvas-container {
+	min-width: 600px;
+}
+
+#flows-canvas-container button.run_flow_icon {
+	position: absolute;
+	right: 29px;
+}
+
+#flows-canvas-container button.reset_view_icon {
+	position: absolute;
+	right: 124px;
+}
+
+#flows-canvas-container button.layout_icon {
+	position: absolute;
+	right: 220px;
+}
+
+/** Overwrite styles of Flow Editor for Structr UI integration */
+
+#nodeEditor {
+	background-color: transparent;
+	background-image: none;
+}
+
+#nodeEditor .node {
+	color: #555;
+	background-color: #f1f1f1;
+	box-shadow: 0 0 12px rgba(127,127,127,.1);
+    margin: 0 ! important;
+    opacity: .9;
+	border: 1px solid rgba(0,0,0,.125);
+	padding: 0;
+}
+
+#nodeEditor .node .title {
+	border: none;
+	font-family: inherit;
+	color: #555;
+	font-weight: normal;
+}
+
+#nodeEditor .node .startNode {
+    background-color: #81ce25;
+}
+
+#nodeEditor .node .startNode:hover {
+    background-color: #bfe27f;
+}
+
+#nodeEditor .node.selected {
+    background: #e0e0e0 ! important;
+    border: 1px solid rgba(0,0,0,.125);
+}
+
+#nodeEditor .node .input-title, #nodeEditor .node .output-title, #nodeEditor .node .control-title {
+    color: #222;
+    font-family: inherit;
+}
+
+#nodeEditor .node .control-textarea {
+	color: #222;
+	background-color: #fff;
+}
+
+#nodeEditor .node .control-select {
+	color: #222;
+	background-color: #fff;
+	font-family: inherit;
+	border: 1px solid rgba(0,0,0,.125);
+}
+
+#nodeEditor select, #nodeEditor input {
+	border-radius: 0;
+	color: #222;
+	background-color: #fff;
+	font-family: inherit;
+	border: 1px solid rgba(0,0,0,.125);
+}
+
+#nodeEditor .node .socket:after {
+	border-color: transparent transparent transparent #555;
+}
+
+#executionResult pre {
+	font-size: 1em ! important;
+	margin-top: 27px;
+}
+
+#executionResult {
+	top: 60px ! important;
+	height: auto ! important;
+}
+
+#executionResult span.close {
+	margin-top: -29px;
+	display: inline-block;
+    position: absolute;
+    right: 69px;
+}
+
+.context-menu {
+	border: 1px solid rgba(0,0,0,.125) ! important;
+	padding: 0 ! important;
+	font-family: inherit ! important;
+}
+
+.context-menu .item {
+	font-size: em;
+	width: auto ! important;
+	min-width: 200px;
+	color: #222 ! important;
+	background-color: #fff ! important;
+	border: none ! important;
+	border-bottom: 1px solid rgba(0,0,0,.125) ! important;
+	border-radius: 0 ! important;
+	padding: 6px ! important;
+	margin: 0;
+	margin-left: 0 ! important;
+}
+
+.context-menu .item:hover {
+	background-color: #81ce25 ! important;
+}
+
+.context-menu .item:first-child {
+	border-radius: 0 ! important;
+}
+
+.context-menu .item:last-child {
+	border-bottom: none ! important;
+}
+
+.context-menu .item .subitems {
+	border: 1px solid rgba(0,0,0,.125) ! important;
+	border-radius: 0 ! important;
+	font-family: inherit ! important;
+}

structr-ui/src/main/resources/structr/css/main.css
@@ -75,7 +75,7 @@ a:hover, a:active {
 
 textarea {
 	font-size: 1em;
-	border: 1px solid #eee;
+	border: 1px solid rgba(0,0,0,.125);
 }
 
 button, textarea, input, select {
@@ -84,7 +84,7 @@ button, textarea, input, select {
 }
 
 input {
-	border: 1px solid #eee;
+	border: 1px solid rgba(0,0,0,.125);
 	padding: 4px 4px;
 }
 
@@ -1435,8 +1435,7 @@ button.action:hover {
 }
 
 #code-main .selected, #code-main .selected:hover,
-#files-main .selected, #files-main .selected:hover,
-#flows-main .selected, #flows-main .selected:hover {
+#files-main .selected, #files-main .selected:hover {
 	color: #333 ! important;
 	border: 1px solid transparent ! important;
 	background-color: #81ce25 ! important;
@@ -3259,11 +3258,32 @@ td#cascading-options, td#propagation-options {
 #folder-contents-container, #code-contents-container, #contents-contents-container, #crawler-list-container, #flows-canvas-container {
 	display: table-cell;
 	vertical-align: top;
-	padding: 12px 12px 12px 10px;
+	padding: 12px 12px 12px 8px;
 }
 
-#folder-contents-container select, #code-contents-container select, #contents-contents-container select, #crawler-list-container select, #flows-canvas-container select {
-	padding: 3px;
+#folder-contents-container select, #code-contents-container select, #contents-contents-container select, #crawler-list-container select, #schema-container select, #flows-canvas-container select {
+	padding: 2px 2px 3px 2px;
+	border: 1px solid rgba(0,0,0,.125);
+	/*margin-top: -1px;*/
+	margin-right: 4px;
+	background-color: #f1f1f1;
+}
+
+#folder-contents-container select, #code-contents-container select, #contents-contents-container:first-child select, #crawler-list-container select, #flows-canvas-container select {
+	margin-left: 0;
+}
+
+#folder-contents-container select:hover, #code-contents-container select:hover, #contents-contents-container select:hover, #crawler-list-container select:hover, #flows-canvas-container select:hover {
+	background-color: #fff;
+}
+
+#folder-contents-container button, #code-contents-container button, #contents-contents-container button, #crawler-list-container button, #flows-canvas-container button, #crud-buttons button {
+	margin-right: 8px;
+}
+
+#flows-canvas-container .input-and-button input {
+	margin-top: 0;
+	height: 22px;
 }
 
 #folder-contents, #code-contents, #contents-contents, #crawler-list, #flows-canvas {
@@ -3272,12 +3292,16 @@ td#cascading-options, td#propagation-options {
 	box-shadow: 0px 0px 36px rgba(127,127,127,.1);
 	border: 1px solid rgba(0,0,0,.125);
 	padding: 12px;
-	margin-top: 12px;
+	margin-top: 8px;
 	overflow: auto;
 	font-size: 9pt;
 	position: absolute;
 }
 
+ #flows-canvas {
+	 padding: 0;
+ }
+ 
 #folder-contents h1, #code-contents h1, #contents-contents h1, #crawler-list h1, #flows-canvas h1 {
 	margin: 0px;
 }

structr-ui/src/main/resources/structr/index.html
@@ -19,6 +19,7 @@
 		<link rel="stylesheet" type="text/css" media="screen" href="css/crud.css" >
 		<link rel="stylesheet" type="text/css" media="screen" href="css/graph_editor.css" >
 		<link rel="stylesheet" type="text/css" media="screen" href="css/lib/cropper.min.css" >
+		<link rel="stylesheet" type="text/css" media="screen" href="css/flow-editor.css" >
 		<link rel="icon" href="favicon.ico" type="image/x-icon">
 		<script src="js/lib/jquery-1.11.1.min.js"></script>
 		<script src="js/lib/jquery-ui-1.11.0.custom.min.js"></script>
@@ -83,7 +84,7 @@
 		<script src="js/virtual-types.js"></script>
 		<script src="js/mail-templates.js"></script>
 		<script src="js/refactoring-helper.js"></script>
-		<script src="js/flows.js"></script>
+		<script src="js/flows.js" type="module"></script>
 	</head>
 	<body>
 		<div id="info-area" class="blockMsg"></div>

structr-ui/src/main/resources/structr/js/code.js
@@ -55,7 +55,7 @@ var _Code = {
 
 		if (codeContents) {
 			codeContents.css({
-				height: windowHeight - headerOffsetHeight - 48 + 'px'
+				height: windowHeight - headerOffsetHeight - 44 + 'px'
 			});
 		}
 

structr-ui/src/main/resources/structr/js/contents.js
@@ -74,7 +74,7 @@ var _Contents = {
 
 		if (contentsContents) {
 			contentsContents.css({
-				height: windowHeight - headerOffsetHeight - 48 + 'px'
+				height: windowHeight - headerOffsetHeight - 41 + 'px'
 			});
 		}
 

structr-ui/src/main/resources/structr/js/files.js
@@ -99,7 +99,7 @@ var _Files = {
 
 		if (folderContents) {
 			folderContents.css({
-				height: windowHeight - headerOffsetHeight - 48 + 'px'
+				height: windowHeight - headerOffsetHeight - 44 + 'px'
 			});
 		}
 
@@ -170,12 +170,12 @@ var _Files = {
 		Structr.initVerticalSlider($('.column-resizer', filesMain), filesResizerLeftKey, 204, _Files.moveResizer);
 
 		$('#folder-contents-container').prepend(
-				'<button class="add_folder_icon button"><i title="Add Folder" class="' + _Icons.getFullSpriteClass(_Icons.add_folder_icon) + '" /> Add Folder</button>'
-				+ '<button class="add_file_icon button"><i title="Add File" class="' + _Icons.getFullSpriteClass(_Icons.add_file_icon) + '" /> Add File</button>'
-				+ '<button class="add_minified_css_file_icon button"><i title="Add Minified CSS File" class="' + _Icons.getFullSpriteClass(_Icons.minification_dialog_css_icon) + '" />' + ' Add Minified CSS File</button>'
-				+ '<button class="add_minified_js_file_icon button"><i title="Add Minified JS File" class="' + _Icons.getFullSpriteClass(_Icons.minification_dialog_js_icon) + '" />' + ' Add Minified JS File</button>'
-				+ '<button class="duplicate_finder button"><i title="Find duplicates" class="' + _Icons.getFullSpriteClass(_Icons.search_icon) + '" /> Find Duplicates</button>'
-				+ '<button class="mount_folder button"><i title="Mount Folder" class="' + _Icons.getFullSpriteClass(_Icons.folder_connect_icon) + '" /> Mount Folder</button>'
+				'<button class="add_folder_icon button"><i title="Add Folder" class="' + _Icons.getFullSpriteClass(_Icons.add_folder_icon) + '"></i> Add Folder</button>'
+				+ '<button class="add_file_icon button"><i title="Add File" class="' + _Icons.getFullSpriteClass(_Icons.add_file_icon) + '"></i> Add File</button>'
+				+ '<button class="add_minified_css_file_icon button"><i title="Add Minified CSS File" class="' + _Icons.getFullSpriteClass(_Icons.minification_dialog_css_icon) + '"></i>' + ' Add Minified CSS File</button>'
+				+ '<button class="add_minified_js_file_icon button"><i title="Add Minified JS File" class="' + _Icons.getFullSpriteClass(_Icons.minification_dialog_js_icon) + '"></i>' + ' Add Minified JS File</button>'
+				+ '<button class="duplicate_finder button"><i title="Find duplicates" class="' + _Icons.getFullSpriteClass(_Icons.search_icon) + '"></i> Find Duplicates</button>'
+				+ '<button class="mount_folder button"><i title="Mount Folder" class="' + _Icons.getFullSpriteClass(_Icons.folder_connect_icon) + '"></i> Mount Folder</button>'
 				);
 
 		$('.add_file_icon', main).on('click', function(e) {

structr-ui/src/main/resources/structr/js/flows.js
@@ -16,7 +16,16 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
  */
-var main, flowsMain, flowsTree, flowsCanvas;
+import { Persistence }         from "./flow-editor/src/js/persistence/Persistence.js";
+import { FlowContainer }       from "./flow-editor/src/js/editor/entities/FlowContainer.js";
+import { FlowEditor }          from "./flow-editor/src/js/editor/FlowEditor.js";
+import { FlowConnectionTypes } from "./flow-editor/src/js/editor/FlowConnectionTypes.js";
+import { LayoutModal }         from "./flow-editor/src/js/editor/utility/LayoutModal.js";
+
+import { Component }           from "./lib/structr/Component.js";
+
+let main, flowsMain, flowsTree, flowsCanvas;
+let editor, flowId;
 var drop;
 var selectedElements = [];
 var activeMethodId, methodContents = {};
@@ -28,8 +37,8 @@ var flowsLastOpenMethodKey = 'structrFlowsLastOpenMethodKey_' + port;
 var flowsResizerLeftKey = 'structrFlowsResizerLeftKey_' + port;
 var activeFlowsTabPrefix = 'activeFlowsTabPrefix' + port;
 
-$(document).ready(function() {
-	Structr.registerModule(_Flows);
+document.addEventListener("DOMContentLoaded", function() {
+    Structr.registerModule(_Flows);
 });
 
 var _Flows = {
@@ -40,96 +49,126 @@ var _Flows = {
 
 		Structr.makePagesMenuDroppable();
 		Structr.adaptUiToAvailableFeatures();
-
+		
 	},
 	resize: function() {
 
-		var windowHeight = $(window).height();
+		var windowHeight = window.innerHeight;
 		var headerOffsetHeight = 100;
 
 		if (flowsTree) {
-			flowsTree.css({
-				height: windowHeight - headerOffsetHeight + 7 + 'px'
-			});
+			flowsTree.style.height = windowHeight - headerOffsetHeight + 5 + 'px';
 		}
 
 		if (flowsCanvas) {
-			flowsCanvas.css({
-				height: windowHeight - headerOffsetHeight - 48 + 'px'
-			});
+			flowsCanvas.style.height = windowHeight - headerOffsetHeight - 24 + 'px';
 		}
 
 		_Flows.moveResizer();
 		Structr.resize();
 
-		if (flowsCanvas) {
-			flowsCanvas.find('.node').each(function() {
-				_Entities.setMouseOver($(this), true);
-			});
-		}
-
 	},
 	moveResizer: function(left) {
 		left = left || LSWrapper.getItem(flowsResizerLeftKey) || 300;
-		$('.column-resizer', flowsMain).css({ left: left });
+		document.querySelector('#flows-main .column-resizer').style.left = left + 'px';
 
-		$('#flows-tree').css({width: left - 14 + 'px'});
-		$('#flows-canvas').css({left: left + 8 + 'px', width: $(window).width() - left - 47 + 'px'});
+		document.querySelector('#flows-tree').style.width   = left - 14 + 'px';
+		document.querySelector('#flows-canvas').style.left  = left +  8 + 'px';
+		document.querySelector('#flows-canvas').style.width = window.innerWidth - left - 47 + 'px';
 	},
 	onload: function() {
 
 		_Flows.init();
+		
+		main = document.querySelector('#main');
 
-		main.append('<div id="flows-main"><div class="column-resizer"></div><div class="fit-to-height" id="flows-tree-container"><div id="flows-tree"></div></div><div class="fit-to-height" id="flows-canvas-container"><div id="flows-button-container"></div><div id="flows-canvas"></div></div>');
-		flowsMain = $('#flows-main');
+		main.innerHTML = '<div id="flows-main"><div class="column-resizer"></div><div class="fit-to-height" id="flows-tree-container"><div id="flows-tree"></div></div><div class="fit-to-height" id="flows-canvas-container"><div id="flows-canvas"></div></div>';
+		flowsMain = document.querySelector('#flows-main');
+		
+		let markup = `
+			<div class="input-and-button"><input id="name-input" type="text" size="12" placeholder="Enter flow name"><button id="create-new-flow" class="action btn"><i class="${_Icons.getFullSpriteClass(_Icons.add_icon)}"></i> Add</button></div>
+			<select id="add-flow-node"><option value="">Add node</option></select>
+			<button class="delete_flow_icon button disabled"><i title="Delete" class="${_Icons.getFullSpriteClass(_Icons.delete_icon)}"></i> Delete flow</button>
+			<button class="run_flow_icon button disabled"><i title="Run" class="${_Icons.getFullSpriteClass(_Icons.exec_icon)}"></i> Run</button>
+			<button class="reset_view_icon button"><i title="Reset view" class="${_Icons.getFullSpriteClass(_Icons.refresh_icon)}"></i> Reset view</button>
+			<button class="layout_icon button disabled"><i title="Layout" class="${_Icons.getFullSpriteClass(_Icons.wand_icon)}"></i> Layout</button>
+		`;
+		
+		document.querySelector('#flows-canvas-container').insertAdjacentHTML('afterbegin', markup);
 
-		flowsTree = $('#flows-tree');
-		flowsCanvas = $('#flows-canvas');
-
-		_Flows.moveResizer();
-		Structr.initVerticalSlider($('.column-resizer', flowsMain), flowsResizerLeftKey, 204, _Flows.moveResizer);
-
-		$.jstree.defaults.core.themes.dots      = false;
-		$.jstree.defaults.dnd.inside_pos        = 'last';
-		$.jstree.defaults.dnd.large_drop_target = true;
-
-		flowsTree.on('select_node.jstree', function(evt, data) {
-
-			if (data.node && data.node.data && data.node.data.type) {
-
-				if (data.node.data.type === 'FlowContainer') {
-
-					// display flow canvas
-					console.log('load flow', data.node.id);
-					
-					flowsCanvas.append('<iframe width="100%" height="100%" src="js/flow-editor/src/html/pages/flow.html?id=' + data.node.id + '"></iframe>');
-					
-				} else {
-
-					if (data.node.state.opened) {
-						flowsTree.jstree('close_node', data.node.id);
-						data.node.state.openend = false;
-					} else {
-						flowsTree.jstree('open_node', data.node.id);
-						data.node.state.openend = true;
-					}
+		let persistence = new Persistence();
+		
+		function createFlow(inputElement) {
+            let name = inputElement.value;
+            inputElement.value = "";
+            persistence.createNode({type:"FlowContainer", name:name}).then( (r) => {
+               _Flows.refreshTree();
+            });
+        }
+		
+		function deleteFlow(id) {
+			if (!document.querySelector(".delete_flow_icon").getAttribute('class').includes('disabled')) {
+				if (confirm('Really delete flow ' + id + '?')) {
+					persistence.deleteNode({type:"FlowContainer", id: flowId}).then(() => {
+						_Flows.refreshTree();
+					});
 				}
+			}
+		}
+			
+		document.querySelector('#create-new-flow').onclick = () => createFlow(document.getElementById('name-input'));
+		document.querySelector('.reset_view_icon').onclick = () => editor.resetView();
+		
+		document.querySelector('.delete_flow_icon').onclick = () => deleteFlow(flowId);
+		document.querySelector('.layout_icon').onclick = function() {
+			if (!this.getAttribute('class').includes('disabled')) {
+				new LayoutModal(editor);
+			}
+		};
 
-			} else {
+		document.querySelector('#add-flow-node').onchange = () => function() {
+			console.log('add flow node');
+		};
 
-				if (data.node.state.opened) {
-					flowsTree.jstree('close_node', data.node.id);
-					data.node.state.openend = false;
-				} else {
-					flowsTree.jstree('open_node', data.node.id);
-					data.node.state.openend = true;
-				}
+		document.querySelector('.run_flow_icon').addEventListener('click', function() {
+			if (!this.getAttribute('class').includes('disabled')) {
+				editor.executeFlow();
 			}
 		});
 
+		flowsTree = document.querySelector('#flows-tree');
+		flowsCanvas = document.querySelector('#flows-canvas');
+
+		_Flows.moveResizer();
+		Structr.initVerticalSlider(document.querySelector('#flows-main .column-resizer'), flowsResizerLeftKey, 204, _Flows.moveResizer);
+
+//		$.jstree.defaults.core.themes.dots      = false;
+//		$.jstree.defaults.dnd.inside_pos        = 'last';
+//		$.jstree.defaults.dnd.large_drop_target = true;
+
+		let displayFlow = function(e) {
+
+			let id = e.target.closest('.jstree-node') ? e.target.closest('.jstree-node').getAttribute('id') : e.target.closest('[data-id]').getAttribute('data-id');
+			
+			// display flow canvas
+			flowsCanvas.innerHTML = '<div id="nodeEditor" class="node-editor"></div>';
+			
+			_Flows.initFlow(id);
+		}
+
+		_Flows.components = {
+			'flowsTree':  new Component('folderTree',  '#flows-tree'),
+			'flowsCanvas': new Component('flowsCanvas', '#flows-canvas')
+		};
+		
+		_Flows.components.flowsTree
+				.on('click', ['.jstree-node'], displayFlow)
+		;
+
 		_TreeHelper.initTree(flowsTree, _Flows.treeInitFunction, 'structr-ui-flows');
 
-		$(window).off('resize').resize(function() {
+		window.removeEventListener('resize', resizeFunction);
+		window.addEventListener('resize', function() {
 			_Flows.resize();
 		});
 
@@ -161,21 +200,6 @@ var _Flows = {
 						id: 'root',
 						text: 'Flows',
 						children: true,
-//						children: [
-//							{ id: 'custom', text: 'Custom', children: true, icon: _Icons.folder_icon },
-//							{
-//								id: 'builtin',
-//								text: 'Built-In',
-//								children: [
-//									{ id: 'core', text: 'Core', children: true, icon: _Icons.folder_icon },
-//									{ id: 'ui',  text: 'Ui',  children: [
-//										{ id: 'web', text: 'Pages', children: true, icon: _Icons.folder_icon },
-//										{ id: 'html', text: 'Html', children: true, icon: _Icons.folder_icon }
-//									], icon: _Icons.folder_icon }
-//								],
-//								icon: _Icons.folder_icon
-//							},
-//						],
 						icon: _Icons.structr_logo_small,
 						path: '/',
 						state: {
@@ -199,8 +223,8 @@ var _Flows = {
 
 	},
 	unload: function() {
-		fastRemoveAllChildren($('.searchBox', main));
-		fastRemoveAllChildren($('#flows-main', main));
+		fastRemoveAllChildren(document.querySelector('#main .searchBox'));
+		fastRemoveAllChildren(document.querySelector('#main #flows-main'));
 	},
 	load: function(id, callback) {
 
@@ -223,18 +247,10 @@ var _Flows = {
 						}
 				}
 
-				var hasVisibleChildren = false;
-
-				if (d.flowNodes) {
-					d.flowNodes.forEach(function(m) {
-						hasVisibleChildren = true;
-					});
-				}
-
 				list.push({
 					id: d.id,
 					text:  d.name ? d.name : '[unnamed]',
-					children: hasVisibleChildren,
+					children: false,
 					icon: 'fa ' + icon,
 					data: {
 						type: d.type
@@ -253,21 +269,6 @@ var _Flows = {
 
 			switch (id) {
 
-//				case 'custom':
-//					Command.query('SchemaNode', methodPageSize, methodPage, 'name', 'asc', { isBuiltinType: false}, displayFunction, true);
-//					break;
-//				case 'core':
-//					Command.query('SchemaNode', methodPageSize, methodPage, 'name', 'asc', { isBuiltinType: true, isAbstract:false, category: 'core' }, displayFunction, false);
-//					break;
-//				case 'web':
-//					Command.query('SchemaNode', methodPageSize, methodPage, 'name', 'asc', { isBuiltinType: true, isAbstract:false, category: 'ui' }, displayFunction, false);
-//					break;
-//				case 'html':
-//					Command.query('SchemaNode', methodPageSize, methodPage, 'name', 'asc', { isBuiltinType: true, isAbstract:false, category: 'html' }, displayFunction, false);
-//					break;
-//				case 'globals':
-//					Command.query('SchemaMethod', methodPageSize, methodPage, 'name', 'asc', {schemaNode: null}, displayFunction, true, 'ui');
-//					break;
 				default:
 					Command.query('FlowContainer', methodPageSize, methodPage, 'name', 'asc', {flowContainer: id}, displayFunction, true, 'ui');
 					break;
@@ -275,36 +276,59 @@ var _Flows = {
 		}
 
 	},
-	fileOrFolderCreationNotification: function (newFileOrFolder) {
-		if ((currentWorkingDir === undefined || currentWorkingDir === null) && newFileOrFolder.parent === null) {
-			_Flows.appendFileOrFolder(newFileOrFolder);
-		} else if ((currentWorkingDir !== undefined && currentWorkingDir !== null) && newFileOrFolder.parent && currentWorkingDir.id === newFileOrFolder.parent.id) {
-			_Flows.appendFileOrFolder(newFileOrFolder);
-		}
-	},
-	registerParentLink: function(d, triggerEl) {
+	initFlow: function(id) {
+		
+		flowId = id;
+        let persistence = new Persistence();
 
-		// Change working dir by click on folder icon
-		triggerEl.on('click', function(e) {
+        persistence.getNodesById(id, new FlowContainer()).then( r => {
+            document.title = "Flow - " + r[0].name;
 
-			e.preventDefault();
-			e.stopPropagation();
+            let rootElement = document.querySelector("#nodeEditor");
+            editor = new FlowEditor(rootElement, r[0]);
 
-			if (d.parentId) {
+            editor.waitForInitialization().then( () => {
 
-				flowsTree.jstree('open_node', $('#' + d.parentId), function() {
-					if (d.name === '..') {
-						$('#' + d.parentId + '_anchor').click();
-					} else {
-						$('#' + d.id + '_anchor').click();
-					}
-				});
+                let promises = [];
 
-			} else {
-				$('#' + d.id + '_anchor').click();
-			}
+                r[0].flowNodes.forEach(node => {
+                    promises.push(persistence.getNodesById(node.id).then(n => editor.renderNode(n[0])));
+                });
 
-			return false;
-		});
+                Promise.all(promises).then(() => {
+                    for (let [name, con] of Object.entries(FlowConnectionTypes.getInst().getAllConnectionTypes())) {
+
+                        persistence.getNodesByClass(con).then(relNodes => {
+
+                            relNodes.forEach(rel => {
+
+                                if (Array.isArray(rel)) {
+                                    rel = rel[0];
+                                }
+
+                                if (r[0].flowNodes.filter(el => el.id === rel.sourceId).length > 0 && r[0].flowNodes.filter(el => el.id === rel.targetId).length > 0) {
+                                    editor.connectNodes(rel);
+                                }
+
+                            });
+
+                        });
+
+                    }
+
+                    editor.applySavedLayout();
+                    editor._editor.view.update();
+					editor.resetView();
+					
+					// activate buttons
+					document.querySelector('.run_flow_icon').classList.remove('disabled');
+					document.querySelector('.delete_flow_icon').classList.remove('disabled');
+					document.querySelector('.layout_icon').classList.remove('disabled');
+
+                });
+
+            });
+
+        });		
 	}
 };

commit b7064ed1cac7617a06cf65e255be777ae76d2283

Bugfix: Fixes wrong icon introduced in 9095f8c

structr-ui/src/main/resources/structr/js/icons.js
@@ -160,7 +160,7 @@ var _Icons = {
 			case _Icons.accept_icon:                  return 'sprite-accept';
 			case _Icons.push_file_icon:               return 'sprite-page_white_get';
 			case _Icons.pull_file_icon:               return 'sprite-page_white_put';
-			case _Icons.exec_icon:                    return 'sprite-control_play_blue';
+			case _Icons.exec_blue_icon:               return 'sprite-control_play_blue';
 			case _Icons.exec_icon:                    return 'sprite-control_play';
 			case _Icons.arrow_undo_icon:              return 'sprite-arrow_undo';
 			case _Icons.information_icon:             return 'sprite-information';