e301528e7a4a55da4718266e5c0e3908880e82af
Christian Kramp <christian.kramp@structr.com>
2018-12-13 15:49:42 +0100

    Moves MailFetchTask to an isolated task to prevent blocking issues.

--- structr-modules/structr-advanced-mail-module/src/main/java/org/structr/mail/service/MailService.java
+++ structr-modules/structr-advanced-mail-module/src/main/java/org/structr/mail/service/MailService.java
@@ -18,6 +18,8 @@
  */
 package org.structr.mail.service;
 
+import com.sun.mail.util.BASE64DecoderStream;
+import io.netty.util.internal.ConcurrentSet;
 import org.apache.commons.lang.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,13 +39,18 @@ import org.structr.schema.SchemaService;
 
 import javax.mail.*;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @ServiceDependency(SchemaService.class)
 public class MailService extends Thread implements RunnableService {
 	private static final Logger logger                  = LoggerFactory.getLogger(MailService.class.getName());
+	private static final ExecutorService threadExecutor = Executors.newFixedThreadPool(10);
 	private boolean run                                 = false;
 	private Set<Class> supportedCommands                = null;
+	private Set<Mailbox> processingMailboxes            = null;
 
 	public static final SettingsGroup mailGroup         = new SettingsGroup("mail","Advanced Mail Configuration");
 	public static final Setting<Integer> maxEmails      = new IntegerSetting(mailGroup,"Mail", "mail.maxEmails",25);
@@ -54,6 +61,8 @@ public class MailService extends Thread implements RunnableService {
 
 		supportedCommands = new LinkedHashSet<>();
 		supportedCommands.add(FetchMailsCommand.class);
+
+		processingMailboxes = new ConcurrentSet<>();
 
 		super.setDaemon(true);
 	}
@@ -116,101 +125,17 @@ public class MailService extends Thread implements RunnableService {
 	}
 
 	public void fetchMails(final Mailbox mb) {
-		try {
 
-			String host = mb.getHost();
-			String mailProtocol = mb.getMailProtocol().toString();
-			String user = mb.getUser();
-			String password = mb.getPassword();
-			Integer port = mb.getPort();
+		if (processingMailboxes.contains(mb)) {
 
-			Properties properties = new Properties();
+			return;
+		} else {
 
-			properties.put("mail." + mailProtocol + ".host", host);
-			properties.put("mail." + mailProtocol + ".starttls.enable", "true");
-
-			if (port != null) {
-				properties.put("mail." + mailProtocol + ".port", port);
-			}
-
-			Session emailSession = Session.getDefaultInstance(properties);
-
-			Store store = emailSession.getStore(mailProtocol);
-
-			store.connect(host, user, password);
-
-			Folder emailFolder = store.getFolder("INBOX");
-			emailFolder.open(Folder.READ_ONLY);
-
-			Message[] messages = emailFolder.getMessages();
-
-			ArrayUtils.reverse(messages);
-
-			App app = StructrApp.getInstance();
-
-			try (Tx tx = app.tx()) {
-
-				for (int i = 0; i < messages.length; i ++) {
-					// Limit fetched emails
-					if (i >= maxEmails.getValue(30000)) {
-						break;
-					}
-
-					Message message = messages[i];
-
-					PropertyMap pm = new PropertyMap();
-
-					String from = Arrays.stream(message.getFrom()).map(Address::toString).reduce("", (a, b) -> a.equals("") ? b : a + "," + b);
-					String to = Arrays.stream(message.getAllRecipients()).map(Address::toString).reduce("", (a, b) -> a.equals("") ? b : a + "," + b);
-
-					Mail existingMail = app.nodeQuery(Mail.class).and(StructrApp.key(Mail.class, "subject"), message.getSubject()).and(StructrApp.key(Mail.class, "from"), from).and(StructrApp.key(Mail.class, "to"),to).and(StructrApp.key(Mail.class, "receivedDate"),message.getReceivedDate()).and(StructrApp.key(Mail.class, "sentDate"),message.getSentDate()).getFirst();
-
-					if (existingMail == null) {
-
-						pm.put(StructrApp.key(Mail.class, "subject"), message.getSubject());
-						pm.put(StructrApp.key(Mail.class, "from"), from);
-						pm.put(StructrApp.key(Mail.class, "to"), to);
-						pm.put(StructrApp.key(Mail.class, "folder"), message.getFolder().getFullName());
-						pm.put(StructrApp.key(Mail.class, "receivedDate"), message.getReceivedDate());
-						pm.put(StructrApp.key(Mail.class, "sentDate"), message.getSentDate());
-						pm.put(StructrApp.key(Mail.class, "mailbox"), mb);
-
-						// Handle content extraction
-						String content = null;
-						Object contentObj = message.getContent();
-						if (contentObj instanceof Part) {
-
-							content = getText((Part)contentObj);
-						} else if (contentObj  instanceof Multipart) {
-
-							content = getText(((Multipart)contentObj).getParent());
-						} else {
-
-							content = contentObj.toString();
-						}
-
-
-						pm.put(StructrApp.key(Mail.class, "content"), content);
-
-						app.create(Mail.class, pm);
-
-					}
-				}
-
-				tx.success();
-			}
-
-			//close the store and folder objects
-			emailFolder.close(false);
-			store.close();
-
-		} catch (MessagingException ex) {
-			logger.error("Error while updating Mails: " + ex.getLocalizedMessage());
-		} catch (FrameworkException | IOException ex) {
-			logger.error("Error while updating Mails: " + ex.getMessage());
-		} catch (Throwable ex) {
-			logger.error("Error while updating Mails: " + ex.getLocalizedMessage());
+			processingMailboxes.add(mb);
 		}
+
+		MailFetchTask task = new MailFetchTask(mb);
+		threadExecutor.submit(task);
 	}
 
 	private void fetchMailsForAllMailboxes() {
@@ -298,5 +223,124 @@ public class MailService extends Thread implements RunnableService {
 	@Override
 	public String getModuleName() {
 		return "advanced-mail";
+	}
+
+	private class MailFetchTask implements Runnable {
+		private final Mailbox mailbox;
+
+		public MailFetchTask(final Mailbox mailbox) {
+			this.mailbox = mailbox;
+		}
+
+		@Override
+		public void run() {
+			try {
+
+				String host = mailbox.getHost();
+				String mailProtocol = mailbox.getMailProtocol().toString();
+				String user = mailbox.getUser();
+				String password = mailbox.getPassword();
+				Integer port = mailbox.getPort();
+
+				if (host == null || mailProtocol == null || user == null || password == null) {
+					logger.warn("MailService::fetchMails: Could not retrieve mails from mailbox[" + mailbox.getUuid() + "], because not all required attributes were specified.");
+					return;
+				}
+
+				Properties properties = new Properties();
+
+				properties.put("mail." + mailProtocol + ".host", host);
+				properties.put("mail." + mailProtocol + ".starttls.enable", "true");
+
+				if (port != null) {
+					properties.put("mail." + mailProtocol + ".port", port);
+				}
+
+				Session emailSession = Session.getDefaultInstance(properties);
+
+				Store store = emailSession.getStore(mailProtocol);
+
+				store.connect(host, user, password);
+
+				Folder emailFolder = store.getFolder("INBOX");
+				emailFolder.open(Folder.READ_ONLY);
+
+				Message[] messages = emailFolder.getMessages();
+
+				ArrayUtils.reverse(messages);
+
+				App app = StructrApp.getInstance();
+
+				for (int i = 0; i < messages.length; i ++) {
+					// Limit fetched emails
+					if (i >= maxEmails.getValue(25)) {
+						break;
+					}
+
+					Message message = messages[i];
+
+					PropertyMap pm = new PropertyMap();
+
+					String from = Arrays.stream(message.getFrom()).map(Address::toString).reduce("", (a, b) -> a.equals("") ? b : a + "," + b);
+					String to = Arrays.stream(message.getAllRecipients()).map(Address::toString).reduce("", (a, b) -> a.equals("") ? b : a + "," + b);
+
+					try (Tx tx = app.tx()) {
+
+						Mail existingMail = app.nodeQuery(Mail.class).and(StructrApp.key(Mail.class, "subject"), message.getSubject()).and(StructrApp.key(Mail.class, "from"), from).and(StructrApp.key(Mail.class, "to"),to).and(StructrApp.key(Mail.class, "receivedDate"),message.getReceivedDate()).and(StructrApp.key(Mail.class, "sentDate"),message.getSentDate()).getFirst();
+
+						if (existingMail == null) {
+
+							pm.put(StructrApp.key(Mail.class, "subject"), message.getSubject());
+							pm.put(StructrApp.key(Mail.class, "from"), from);
+							pm.put(StructrApp.key(Mail.class, "to"), to);
+							pm.put(StructrApp.key(Mail.class, "folder"), message.getFolder().getFullName());
+							pm.put(StructrApp.key(Mail.class, "receivedDate"), message.getReceivedDate());
+							pm.put(StructrApp.key(Mail.class, "sentDate"), message.getSentDate());
+							pm.put(StructrApp.key(Mail.class, "mailbox"), mailbox);
+
+							// Handle content extraction
+							String content = null;
+							Object contentObj = message.getContent();
+							if (contentObj instanceof Part) {
+
+								content = getText((Part)contentObj);
+							} else if (contentObj  instanceof Multipart) {
+
+								content = getText(((Multipart) contentObj).getParent());
+							} else if (contentObj instanceof InputStream) {
+
+								logger.info("MailService: Can't process streamed content. Not implemented yet!");
+							} else {
+
+								content = contentObj.toString();
+							}
+
+
+							pm.put(StructrApp.key(Mail.class, "content"), content);
+
+							app.create(Mail.class, pm);
+
+						}
+
+						tx.success();
+
+					}
+
+				}
+
+				//close the store and folder objects
+				emailFolder.close(false);
+				store.close();
+
+			} catch (MessagingException ex) {
+				logger.error("Error while updating Mails: " + ex.getLocalizedMessage());
+			} catch (FrameworkException | IOException ex) {
+				logger.error("Error while updating Mails: " + ex.getMessage());
+			} catch (Throwable ex) {
+				logger.error("Error while updating Mails: " + ex.getLocalizedMessage());
+			}
+
+			processingMailboxes.remove(mailbox);
+		}
 	}
 }
