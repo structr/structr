7f7457cab0ccecdb1c155239eda53fdf98d7c474
Axel Morgner <axel@morgner.de>
2019-01-29 13:05:36 +0100

    Removes unused websocket GetPropertiesCommand (identical to GetCommand). Fixes bug in JS Command.getRelationship that caused the nodeId to be overwritten when custom properties were requested.

--- structr-ui/src/main/resources/structr/js/command.js
+++ structr-ui/src/main/resources/structr/js/command.js
@@ -116,9 +116,7 @@ var Command = {
 			}
 		};
 		if (properties !== null) {
-			obj.data = {
-				properties: properties
-			};
+			obj.data.properties = properties
 		}
 		_Logger.log(_LogType.WS[obj.command], 'getRelationship()', obj, callback);
 		return sendObj(obj, callback);
@@ -265,22 +263,6 @@ var Command = {
 			}
 		};
 		_Logger.log(_LogType.WS[obj.command], 'getProperty()', obj, callback);
-		return sendObj(obj, callback);
-	},
-	/**
-	 * Send a GET_PROPERTIES command to the server.
-	 *
-	 * The server will return the properties for the node with the given id.
-	 */
-	getProperties: function(id, properties, callback) {
-		var obj = {
-			command: 'GET_PROPERTIES',
-			id: id,
-			data: {
-				properties: properties
-			}
-		};
-		_Logger.log(_LogType.WS[obj.command], 'getProperties()', obj, callback);
 		return sendObj(obj, callback);
 	},
 	/**
