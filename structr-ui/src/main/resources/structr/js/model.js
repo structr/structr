9b75ce2fc338a673f1215c732eb29b5370e49c16
vigorouscoding <kai.schwaiger@structr.com>
2019-02-05 17:21:22 +0100

    Bugfix: Correctly update visibility flag in UI

--- structr-ui/src/main/resources/structr/js/model.js
+++ structr-ui/src/main/resources/structr/js/model.js
@@ -242,19 +242,18 @@ var StructrModel = {
 					obj[k] = newProperties[k];
 				});
 				StructrModel.refresh(obj.id);
-			}
+			};
+
+			let callback = function(newProperties) {
+				refreshObj(obj, newProperties);
+				StructrModel.callCallback(data.callback, obj);
+			};
 
 			if (data.relData && data.relData.sourceId) {
-				Command.getRelationship(data.id, data.relData.sourceId, Object.keys(obj).join(','), function(newProperties) {
-					refreshObj(obj, newProperties);
-				});
-				
+				Command.getRelationship(data.id, data.relData.sourceId, Object.keys(obj).join(','), callback);
 			} else {
-				Command.get(data.id, Object.keys(obj).join(','), function(newProperties) {
-					refreshObj(obj, newProperties);
-				});
+				Command.get(data.id, Object.keys(obj).join(','), callback);
 			}
-
 		}
 
 		return obj;
