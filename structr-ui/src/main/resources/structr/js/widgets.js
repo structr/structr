3c748688c761de7006a0b5258c249e74a7feb3bd
vigorouscoding <kai.schwaiger@structr.com>
2019-03-13 13:22:46 +0100

    Feature: Allow configuration of multiple widget servers

--- structr-ui/src/main/resources/structr/js/widgets.js
+++ structr-ui/src/main/resources/structr/js/widgets.js
@@ -17,133 +17,221 @@
  * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
  */
 var _Widgets = {
-	widgetServerKey: 'structrWidgetServerKey_' + port,
-	url: 'https://widgets.structr.org/structr/rest/widgets',
 	defaultWidgetServerUrl: 'https://widgets.structr.org/structr/rest/widgets',
+	widgetServerKey: 'structrWidgetServerKey_' + port,
+	applicationConfigurationDataNodeKey: 'remote_widget_server',
+
 	remoteWidgetData: [],
 	remoteWidgetFilterEl: undefined,
 	remoteWidgetFilter: undefined,
 	remoteWidgetsEl: undefined,
 	localWidgetsEl: undefined,
+	widgetServerSelector: undefined,
+
 	localWidgetsCollapsed: false,
 	remoteWidgetsCollapsed: false,
-	remoteWidgetsUrlInputEl: undefined,
-	remoteWidgetsUrlResetButton: undefined,
-	remoteWidgetsUrlApplyButton: undefined,
 
 	reloadWidgets: function() {
 
 		widgetsSlideout.find(':not(.compTab)').remove();
 
-		var widgetUrl = LSWrapper.getItem(_Widgets.widgetServerKey);
+		_Widgets.getConfiguredWidgetServers(function(serverConfigs) {
 
-		if (widgetUrl === null) {
-			widgetUrl = _Widgets.defaultWidgetServerUrl;
-		}
+			let templateConfig = {
+				localCollapsed: _Widgets.localWidgetsCollapsed === true,
+				remoteCollapsed: _Widgets.remoteWidgetsCollapsed === true
+			};
 
-		let templateConfig = {
-			localCollapsed: _Widgets.localWidgetsCollapsed === true,
-			remoteCollapsed: _Widgets.remoteWidgetsCollapsed === true,
-			remoteWidgetsUrl: widgetUrl
-		};
+			Structr.fetchHtmlTemplate('widgets/slideout', templateConfig, function(html) {
 
-		Structr.fetchHtmlTemplate('widgets/slideout', templateConfig, function(html) {
+				widgetsSlideout.append(html);
 
-			widgetsSlideout.append(html);
+				widgetsSlideout[0].querySelectorAll('a.tab-group-toggle').forEach(function(toggleLink) {
 
-			widgetsSlideout[0].querySelectorAll('a.tab-group-toggle').forEach(function(toggleLink) {
-
-				toggleLink.addEventListener('click', function(event) {
-					event.target.closest('.tab-group').classList.toggle('collapsed');
+					toggleLink.addEventListener('click', function(event) {
+						event.target.closest('.tab-group').classList.toggle('collapsed');
+					});
 				});
-			});
 
-			_Widgets.localWidgetsEl = $('#widgets', widgetsSlideout);
+				_Widgets.localWidgetsEl = $('#widgets', widgetsSlideout);
 
-			$('.add_widgets_icon', widgetsSlideout).on('click', function(e) {
-				e.stopPropagation();
-				Command.create({type: 'Widget'});
-			});
-
-			_Widgets.localWidgetsEl.droppable({
-				drop: function(e, ui) {
-					e.preventDefault();
+				$('.add_widgets_icon', widgetsSlideout).on('click', function(e) {
 					e.stopPropagation();
-					dropBlocked = true;
-					var sourceId = Structr.getId($(ui.draggable));
-					var sourceWidget = StructrModel.obj(sourceId);
+					Command.create({type: 'Widget'});
+				});
 
-					if (sourceWidget && sourceWidget.isWidget) {
-						if (sourceWidget.treePath) {
-							_Logger.log(_LogType.WIDGETS, 'Copying remote widget', sourceWidget);
+				_Widgets.localWidgetsEl.droppable({
+					drop: function(e, ui) {
+						e.preventDefault();
+						e.stopPropagation();
+						dropBlocked = true;
+						var sourceId = Structr.getId($(ui.draggable));
+						var sourceWidget = StructrModel.obj(sourceId);
 
-							Command.create({ type: 'Widget', name: sourceWidget.name + ' (copied)', source: sourceWidget.source, description: sourceWidget.description, configuration: sourceWidget.configuration }, function(entity) {
-								_Logger.log(_LogType.WIDGETS, 'Copied remote widget successfully', entity);
-								dropBlocked = false;
+						if (sourceWidget && sourceWidget.isWidget) {
+							if (sourceWidget.treePath) {
+								_Logger.log(_LogType.WIDGETS, 'Copying remote widget', sourceWidget);
+
+								Command.create({ type: 'Widget', name: sourceWidget.name + ' (copied)', source: sourceWidget.source, description: sourceWidget.description, configuration: sourceWidget.configuration }, function(entity) {
+									_Logger.log(_LogType.WIDGETS, 'Copied remote widget successfully', entity);
+									dropBlocked = false;
+								});
+							}
+						} else {
+							$.ajax({
+								url: viewRootUrl + sourceId + '?edit=1',
+								contentType: 'text/html',
+								statusCode: {
+									200: function(data) {
+										Command.createLocalWidget(sourceId, 'New Widget (' + sourceId + ')', data, function(entity) {
+											_Logger.log(_LogType.WIDGETS, 'Created widget successfully', entity);
+											dropBlocked = false;
+										});
+									}
+								}
 							});
 						}
-					} else {
-						$.ajax({
-							url: viewRootUrl + sourceId + '?edit=1',
-							contentType: 'text/html',
-							statusCode: {
-								200: function(data) {
-									Command.createLocalWidget(sourceId, 'New Widget (' + sourceId + ')', data, function(entity) {
-										_Logger.log(_LogType.WIDGETS, 'Created widget successfully', entity);
-										dropBlocked = false;
-									});
-								}
-							}
-						});
 					}
-				}
-			});
+				});
 
-			_Pager.initPager('local-widgets', 'Widget', 1, 25);
-			var _wPager = _Pager.addPager('local-widgets', _Widgets.localWidgetsEl, true, 'Widget', 'public', function(entities) {
-				entities.forEach(function (entity) {
-					StructrModel.create(entity, null, false);
-					_Widgets.appendWidgetElement(entity, false, _Widgets.localWidgetsEl);
+				_Pager.initPager('local-widgets', 'Widget', 1, 25);
+				var _wPager = _Pager.addPager('local-widgets', _Widgets.localWidgetsEl, true, 'Widget', 'public', function(entities) {
+					entities.forEach(function (entity) {
+						StructrModel.create(entity, null, false);
+						_Widgets.appendWidgetElement(entity, false, _Widgets.localWidgetsEl);
+					});
+				});
+
+				_wPager.pager.append('Filter: <input type="text" class="filter" data-attribute="name" />');
+				_wPager.activateFilterElements();
+
+				_Widgets.remoteWidgetsEl = $('#remoteWidgets', widgetsSlideout);
+
+				_Widgets.remoteWidgetFilterEl = $('#remoteWidgetsFilter');
+				_Widgets.remoteWidgetFilterEl.val(_Widgets.remoteWidgetFilter);
+				_Widgets.remoteWidgetFilterEl.keyup(function (e) {
+					if (e.keyCode === 27) {
+						$(this).val('');
+					}
+
+					_Widgets.repaintRemoteWidgets($(this).val());
+				});
+
+				document.querySelector('button#edit-widget-servers').addEventListener('click', _Widgets.showWidgetServersDialog);
+
+				_Widgets.updateWidgetServerSelector(function() {
+					_Widgets.refreshRemoteWidgets();
 				});
 			});
-
-			_wPager.pager.append('Filter: <input type="text" class="filter" data-attribute="name" />');
-			_wPager.activateFilterElements();
-
-			_Widgets.remoteWidgetsEl = $('#remoteWidgets', widgetsSlideout);
-
-			_Widgets.remoteWidgetFilterEl = $('#remoteWidgetsFilter');
-			_Widgets.remoteWidgetFilterEl.val(_Widgets.remoteWidgetFilter);
-			_Widgets.remoteWidgetFilterEl.keyup(function (e) {
-				if (e.keyCode === 27) {
-					$(this).val('');
-				}
-
-				_Widgets.repaintRemoteWidgets($(this).val());
-			});
-
-			_Widgets.remoteWidgetsUrlInputEl = document.querySelector('#remoteWidgetsUrl');
-			_Widgets.remoteWidgetsUrlResetButton = document.querySelector('button#resetRemoteWidgetsUrl');
-			_Widgets.remoteWidgetsUrlApplyButton = document.querySelector('button#applyRemoteWidgetsUrl');
-
-			_Widgets.remoteWidgetsUrlInputEl.addEventListener('keyup', function(e) {
-				if (e.which === 13) {
-					_Widgets.refreshRemoteWidgets();
-				}
-			});
-			_Widgets.remoteWidgetsUrlApplyButton.addEventListener('click', _Widgets.refreshRemoteWidgets);
-			_Widgets.remoteWidgetsUrlResetButton.addEventListener('click', _Widgets.resetWidgetServerUrl);
-
-			_Widgets.refreshRemoteWidgets();
 		});
+
 	},
 	getWidgetServerUrl: function() {
-		return _Widgets.remoteWidgetsUrlInputEl.value;
+		return _Widgets.widgetServerSelector.value;
 	},
-	resetWidgetServerUrl: function() {
-		LSWrapper.setItem(_Widgets.widgetServerKey, _Widgets.defaultWidgetServerUrl);
-		_Widgets.remoteWidgetsUrlInputEl.value = _Widgets.defaultWidgetServerUrl;
-		_Widgets.refreshRemoteWidgets();
+	getConfiguredWidgetServers: function (callback) {
+
+		Command.getApplicationConfigurationDataNodes(_Widgets.applicationConfigurationDataNodeKey, null, function(acdns) {
+
+			acdns.push({id: '', name: 'default', content: _Widgets.defaultWidgetServerUrl, editable: false});
+
+			callback(acdns);
+		});
+
+	},
+	showWidgetServersDialog: function() {
+
+		Structr.fetchHtmlTemplate('widgets/servers-dialog', {}, function(html) {
+
+			Structr.dialog('Widget Servers');
+			dialogText.html(html);
+
+			_Widgets.updateWidgetServersTable();
+
+			dialogText[0].querySelector('button#save-widget-server').addEventListener('click', function () {
+				let name = document.querySelector("#new-widget-server-name").value;
+				let url = document.querySelector("#new-widget-server-url").value;
+
+				Command.createApplicationConfigurationDataNode(_Widgets.applicationConfigurationDataNodeKey, name, url, function(e) {
+					_Widgets.updateWidgetServersTable();
+				});
+			});
+		});
+
+	},
+	updateWidgetServersTable: function() {
+
+		_Widgets.getConfiguredWidgetServers(function(serverConfigs) {
+
+			Structr.fetchHtmlTemplate('widgets/servers-table', {servers: serverConfigs}, function(html) {
+
+				let tableContainer = dialogText[0].querySelector('#widget-servers-table-container');
+
+				tableContainer.innerHTML = html;
+
+				tableContainer.querySelectorAll('button.delete').forEach(function(deleteButton) {
+					deleteButton.addEventListener('click', function(e) {
+						let el = e.target;
+						let tr = el.closest('tr');
+						let acdnID = tr.dataset.acdnId;
+
+						Structr.confirmation('Really delete Widget Server URL?', function() {
+							Command.deleteNode(acdnID, false, function() {
+								tr.remove();
+
+								_Widgets.updateWidgetServerSelector();
+
+								$.unblockUI({
+									fadeOut: 25
+								});
+
+								_Widgets.showWidgetServersDialog();
+							});
+						});
+					});
+				});
+
+				tableContainer.querySelectorAll('table input').forEach(function(input) {
+					input.addEventListener('change', function(e) {
+						let el = e.target;
+						let acdnID = el.closest('tr').dataset.acdnId;
+						let key = el.dataset.key;
+						console.log(acdnID, key, el.value);
+
+						Command.setProperty(acdnID, key, el.value, false, function(e) {
+
+							blinkGreen($(el));
+
+							_Widgets.updateWidgetServerSelector();
+						});
+					});
+				});
+			});
+		});
+	},
+	updateWidgetServerSelector: function(callback) {
+
+		_Widgets.getConfiguredWidgetServers(function(serverConfigs) {
+
+			let templateConfig = {
+				servers: serverConfigs,
+				selectedServerURL: LSWrapper.getItem(_Widgets.widgetServerKey, _Widgets.defaultWidgetServerUrl)
+			};
+
+			Structr.fetchHtmlTemplate('widgets/servers-selector', templateConfig, function(html) {
+
+				let selectorContainer = document.querySelector('#widget-server-selector-container');
+
+				selectorContainer.innerHTML = html;
+
+				_Widgets.widgetServerSelector = document.querySelector('#widget-server-selector');
+				_Widgets.widgetServerSelector.addEventListener('change', _Widgets.refreshRemoteWidgets);
+
+				if (typeof callback === 'function') {
+					callback();
+				}
+			});
+		});
 	},
 	refreshRemoteWidgets: function() {
 
@@ -151,23 +239,30 @@ var _Widgets = {
 
 		LSWrapper.setItem(_Widgets.widgetServerKey, url);
 
-		if (url === _Widgets.defaultWidgetServerUrl) {
-			_Widgets.remoteWidgetsUrlResetButton.classList.add('hidden');
-		} else {
-			_Widgets.remoteWidgetsUrlResetButton.classList.remove('hidden');
-		}
-
 		if (!url.startsWith(document.location.origin)) {
 
 			_Widgets.remoteWidgetsEl.empty();
 
-			_Widgets.getRemoteWidgets(url, function(entity) {
-				var obj = StructrModel.create(entity, null, false);
-				obj.srcUrl = url + '/' + entity.id;
-				_Widgets.remoteWidgetData.push(obj);
-			}, function () {
+			fetch(url + '?sort=treePath').then(function(response) {
+
+				return response.json().then((json) => {
+					return json.result;
+				});
+
+			}).then(function(data) {
+
+				data.forEach(function(entity) {
+					var obj = StructrModel.create(entity, null, false);
+					obj.srcUrl = url + '/' + entity.id;
+					_Widgets.remoteWidgetData.push(obj);
+				});
+
 				_Widgets.repaintRemoteWidgets(_Widgets.remoteWidgetFilter);
+
+			}).catch(function(e) {
+				new MessageBuilder().error().text('Could not fetch data from server. Make sure that the resource loads correctly and check CORS settings.').requiresConfirmation().show();
 			});
+
 		} else {
 			new MessageBuilder().warning().text('Can not display local widgets as remote widgets. Please select another widget server!').show();
 		}
@@ -192,49 +287,6 @@ var _Widgets = {
 				_Widgets.appendWidgetElement(obj, true, _Widgets.remoteWidgetsEl);
 			});
 		}
-	},
-	getRemoteWidgets: function(baseUrl, callback, finishCallback) {
-		$.ajax({
-			url: baseUrl + '?sort=treePath',
-			type: 'GET',
-			dataType: 'json',
-			contentType: 'application/json; charset=utf-8',
-			statusCode: {
-				200: function(data) {
-					if (callback) {
-						$.each(data.result, function(i, entity) {
-							callback(entity);
-						});
-						if (finishCallback) {
-							finishCallback();
-						}
-					}
-				},
-				400: function(data, status, xhr) {
-					console.log(data, status, xhr);
-				},
-				401: function(data, status, xhr) {
-					console.log(data, status, xhr);
-				},
-				403: function(data, status, xhr) {
-					console.log(data, status, xhr);
-				},
-				404: function(data, status, xhr) {
-					console.log(data, status, xhr);
-				},
-				422: function(data, status, xhr) {
-					console.log(data, status, xhr);
-				},
-				500: function(data, status, xhr) {
-					console.log(data, status, xhr);
-				}
-			}
-		}).fail(function(data, status, xhr) {
-			console.log(data, status, xhr);
-			if (data.status === 0) {
-				new MessageBuilder().error().text('Could not fetch data from server. Make sure that the resource loads correctly and check CORS settings.').requiresConfirmation().show();
-			}
-		});
 	},
 	getTreeParent: function(element, treePath, suffix) {
 
