4462d65fa45eb32613ccb29505e1c6a533e163bb
Lukas Reich <lukas.reich@structr.com>
2019-03-27 10:47:44 +0100

    Adds new settings that allows to log the jetty ssl configuration on startup.

--- structr-db-driver-api/src/main/java/org/structr/api/config/Settings.java
+++ structr-db-driver-api/src/main/java/org/structr/api/config/Settings.java
@@ -104,10 +104,11 @@ public class Settings {
 	public static final Setting<Boolean> WsIndentation        = new BooleanSetting(serverGroup, "HTTP Settings", "ws.indentation",                  true);
 	public static final Setting<Integer> SessionTimeout       = new IntegerSetting(serverGroup, "HTTP Settings", "application.session.timeout",     1800, "The session inactivity timeout. Unit is seconds");
 
-	public static final Setting<Boolean> ForceHttps           = new BooleanSetting(serverGroup, "HTTPS Settings", "HttpService.force.https",         		false);
-	public static final Setting<String> excludedProtocols     = new StringSetting(serverGroup, "HTTPS Settings", "HttpService.ssl.protocols.excluded",  	"TLSv1,TLSv1.1");
-	public static final Setting<String> includedProtocols 	  = new StringSetting(serverGroup, "HTTPS Settings", "HttpService.ssl.protocols.included",		"TLSv1.2");
-	public static final Setting<String> disabledCipherSuits   = new StringSetting(serverGroup,  "HTTPS Settings", "HttpService.ssl.ciphers.excluded",    	"");
+	public static final Setting<Boolean> ForceHttps           	= new BooleanSetting(serverGroup, "HTTPS Settings", "HttpService.force.https",         		false);
+	public static final Setting<Boolean> dumbJettyStartupConfig = new BooleanSetting(serverGroup, "HTTPS Settings", "HttpService.log.jetty.startupConfig",	false);
+	public static final Setting<String> excludedProtocols     	= new StringSetting(serverGroup, "HTTPS Settings", "HttpService.ssl.protocols.excluded",  	"TLSv1,TLSv1.1");
+	public static final Setting<String> includedProtocols 	  	= new StringSetting(serverGroup, "HTTPS Settings", "HttpService.ssl.protocols.included",		"TLSv1.2");
+	public static final Setting<String> disabledCipherSuits   	= new StringSetting(serverGroup,  "HTTPS Settings", "HttpService.ssl.ciphers.excluded",    	"");
 
 	public static final Setting<String> AccessControlMaxAge           = new StringSetting(serverGroup, "CORS Settings", "access.control.max.age",           "3600");
 	public static final Setting<String> AccessControlAllowMethods     = new StringSetting(serverGroup, "CORS Settings", "access.control.allow.methods",     "");
