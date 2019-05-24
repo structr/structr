32dd4a422deaf711335ff847574da8ccf8f8d552
Christian Kramp <christian.kramp@structr.com>
2019-05-24 12:56:36 +0200

    Reverts changes made to dashboard.js

--- structr-ui/src/main/resources/structr/js/dashboard.js
+++ structr-ui/src/main/resources/structr/js/dashboard.js
@@ -41,9 +41,7 @@ var _Dashboard = {
 
 		let templateConfig = {};
 
-		fetch(rootUrl + '_env', {
-			credentials: 'same-origin'
-		}).then(function(response) {
+		fetch(rootUrl + '/_env').then(function(response) {
 
 			return response.json();
 
@@ -59,9 +57,7 @@ var _Dashboard = {
 				templateConfig.envInfo.endDate = _Dashboard.dateToIsoString(templateConfig.envInfo.endDate);
 			}
 
-			return fetch(rootUrl + 'me/ui', {
-				credentials: 'same-origin'
-			});
+			return fetch(rootUrl + '/me/ui');
 
 		}).then(function(response) {
 
