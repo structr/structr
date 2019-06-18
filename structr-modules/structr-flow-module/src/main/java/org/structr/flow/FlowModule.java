f6fe1d395353d75f4b3398a9d3bb7501217a5a0d
Christian Morgner <christian@morgner.de>
2019-06-18 10:14:03 +0200

    Adds new flows module, removes flows from api-builder module.

---
+++ structr-modules/structr-flow-module/src/main/java/org/structr/flow/FlowModule.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (C) 2010-2019 Structr GmbH
+ *
+ * This file is part of Structr <http://structr.org>.
+ *
+ * Structr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Structr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.structr.flow;
+
+import com.google.gson.Gson;
+import java.nio.file.Path;
+import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.structr.api.service.LicenseManager;
+import org.structr.common.error.FrameworkException;
+import org.structr.core.datasources.DataSources;
+import org.structr.core.entity.AbstractSchemaNode;
+import org.structr.core.function.Functions;
+import org.structr.flow.datasource.FlowContainerDataSource;
+import org.structr.flow.impl.FlowFunction;
+import org.structr.module.StructrModule;
+import org.structr.schema.SourceFile;
+import org.structr.schema.action.Actions;
+
+/**
+ *
+ */
+public class FlowModule implements StructrModule {
+
+	private static final Logger logger = LoggerFactory.getLogger(FlowModule.class.getName());
+
+	@Override
+	public void onLoad(final LicenseManager licenseManager) {
+
+		final boolean licensed = licenseManager == null || licenseManager.isModuleLicensed(getName());
+
+		DataSources.put(licensed, getName(), "flowDataSource", new FlowContainerDataSource());
+	}
+
+	@Override
+	public void registerModuleFunctions(final LicenseManager licenseManager) {
+
+		Functions.put(licenseManager, new FlowFunction());
+	}
+
+	@Override
+	public String getName() {
+		return "flows";
+	}
+
+	@Override
+	public Set<String> getDependencies() {
+		return null;
+	}
+
+	@Override
+	public Set<String> getFeatures() {
+		return null;
+	}
+
+	@Override
+	public void insertImportStatements(final AbstractSchemaNode schemaNode, final SourceFile buf) {
+	}
+
+	@Override
+	public void insertSourceCode(final AbstractSchemaNode schemaNode, final SourceFile buf) {
+	}
+
+	@Override
+	public Set<String> getInterfacesForType(final AbstractSchemaNode schemaNode) {
+		return null;
+	}
+
+	@Override
+	public void insertSaveAction(final AbstractSchemaNode schemaNode, final SourceFile buf, final Actions.Type type) {
+	}
+
+	@Override
+	public boolean hasDeploymentData () {
+		return true;
+	}
+
+	@Override
+	public void exportDeploymentData (final Path target, final Gson gson) throws FrameworkException {
+		FlowDeploymentHandler.exportDeploymentData(target, gson);
+	}
+
+	@Override
+	public void importDeploymentData (final Path source, final Gson gson) throws FrameworkException {
+		FlowDeploymentHandler.importDeploymentData(source, gson);
+	}
+}
