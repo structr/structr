8c36dd624ef1c4a66cc19576d1318881c225b4da
Christian Morgner <christian@morgner.de>
2019-07-02 15:27:44 +0200

    Adds schema test from master manually to avoid complex merge.

--- structr-core/src/test/java/org/structr/test/schema/SchemaTest.java
+++ structr-core/src/test/java/org/structr/test/schema/SchemaTest.java
@@ -983,6 +983,40 @@ public class SchemaTest extends StructrTest {
 		}
 	}
 
+	@Test
+	public void testRelatedTypeOnNotionProperty() {
+
+		cleanDatabaseAndSchema();
+
+		try (final Tx tx = app.tx()) {
+
+			final JsonSchema schema = StructrSchema.createFromDatabase(app);
+
+			final JsonObjectType project    = schema.addType("Project");
+			final JsonObjectType task       = schema.addType("Task");
+			final JsonReferenceType rel     = project.relate(task, "TASK", Cardinality.OneToMany, "project", "tasks");
+			final JsonReferenceProperty ref = rel.getSourceProperty();
+
+			project.addStringProperty("blah").setUnique(true);
+
+			task.addReferenceProperty("projectBlah", ref).setProperties("blah", "true");
+
+			Settings.LogSchemaOutput.setValue(true);
+
+			StructrSchema.extendDatabaseSchema(app, schema);
+
+			tx.success();
+
+		} catch (Throwable t) {
+
+			t.printStackTrace();
+			fail("NotionProperty setup failed.");
+		}
+
+		Settings.LogSchemaOutput.setValue(true);
+	}
+
+
 	// ----- private methods -----
 	private void checkSchemaString(final String source) {
 
