e2fe8dde8bedcf0f5233c9a7304a78cefe0bb952
Christian Morgner <christian@morgner.de>
2019-10-21 10:19:46 +0200

    Fixes failing tests.

--- structr-ui/src/test/java/org/structr/test/console/ConsoleTest.java
+++ structr-ui/src/test/java/org/structr/test/console/ConsoleTest.java
@@ -147,8 +147,8 @@ public class ConsoleTest extends StructrUiTest {
 	public void testRebuildCommand() {
 
 		final Console console = new Console(securityContext, ConsoleMode.JavaScript, Collections.emptyMap());
-		final int nodeCount   = 2134;
-		final int relCount    = 1948;
+		final int nodeCount   = 2146;
+		final int relCount    = 1966;
 
 		final String fullIndexRebuildOutput =
 			"Node type not set or no entity class found. Starting (re-)indexing all nodes\r\n" +
