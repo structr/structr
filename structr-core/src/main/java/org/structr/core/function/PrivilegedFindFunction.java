939211948026a12e496d9d439b39c68a6e778bde
Christian Morgner <christian@morgner.de>
2019-11-11 15:31:21 +0100

    Modifies find_privileged() to support advanced find syntax as well.

--- structr-core/src/main/java/org/structr/core/function/PrivilegedFindFunction.java
+++ structr-core/src/main/java/org/structr/core/function/PrivilegedFindFunction.java
@@ -18,22 +18,19 @@
  */
 package org.structr.core.function;
 
-import java.util.List;
-import java.util.Map;
 import org.structr.common.SecurityContext;
 import org.structr.common.error.FrameworkException;
-import org.structr.core.GraphObject;
+import org.structr.core.app.App;
 import org.structr.core.app.Query;
 import org.structr.core.app.StructrApp;
-import org.structr.core.converter.PropertyConverter;
-import org.structr.core.property.PropertyKey;
-import org.structr.core.property.PropertyMap;
 import org.structr.schema.ConfigurationProvider;
 import org.structr.schema.action.ActionContext;
 
-public class PrivilegedFindFunction extends AdvancedScriptingFunction {
+public class PrivilegedFindFunction extends AbstractQueryFunction {
 
-    public static final String ERROR_MESSAGE_PRIVILEGEDFIND = "Usage: ${find_privileged(type, key, value)}. Example: ${find_privileged(\"User\", \"email\", \"tester@test.com\"}";
+    public static final String ERROR_MESSAGE_PRIVILEGEDFIND                       = "Usage: ${find_privileged(type, key, value)}. Example: ${find_privileged(\"User\", \"email\", \"tester@test.com\"}";
+	public static final String ERROR_MESSAGE_PRIVILEGEDFIND_NO_TYPE_SPECIFIED = "Error in find_privileged(): no type specified.";
+	public static final String ERROR_MESSAGE_PRIVILEGEDFIND_TYPE_NOT_FOUND    = "Error in find_privileged(): type not found: ";
 
     @Override
     public String getName() {
@@ -43,11 +40,19 @@ public class PrivilegedFindFunction extends AdvancedScriptingFunction {
     @Override
     public Object apply(final ActionContext ctx, final Object caller, Object[] sources) throws FrameworkException {
 
-		if (sources != null) {
+		final SecurityContext securityContext = SecurityContext.getSuperUserInstance();
+		final boolean ignoreResultCount       = securityContext.ignoreResultCount();
 
-			final SecurityContext securityContext = SecurityContext.getSuperUserInstance();
+		try {
+
+			if (sources == null) {
+
+				throw new IllegalArgumentException();
+			}
+
 			final ConfigurationProvider config    = StructrApp.getConfiguration();
-			final Query query                     = StructrApp.getInstance(securityContext).nodeQuery().sort(GraphObject.createdDate).order(false);
+			final App app                         = StructrApp.getInstance(securityContext);
+			final Query query                     = app.nodeQuery();
 
 			// the type to query for
 			Class type = null;
@@ -64,7 +69,7 @@ public class PrivilegedFindFunction extends AdvancedScriptingFunction {
 				} else {
 
 					logger.warn("Error in find_privileged(): type \"{}\" not found.", typeString);
-					return "Error in find_privileged(): type " + typeString + " not found.";
+					return ERROR_MESSAGE_PRIVILEGEDFIND_TYPE_NOT_FOUND + typeString;
 
 				}
 			}
@@ -72,74 +77,25 @@ public class PrivilegedFindFunction extends AdvancedScriptingFunction {
 			// exit gracefully instead of crashing..
 			if (type == null) {
 				logger.warn("Error in find_privileged(): no type specified. Parameters: {}", getParametersAsString(sources));
-				return "Error in find_privileged(): no type specified.";
+				return ERROR_MESSAGE_PRIVILEGEDFIND_NO_TYPE_SPECIFIED;
 			}
 
-			// experimental: disable result count, prevents instantiation
-			// of large collections just for counting all the objects..
-			securityContext.ignoreResultCount(true);
+			// apply sorting and pagination by surrounding sort() and slice() expressions
+			applyQueryParameters(securityContext, query);
 
-			// extension for native javascript objects
-			if (sources.length == 2 && sources[1] instanceof Map) {
+			return handleQuerySources(securityContext, type, query, sources, true);
 
-				query.and(PropertyMap.inputTypeToJavaType(securityContext, type, (Map)sources[1]));
+		} catch (final IllegalArgumentException e) {
 
-			} else if (sources.length == 2) {
+			logParameterError(caller, sources, ctx.isJavaScriptContext());
 
-				if (sources[1] == null) {
+			return usage(ctx.isJavaScriptContext());
 
-					throw new IllegalArgumentException();
-				}
+		} finally {
 
-				// special case: second parameter is a UUID
-				final PropertyKey key = StructrApp.key(type, "id");
-
-				query.and(key, sources[1].toString());
-
-				final List<GraphObject> result = query.getAsList();
-				final int resultCount = result.size();
-
-				switch (resultCount) {
-					case 1:
-						return result.get(0);
-					case 0:
-						return null;
-					default:
-						throw new FrameworkException(400, "Multiple Objects found for id! [" + sources[1].toString() + "]");
-				}
-
-			} else {
-
-				final int parameter_count = sources.length;
-
-				if (parameter_count % 2 == 0) {
-
-					throw new FrameworkException(400, "Invalid number of parameters: " + parameter_count + ". Should be uneven: " + ERROR_MESSAGE_PRIVILEGEDFIND);
-				}
-
-				for (int c = 1; c < parameter_count; c += 2) {
-
-					final PropertyKey key = StructrApp.key(type, sources[c].toString());
-
-					if (key != null) {
-
-						final PropertyConverter inputConverter = key.inputConverter(securityContext);
-						Object value = sources[c + 1];
-
-						if (inputConverter != null) {
-
-							value = inputConverter.convert(value);
-						}
-
-						query.and(key, value);
-					}
-				}
-			}
-
-			return query.getAsList();
+			resetQueryParameters(securityContext);
+			securityContext.ignoreResultCount(ignoreResultCount);
 		}
-
-		return "";
     }
 
     @Override
