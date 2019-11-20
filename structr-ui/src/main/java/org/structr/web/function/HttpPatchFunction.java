2526d58d22b3ced381d42165e8891fd70f565d4a
Lukas Reich <lukas.reich@structr.com>
2019-11-20 16:14:33 +0100

    Bugfix: changes http call from PUT to PATCH in new PATCH-Function. Bugfix: adds handler for empty response in PATCH function.

--- structr-ui/src/main/java/org/structr/web/function/HttpPatchFunction.java
+++ structr-ui/src/main/java/org/structr/web/function/HttpPatchFunction.java
@@ -58,7 +58,7 @@ public class HttpPatchFunction extends UiAdvancedFunction {
 				charset = sources[3].toString();
 			}
 
-			final Map<String, String> responseData = HttpHelper.put(uri, body, null, null, ctx.getHeaders(), charset);
+			final Map<String, String> responseData = HttpHelper.patch(uri, body, null, null, ctx.getHeaders(), charset);
 
 			final int statusCode = Integer.parseInt(responseData.get("status"));
 			responseData.remove("status");
