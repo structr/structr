c441ca0849d9d086aac38a3d9bbbac61af0890f2
vigorouscoding <kai.schwaiger@structr.com>
2020-04-01 11:24:15 +0200

    Bugfix: Make console aware if it is in a JavaScript context

--- structr-ui/src/main/java/org/structr/console/Console.java
+++ structr-ui/src/main/java/org/structr/console/Console.java
@@ -264,6 +264,8 @@ public class Console {
 
 	private void runStructrScript(final String line, final Writable writable) throws FrameworkException, IOException {
 
+		actionContext.setJavaScriptContext(false);
+
 		try (final Tx tx = StructrApp.getInstance(actionContext.getSecurityContext()).tx()) {
 
 			final Object result = Functions.evaluate(actionContext, null, line);
@@ -293,6 +295,8 @@ public class Console {
 		final Context scriptingContext = Context.enter();
 
 		init(scriptingContext);
+
+		actionContext.setJavaScriptContext(true);
 
 		try (final Tx tx = StructrApp.getInstance(actionContext.getSecurityContext()).tx()) {
 
