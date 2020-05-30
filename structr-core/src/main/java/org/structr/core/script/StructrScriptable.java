9da28b211fe97eb6a8cf1ec246743f0779c2184e
vigorouscoding <kai.schwaiger@structr.com>
2020-05-30 17:24:48 +0200

    Bugfix: Only recursively convert ConsString in NativeObjects and create custom logging code for NativeObject to prevent showing as "object Object"

--- structr-core/src/main/java/org/structr/core/script/StructrScriptable.java
+++ structr-core/src/main/java/org/structr/core/script/StructrScriptable.java
@@ -23,6 +23,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -462,10 +463,14 @@ public class StructrScriptable extends ScriptableObject {
 
 				// NativeObject can contain ConsString and other things that need unwrapping
 				for (Map.Entry entry : ((NativeObject)source).entrySet()) {
+
+					final Object value     = entry.getValue();
+					final Object overwrite = (value instanceof NativeObject) ? unwrap(value) : (value instanceof ConsString) ? value.toString() : value;
+
 					if (entry.getKey() instanceof Integer) {
-						NativeObject.putProperty((NativeObject)source, (Integer)entry.getKey(), unwrap(entry.getValue()));
+						NativeObject.putProperty((NativeObject)source, (Integer)entry.getKey(), overwrite);
 					} else {
-						NativeObject.putProperty((NativeObject)source, entry.getKey().toString(), unwrap(entry.getValue()));
+						NativeObject.putProperty((NativeObject)source, entry.getKey().toString(), overwrite);
 					}
 				}
 
@@ -475,7 +480,6 @@ public class StructrScriptable extends ScriptableObject {
 
 				return ScriptUtils.unwrap(source);
 			}
-
 		}
 
 		return source;
@@ -503,6 +507,36 @@ public class StructrScriptable extends ScriptableObject {
 		}
 
 		return Functions.get(word);
+	}
+
+	public static String formatForLogging(final Object value) {
+
+		if (value instanceof NativeObject) {
+
+			final StringBuffer buf = new StringBuffer("{");
+
+			// NativeObject can contain ConsString and other things that need unwrapping
+			for (final Iterator<Map.Entry<Object, Object>> it = ((NativeObject)value).entrySet().iterator(); it.hasNext();) {
+
+				final Map.Entry entry = it.next();
+
+				buf.append(Scripting.formatForLogging(entry.getKey()));
+				buf.append(": ");
+				buf.append(Scripting.formatForLogging(entry.getValue()));
+
+				if (it.hasNext()) {
+					buf.append(", ");
+				}
+			}
+
+			buf.append("}");
+
+			return buf.toString();
+
+		} else {
+
+			return value.toString();
+		}
 	}
 
 	// ----- nested classes -----
