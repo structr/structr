1a63dd59e0a03c59377993434379722f5d105719
Axel Morgner <axel@morgner.de>
2020-06-08 21:23:21 +0200

    Enhancement: Adds support for setting OAuth scope via configuration settings. Also adds support for LinkedIn API v2.

--- structr-ui/src/main/java/org/structr/web/auth/GitHubAuthClient.java
+++ structr-ui/src/main/java/org/structr/web/auth/GitHubAuthClient.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2010-2020 Structr GmbH
  *
  * This file is part of Structr <http://structr.org>.
@@ -41,7 +41,7 @@ public class GitHubAuthClient extends StructrOAuthClient {
 
 	@Override
 	public String getScope() {
-		return "user:email";
+		return Settings.OAuthGithubScope.getValue();
 	}
 
 	@Override
