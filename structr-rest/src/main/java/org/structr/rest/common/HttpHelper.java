a6ef7f400d3a6fcb32243092c0bfb045417fab94
vigorouscoding <kai.schwaiger@structr.com>
2020-06-10 16:18:28 +0200

    Feature: Adds connection timeout settings to prevent getting stuck in socketRead0 when making outgoing connections

--- structr-rest/src/main/java/org/structr/rest/common/HttpHelper.java
+++ structr-rest/src/main/java/org/structr/rest/common/HttpHelper.java
@@ -107,7 +107,7 @@ public class HttpHelper {
 
 		if (StringUtils.isNotBlank(proxyUrl)) {
 
-			proxy         = HttpHost.create(proxyUrl);
+			proxy = HttpHost.create(proxyUrl);
 
 			if (StringUtils.isNoneBlank(proxyUsername, proxyPassword)) {
 
@@ -116,7 +116,6 @@ public class HttpHelper {
 					new UsernamePasswordCredentials(proxyUsername, proxyPassword)
 				);
 			}
-
 		}
 
 		client = HttpClients.custom()
@@ -129,6 +128,9 @@ public class HttpHelper {
 			.setProxy(proxy)
 			.setRedirectsEnabled(followRedirects)
 			.setCookieSpec(CookieSpecs.DEFAULT)
+			.setConnectTimeout(Settings.HttpConnectTimeout.getValue() * 1000)
+			.setSocketTimeout(Settings.HttpSocketTimeout.getValue() * 1000)
+			.setConnectionRequestTimeout(Settings.HttpConnectionRequestTimeout.getValue() * 1000)
 			.build();
 
 		req.setConfig(reqConfig);
@@ -176,7 +178,7 @@ public class HttpHelper {
 	throws FrameworkException {
 		return getBinary(address, username, password, null, null, null, null, headers);
 	}
-	
+
 	public static String get(final String address, final String proxyUrl, final String proxyUsername, final String proxyPassword, final String cookie, final Map<String, String> headers)
 	throws FrameworkException {
 		return get(address, null, null, proxyUrl, proxyUsername, proxyPassword, cookie, headers);
@@ -289,7 +291,7 @@ public class HttpHelper {
 				final InputStream responseContent = entity.getContent();
 
 				if (responseContent != null) {
-					
+
 					content = IOUtils.toString(responseContent, charset(response));
 				}
 			}
