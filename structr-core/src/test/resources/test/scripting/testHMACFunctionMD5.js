45e1c4c242a88aeaf53976a3f9ea4fad5ab5ec4c
Christian Kramp <christian.kramp@structr.com>
2020-07-11 13:50:18 +0200

    Fixes format of test js files to match polyglot engine.

--- structr-core/src/test/resources/test/scripting/testHMACFunctionMD5.js
+++ structr-core/src/test/resources/test/scripting/testHMACFunctionMD5.js
@@ -1,9 +1,5 @@
-function test() {
-    let value = 'test';
-    let secret = 'test';
-    let algorithm = 'MD5';
+let value = 'test';
+let secret = 'test';
+let algorithm = 'MD5';
 
-    return $.hmac(value, secret, algorithm);
-}
-
-let _structrMainResult = test();
+return $.hmac(value, secret, algorithm);
