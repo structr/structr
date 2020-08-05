afacadc86f268a1cee9efb173774e6eda7fa57ec
Christian Morgner <christian@morgner.de>
2020-08-05 11:14:37 +0200

    Bugfix: fixes empty() predicate in new query API, adds test.

--- structr-neo4j-bolt-driver/src/main/java/org/structr/bolt/factory/EmptyQueryFactory.java
+++ structr-neo4j-bolt-driver/src/main/java/org/structr/bolt/factory/EmptyQueryFactory.java
@@ -41,8 +41,13 @@ public class EmptyQueryFactory extends AbstractQueryFactory<AdvancedCypherQuery>
 			query.indexLabel(label);
 		}
 
-		// not empty query is simple
-		query.addSimpleParameter(predicate.getName(), "is", null);
+		final String name = predicate.getName();
+
+		query.beginGroup();
+		query.addSimpleParameter(name, "is", null);
+		query.or();
+		query.addSimpleParameter(name, "=", "");
+		query.endGroup();
 
 		return true;
 	}
