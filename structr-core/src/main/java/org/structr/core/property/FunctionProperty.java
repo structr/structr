6cfe902d1b7e7ed0cfe08028495c4e77e7be8c1b
Christian Kramp <christian.kramp@structr.com>
2020-08-28 17:11:14 +0200

    Fixes issue in FunctionProperty read value conversion. Changes NaN numeric values in scripting results to be treated as null.

--- structr-core/src/main/java/org/structr/core/property/FunctionProperty.java
+++ structr-core/src/main/java/org/structr/core/property/FunctionProperty.java
@@ -18,6 +18,7 @@
  */
 package org.structr.core.property;
 
+import java.awt.*;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Map;
@@ -119,16 +120,36 @@ public class FunctionProperty<T> extends Property<T> {
 
 						switch (typeHint.toLowerCase()) {
 
-							case "boolean": converter = pBoolean.inputConverter(securityContext); break;
-							case "int":     converter = pInt.inputConverter(securityContext); break;
-							case "long":    converter = pLong.inputConverter(securityContext); break;
-							case "double":  converter = pDouble.inputConverter(securityContext); break;
-							case "date":    converter = pDate.inputConverter(securityContext); break;
+							case "boolean":
+								converter = pBoolean.inputConverter(securityContext);
+								break;
+							case "int":
+								converter = pInt.inputConverter(securityContext);
+								break;
+							case "long":
+								converter = pLong.inputConverter(securityContext);
+								break;
+							case "double":
+								converter = pDouble.inputConverter(securityContext);
+								break;
+							case "date":
+								converter = pDate.inputConverter(securityContext);
+								break;
 						}
 
 						if (converter != null) {
+							{
+								try {
 
-							result = converter.convert(result);
+									Object convertedResult = converter.convert(result);
+									if (convertedResult != null) {
+										result = convertedResult;
+									}
+								} catch (FrameworkException ex) {
+
+									logger.warn("Could not convert value of function property. Conversion type: :%s, Raw value: " + result.toString(), ex);
+								}
+							}
 						}
 					}
 
