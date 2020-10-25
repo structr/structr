ed15359ed55f917bd8fc574e49e23af04b5b3a54
Axel Morgner <axel@morgner.de>
2020-10-25 22:09:41 +0100

    Enhancement: Adds tnSmall and tnMid attributes so that thumbnail images are shown again. This is possible because thumbnail creation is now asynchronous. Also removes a debugging 'console.log' statement.

--- structr-ui/src/main/resources/structr/js/files.js
+++ structr-ui/src/main/resources/structr/js/files.js
@@ -543,7 +543,7 @@ var _Files = {
 			let pagerId = 'filesystem-files';
 			_Pager.initFilters(pagerId, 'File', filterOptions, ['parentId', 'hasParent', 'isThumbnail']);
 
-			let filesPager = _Pager.addPager(pagerId, folderContents, false, 'File', 'public', handleChildren, null, 'id,name,type,contentType,isFile,isImage,isThumbnail,path,size,owner,visibleToPublicUsers,visibleToAuthenticatedUsers');
+			let filesPager = _Pager.addPager(pagerId, folderContents, false, 'File', 'public', handleChildren, null, 'id,name,type,contentType,isFile,isImage,isThumbnail,tnSmall,tnMid,path,size,owner,visibleToPublicUsers,visibleToAuthenticatedUsers');
 
 			filesPager.cleanupFunction = function () {
 				var toRemove = $('.node.file', filesPager.el).closest( (_Files.isViewModeActive('list') ? 'tr' : '.tile') );
@@ -1037,8 +1037,6 @@ var _Files = {
 
 			fileContents = {};
 			editor = undefined;
-
-			console.log(parent)
 
 			selectedElements = $('.node.selected');
 			if (selectedElements.length > 1 && parent.hasClass('selected')) {
