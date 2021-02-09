05ab895afbd4b6735e076d982b330dcaf1a22687
vigorouscoding <kai.schwaiger@structr.com>
2021-02-09 11:42:33 +0100

    Improvement: Adds several HTML attributes to most used attributes

--- structr-ui/src/main/resources/structr/js/elements.js
+++ structr-ui/src/main/resources/structr/js/elements.js
@@ -89,12 +89,12 @@ var _Elements = {
 		},
 		{
 			elements: ['input', 'textarea'],
-			attrs: ['name', 'type', 'checked', 'selected', 'value', 'size', 'multiple', 'disabled', 'autofocus', 'placeholder', 'style', 'rows', 'cols'],
+			attrs: ['name', 'type', 'checked', 'selected', 'value', 'size', 'multiple', 'disabled', 'autofocus', 'placeholder', 'style', 'rows', 'cols', 'required'],
 			focus: 'type'
 		},
 		{
 			elements: ['button'],
-			attrs: ['name', 'type', 'checked', 'selected', 'value', 'size', 'multiple', 'disabled', 'autofocus', 'placeholder', 'onclick', 'style', 'title']
+			attrs: ['name', 'type', 'checked', 'selected', 'value', 'size', 'multiple', 'disabled', 'autofocus', 'placeholder', 'onclick', 'style', 'title', 'form', 'formaction', 'formmethod']
 		},
 		{
 			elements: ['select', 'option'],
@@ -115,7 +115,7 @@ var _Elements = {
 			focus: 'src'
 		},
 		{
-			elements: ['script', 'img', 'object'],
+			elements: ['script', 'object'],
 			attrs: ['type', 'rel', 'href', 'media', 'src', 'style'],
 			focus: 'src'
 		},
