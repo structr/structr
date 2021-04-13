1f4ac230cdc05801c5517722c39dbc99ad77139d
Christian Kramp <christian.kramp@structr.com>
2021-04-13 12:47:51 +0200

    Adds methodParameters keyword to access all available method parameters in JS scripting contexts.

--- structr-core/src/main/java/org/structr/common/ContextStore.java
+++ structr-core/src/main/java/org/structr/common/ContextStore.java
@@ -36,7 +36,7 @@ public class ContextStore {
 
 	protected Map<String, String> headers               = new HashMap<>();
 	protected Map<String, Object> constants             = new HashMap<>();
-	protected Map<String, Object> requestStore = new HashMap<>();
+	protected Map<String, Object> requestStore          = new HashMap<>();
 	protected Map<String, Object> tmpParameters         = new HashMap<>();
 	protected Map<String, Date> timerStore              = new HashMap<>();
 	protected Map<Integer, Integer> counters            = new HashMap<>();
@@ -98,6 +98,10 @@ public class ContextStore {
 
 	public Set<String> getTemporaryParameterKeys() {
 		return this.tmpParameters.keySet();
+	}
+
+	public Map<String, Object> getTemporaryParameters() {
+		return this.tmpParameters;
 	}
 
 	public void clearTemporaryParameters() {
