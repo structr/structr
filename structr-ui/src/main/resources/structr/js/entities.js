ca2f2a10c17d73cce5f7df430097fe2be1e37a69
vigorouscoding <kai.schwaiger@structr.com>
2021-06-29 17:30:39 +0200

    UI: Fixes padding for tabs in schema

--- structr-ui/src/main/resources/structr/js/entities.js
+++ structr-ui/src/main/resources/structr/js/entities.js
@@ -981,7 +981,7 @@ var _Entities = {
 	appendPropTab: function(entity, tabsEl, contentEl, name, label, active, callback, initCallback, showCallback) {
 
 		var ul = tabsEl.children('ul');
-		ul.append('<li id="tab-' + name + '"><div class="fill-pixel"></div>' + label + '</li>');
+		ul.append('<li id="tab-' + name + '"><div class="fill-pixel"></div><span>' + label + '</span></li>');
 
 		var tab = $('#tab-' + name + '');
 		if (active) {
