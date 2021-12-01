e2bd15db0f003e62625212c6a46c10ee0a5bde65
vigorouscoding <kai.schwaiger@structr.com>
2021-12-01 16:54:47 +0100

    Minor: Fixes error messages

--- structr-core/src/main/java/org/structr/core/function/GrantFunction.java
+++ structr-core/src/main/java/org/structr/core/function/GrantFunction.java
@@ -58,7 +58,7 @@ public class GrantFunction extends AdvancedScriptingFunction {
 
 			} else if (sources[0] instanceof SuperUser) {
 
-				logParameterError(caller, sources, "Expected node of type Principal as first argument - unable to revoke rights for the SuperUser!", ctx.isJavaScriptContext());
+				logParameterError(caller, sources, "Expected node of type Principal as first argument - unable to grant rights for the SuperUser!", ctx.isJavaScriptContext());
 
 			} else if (!(sources[1] instanceof AbstractNode)) {
 
