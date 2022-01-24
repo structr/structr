18f7293aee302531d102bc26d6c9c9a4922faeb0
Axel Morgner <axel@morgner.de>
2022-01-24 19:57:50 +0100

    Enhancement: Adds new function PDFEncryptFunction to password-encrypt PDF files.

---
+++ structr-modules/structr-pdf-module/src/main/java/org/structr/pdf/function/PDFEncryptFunction.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2010-2021 Structr GmbH
+ *
+ * This file is part of Structr <http://structr.org>.
+ *
+ * Structr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Structr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.structr.pdf.function;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
+import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
+import org.apache.pdfbox.pdmodel.interactive.digitalsignature.PDSignature;
+import org.structr.api.config.Settings;
+import org.structr.common.error.ArgumentCountException;
+import org.structr.common.error.ArgumentNullException;
+import org.structr.common.error.FrameworkException;
+import org.structr.core.GraphObject;
+import org.structr.core.app.StructrApp;
+import org.structr.core.function.AdvancedScriptingFunction;
+import org.structr.schema.action.ActionContext;
+import org.structr.web.entity.File;
+
+import java.io.*;
+
+public class PDFEncryptFunction extends AdvancedScriptingFunction {
+
+	public static final String ERROR_MESSAGE_PDF_ENCRYPT = "Usage: ${pdf_encrypt(file, password)}. Example: ${pdf_encrypt(first(find('File', 'name', 'document.pdf')), 'mypassword')}";
+
+	private static final int keyLength = 256;
+
+	@Override
+	public String getName() {
+		return "pdf_encrypt";
+	}
+
+	@Override
+	public String getSignature() {
+		return "file, password";
+	}
+
+	@Override
+	public Object apply(final ActionContext ctx, final Object caller, final Object[] sources) throws FrameworkException {
+
+		try {
+
+			assertArrayHasMinLengthAndAllElementsNotNull(sources, 2);
+
+			try {
+
+				if (!(sources[0] instanceof File)) {
+
+					logParameterError(caller, sources, "First parameter is not a file object.", ctx.isJavaScriptContext());
+					return usage(ctx.isJavaScriptContext());
+				}
+
+				if (!(sources[1] instanceof String)) {
+
+					logParameterError(caller, sources, "Second parameter is not a string.", ctx.isJavaScriptContext());
+					return usage(ctx.isJavaScriptContext());
+				}
+
+				final File pdfFileObject  = (File) sources[0];
+				final String userPassword = (String) sources[1];
+
+				final java.io.File fileOnDisk = pdfFileObject.getFileOnDisk();
+				final PDDocument pdDocument = Loader.loadPDF(fileOnDisk);
+
+				final AccessPermission accessPermission = new AccessPermission();
+				accessPermission.setCanPrint(false);
+
+				// Owner password (to open the file with all permissions) is the superuser password
+				final StandardProtectionPolicy standardProtectionPolicy = new StandardProtectionPolicy(Settings.SuperUserPassword.getValue(), userPassword, accessPermission);
+				standardProtectionPolicy.setEncryptionKeyLength(keyLength);
+				standardProtectionPolicy.setPermissions(accessPermission);
+				pdDocument.protect(standardProtectionPolicy);
+
+				if (fileOnDisk.delete()) {
+					pdDocument.save(fileOnDisk);
+				}
+
+				pdDocument.close();
+
+			} catch (final IOException ioex) {
+
+				logException(caller, ioex, sources);
+			}
+
+		} catch (final ArgumentNullException pe) {
+
+			logParameterError(caller, sources, pe.getMessage(), ctx.isJavaScriptContext());
+
+		} catch (final ArgumentCountException pe) {
+
+			logParameterError(caller, sources, pe.getMessage(), ctx.isJavaScriptContext());
+			return usage(ctx.isJavaScriptContext());
+		}
+
+		return "";
+	}
+
+	@Override
+	public String usage(boolean inJavaScriptContext) {
+		return ERROR_MESSAGE_PDF_ENCRYPT;
+	}
+
+	@Override
+	public String shortDescription() {
+		return "Encrypts a PDF file so that it can't be opened without password.";
+	}
+}
