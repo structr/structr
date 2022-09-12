35fc3d2eff3e13cd746c41280cfb29c1ff1f46d9
Axel Morgner <axel@morgner.de>
2022-09-12 16:22:35 +0200

    Reduces log level for validation errors.

--- structr-ui/src/main/java/org/structr/websocket/command/UpdateCommand.java
+++ structr-ui/src/main/java/org/structr/websocket/command/UpdateCommand.java
@@ -153,7 +153,7 @@ public class UpdateCommand extends AbstractCommand {
 
 		} catch (FrameworkException ex) {
 
-			logger.warn("Exception occurred", ex);
+			logger.debug("Exception occurred", ex);
 			getWebSocket().send(MessageBuilder.status().code(ex.getStatus()).message(ex.getMessage()).jsonErrorObject(ex.toJSON()).build(), true);
 		}
 	}
