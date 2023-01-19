fd8f7d585dcc060c4a5d820608b9701c8b4235a7
Axel Morgner <axel@morgner.de>
2023-01-19 13:36:24 +0100

    Fix: Always register view properties for classes that have interfaces.

--- structr-base/src/main/java/org/structr/module/JarConfigurationProvider.java
+++ structr-base/src/main/java/org/structr/module/JarConfigurationProvider.java
@@ -583,7 +583,11 @@ public class JarConfigurationProvider implements ConfigurationProvider {
 
 				// register view and properties only when they weren't defined in declaring superclass
 				// or were defined by an interface or a non-dynamic class
-				if (type.equals(declaringClass) || declaringClass.isInterface() || !declaringClass.getName().startsWith("org.structr.dynamic")) {
+				if (type.equals(declaringClass)
+					|| declaringClass.isInterface()
+					|| !declaringClass.getName().startsWith("org.structr.dynamic")
+					|| type.getInterfaces().length > 0
+				) {
 					registerPropertySet(type, view.name(), keys);
 				}
 
