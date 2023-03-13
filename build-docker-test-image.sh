b6ab13e7bb74a98e92915f05c673bb5a15ba93c1
Christian Morgner <christian@morgner.de>
2023-03-13 11:08:19 +0100

    Work in progress.

--- build-docker-test-image.sh
+++ build-docker-test-image.sh
@@ -1,5 +1,5 @@
 mvn clean install -DskipDeb -DskipTests -DskipDockerTestDB -DskipDocker
 cd structr-binaries
- docker build . --build-arg STRUCTR_VERSION=$(ls target/*.zip |awk -F- '{ print $2"-"$3"-"$4; }') -t structr:local
+docker build . --build-arg STRUCTR_VERSION=$(ls target/*.zip |awk -F- '{ print $2"-"$3"-"$4; }') -t ghcr.io/structr/structr:1.1
 cd ..
 notify-send -a Structr "Docker build finished." -t 3000
