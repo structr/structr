49b094d481a4d85fa1f1e5325506bf148651e9cb
Axel Morgner <axel@morgner.de>
2023-08-13 00:25:45 +0200

    Fix: Prevent actions from being replicated when cloning DOM nodes by adding "reloadingActions", "failureActions", "successNotificationActions" and "failureNotificationActions" to the blacklist for cloning DOM nodes

--- structr-base/src/main/java/org/structr/web/entity/dom/DOMNode.java
+++ structr-base/src/main/java/org/structr/web/entity/dom/DOMNode.java
@@ -306,7 +306,7 @@ public interface DOMNode extends NodeInterface, Node, Renderable, DOMAdoptable, 
 	public static final String NOT_SUPPORTED_ERR_MESSAGE_RENAME        = "Renaming of nodes is not supported by this implementation.";
 
 	public static final Set<String> cloneBlacklist = new LinkedHashSet<>(Arrays.asList(new String[] {
-		"id", "type", "ownerDocument", "pageId", "parent", "parentId", "syncedNodes", "syncedNodesIds", "children", "childrenIds", "linkable", "linkableId", "path", "relationshipId", "triggeredActions"
+		"id", "type", "ownerDocument", "pageId", "parent", "parentId", "syncedNodes", "syncedNodesIds", "children", "childrenIds", "linkable", "linkableId", "path", "relationshipId", "triggeredActions", "reloadingActions", "failureActions", "successNotificationActions", "failureNotificationActions"
 	}));
 
 	public static final String[] rawProps = new String[] {
