af98bf8772e3deb858eeaa73050ff85c2c7b7f53
vigorouscoding <kai.schwaiger@structr.com>
2023-08-28 12:34:10 +0200

    Minor: Removes unnecessary line of code

--- structr-base/src/main/resources/structr/js/files.js
+++ structr-base/src/main/resources/structr/js/files.js
@@ -1461,7 +1461,6 @@ let _Files = {
 			let saveFileAction = (callback) => {
 				_Files.updateTextFile(file, newText, callback);
 				initialText = newText;
-				toggleSaveButtons(true);
 			};
 
 			if (isTemplateCheckbox.checked) {
