ca879734be31dbeb790d9016bb2d5aa0e85055c8
Christian Morgner <christian@morgner.de>
2024-07-09 11:13:15 +0200

    Bugfix: prevent repeated thumbnail creation attempts after first failure.

--- structr-ui/src/test/java/org/structr/test/console/ConsoleTest.java
+++ structr-ui/src/test/java/org/structr/test/console/ConsoleTest.java
@@ -151,8 +151,8 @@ public class ConsoleTest extends StructrUiTest {
 
 		final Console console = new Console(securityContext, ConsoleMode.JavaScript, Collections.emptyMap());
 
-		final int nodeCount           = 1835;
-		final int relCount            = 2843;
+		final int nodeCount           = 1836;
+		final int relCount            = 2845;
 		final int resourceAccessCount = 0;
 		final int typedNodeCount      = 669;
 
