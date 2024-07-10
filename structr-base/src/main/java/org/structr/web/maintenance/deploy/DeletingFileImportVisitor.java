3e69ecd8742bd058912229d3640718bc2290d28a
vigorouscoding <kai.schwaiger@structr.com>
2024-07-10 10:10:34 +0200

    Improvement: Multiple changes to data deployment (progress messages, etc)

--- structr-base/src/main/java/org/structr/web/maintenance/deploy/DeletingFileImportVisitor.java
+++ structr-base/src/main/java/org/structr/web/maintenance/deploy/DeletingFileImportVisitor.java
@@ -18,6 +18,7 @@
  */
 package org.structr.web.maintenance.deploy;
 
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.structr.common.SecurityContext;
@@ -33,14 +34,30 @@ import java.util.Map;
 /**
  *
  */
-public class DeletingFileImportVisitor extends FileImportVisitor {
+public abstract class DeletingFileImportVisitor extends FileImportVisitor {
 
 	private static final Logger logger = LoggerFactory.getLogger(DeletingFileImportVisitor.class.getName());
 
-	public DeletingFileImportVisitor(final SecurityContext securityContext, final Path basePath, final Map<String, Object> metadata) {
+	private int count      = 0;
+	private long totalTime = 0;
+	private int batchCount = 0;
+
+	private int batchSize;
+	private long startTime;
+
+	private List<String> parents;
+
+	public DeletingFileImportVisitor(final SecurityContext securityContext, final Path basePath, final Map<String, Object> metadata, final int batchSize, final List<String> requiredParentsPaths) {
 
 		super(securityContext, basePath, metadata);
+
+		this.batchSize = batchSize;
+		this.startTime = System.currentTimeMillis();
+
+		this.parents = requiredParentsPaths;
 	}
+
+	protected abstract void sendProgressUpdateNotification(final int count, final long duration, final long meanDuration);
 
 	// ----- private methods -----
 	@Override
@@ -48,24 +65,32 @@ public class DeletingFileImportVisitor extends FileImportVisitor {
 
 		final String folderPath = harmonizeFileSeparators("/", basePath.relativize(folderObj).toString());
 
-		try (final Tx tx = app.tx()) {
+		// only delete folder if it was in the export set and NOT as a required parent (meaning that it was deliberately exported and not just because it was required because a child was exported)
+		final boolean folderWasOnlyExportedAsParent = parents.contains(folderPath);
 
-			tx.disableChangelog();
+		if (!folderWasOnlyExportedAsParent) {
 
-			final Folder folder = getExistingFolder(folderPath);
-			if (folder != null) {
+			try (final Tx tx = app.tx()) {
 
-				this.folderCache.remove(folderPath);
+				tx.disableChangelog();
 
-				app.delete(folder);
+				final Folder folder = getExistingFolder(folderPath);
+				if (folder != null) {
+
+					this.folderCache.remove(folderPath);
+
+					app.delete(folder);
+				}
+
+			} catch (Exception ex) {
+
+				logger.error("Error occurred while deleting folder before import: " + folderObj, ex);
 			}
-
-		} catch (Exception ex) {
-
-			logger.error("Error occurred while deleting folder before import: " + folderObj, ex);
 		}
 
 		super.createFolder(folderObj);
+
+		objectProcessed();
 	}
 
 	@Override
@@ -101,5 +126,41 @@ public class DeletingFileImportVisitor extends FileImportVisitor {
 		}
 
 		super.createFile(path, fileName);
+
+		objectProcessed();
+	}
+
+	private void objectProcessed() {
+
+		count++;
+
+		if (count % batchSize == 0) {
+
+			batchCount++;
+
+			final long endTime = System.currentTimeMillis();
+
+			final long duration = endTime - startTime;
+			totalTime += duration;
+
+			startTime = endTime;
+
+			sendProgressUpdateNotification(count, duration, totalTime / batchCount);
+		}
+	}
+
+	public void finished() {
+
+		if (count % batchSize != 0) {
+
+			batchCount++;
+
+			final long endTime = System.currentTimeMillis();
+
+			final long duration = endTime - startTime;
+			totalTime += duration;
+
+			sendProgressUpdateNotification(count, duration, totalTime / batchCount);
+		}
 	}
 }
