99b95d5979311555904479381e4794abcc23c2d5
Christian Morgner <christian@morgner.de>
2024-09-04 16:34:18 +0200

    Implements fire-event notification in frontend.js, adds tests, consolidates HTML code generation for frontend.js.

--- structr-base/src/main/java/org/structr/web/entity/event/ActionMapping.java
+++ structr-base/src/main/java/org/structr/web/entity/event/ActionMapping.java
@@ -41,15 +41,6 @@ public interface ActionMapping extends NodeInterface {
 		type.setImplements(URI.create("https://structr.org/v1.1/definitions/ActionMapping"));
 		type.setExtends(URI.create("#/definitions/NodeInterface"));
 
-		/*
-		type.relate(domElement,       "TRIGGERED_BY",   Cardinality.ManyToMany, "triggeredActions",   "triggerElements");
-		type.relate(domNode,          "SUCCESS_TARGET", Cardinality.ManyToMany,"reloadingActions","successTargets");
-		type.relate(domNode,          "FAILURE_TARGET", Cardinality.ManyToMany,"failureActions",  "failureTargets");
-		type.relate(parameterMapping, "PARAMETER",      Cardinality.OneToMany, "actionMapping",   "parameterMappings");
-		type.relate(domNode,          "SUCCESS_NOTIFICATION_ELEMENT", Cardinality.ManyToMany,"successNotificationActions",   "successNotificationElements");
-		type.relate(domNode,          "FAILURE_NOTIFICATION_ELEMENT", Cardinality.ManyToMany,"failureNotificationActions", "failureNotificationElements");
-		*/
-
 		domElement.relate(type,       "TRIGGERED_BY",   Cardinality.ManyToMany,   "triggerElements", "triggeredActions");
 		domNode.relate(type,          "SUCCESS_TARGET", Cardinality.ManyToMany,"successTargets","reloadingActions");
 		domNode.relate(type,          "FAILURE_TARGET", Cardinality.ManyToMany,  "failureTargets","failureActions");
