ecf085fd069ff4ef1e3ee0fc5b6c9be85c98492e
Christian Kramp <christian.kramp@structr.com>
2025-02-19 09:31:02 +0100

    Ensures scripting contexts are properly closed and invalidated.

--- structr-base/src/main/java/org/structr/core/script/Scripting.java
+++ structr-base/src/main/java/org/structr/core/script/Scripting.java
@@ -269,6 +269,8 @@ public class Scripting {
 		} finally {
 
 			context.leave();
+			context.close();
+			actionContext.putScriptingContext(engineName, null);
 		}
 
 		// Legacy print() support: Prefer explicitly printed output over actual result
