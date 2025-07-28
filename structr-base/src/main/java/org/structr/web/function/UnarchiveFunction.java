c98af1b41b167936d060457e75126e4a160f9156
Axel Morgner <axel@morgner.de>
2025-07-28 10:44:47 +0200

    Fix: Use ZipFile when unarchiving ZIP files to avoid bug when ZIP archive contains other ZIP files (like e.g. Office files which are basically ZIPed XML files)

--- structr-base/src/main/java/org/structr/web/function/UnarchiveFunction.java
+++ structr-base/src/main/java/org/structr/web/function/UnarchiveFunction.java
@@ -63,7 +63,7 @@ public class UnarchiveFunction extends UiAdvancedFunction {
 		try {
 
 			final File archiveFile = ((NodeInterface)sources[0]).as(File.class);
-			Folder parentFolder;
+			Folder parentFolder = null;
 
 			if (sources.length == 2 && sources[1] != null) {
 
@@ -76,7 +76,7 @@ public class UnarchiveFunction extends UiAdvancedFunction {
 				props.put(Traits.of(StructrTraits.FOLDER).key(AbstractFileTraitDefinition.PARENT_PROPERTY), archiveFile.getParent());
 
 				// Create folder with same name (without extension) and in same folder as file
-				parentFolder = StructrApp.getInstance(ctx.getSecurityContext()).create(StructrTraits.FOLDER, props).as(Folder.class);
+				//parentFolder = StructrApp.getInstance(ctx.getSecurityContext()).create(StructrTraits.FOLDER, props).as(Folder.class);
 			}
 
 			FileHelper.unarchive(ctx.getSecurityContext(), archiveFile, parentFolder == null ? null : parentFolder.getUuid());
