c4ae599d2de824170a8448b4eb24015541c5892c
Christian Morgner <christian@morgner.de>
2025-10-02 15:49:00 +0200

    Fixes path resolution of files vs. pages.

--- structr-base/src/main/java/org/structr/core/function/PrivilegedFindFunction.java
+++ structr-base/src/main/java/org/structr/core/function/PrivilegedFindFunction.java
@@ -23,6 +23,7 @@ import org.structr.common.error.FrameworkException;
 import org.structr.core.app.App;
 import org.structr.core.app.QueryGroup;
 import org.structr.core.app.StructrApp;
+import org.structr.core.traits.StructrTraits;
 import org.structr.core.traits.Traits;
 import org.structr.schema.action.ActionContext;
 
@@ -67,6 +68,12 @@ public class PrivilegedFindFunction extends AbstractQueryFunction {
 			if (sources.length >= 1 && sources[0] != null) {
 
 				final String typeString = sources[0].toString();
+
+				if (StructrTraits.GRAPH_OBJECT.equals(typeString)) {
+
+					throw new FrameworkException(422, "Type GraphObject not supported in find_privileged(), please use type NodeInterface to search for nodes of all types.");
+				}
+
 				type = Traits.of(typeString);
 
 				if (type != null) {
