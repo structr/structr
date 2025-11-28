f900e8685e2a29ca9c5675b8d4b8b50a2124c747
vigorouscoding <kai.schwaiger@structr.com>
2025-11-28 11:49:21 +0100

    Removes tests for keys() and values() functions

--- structr-base/src/test/java/org/structr/test/core/script/ScriptingTest.java
+++ structr-base/src/test/java/org/structr/test/core/script/ScriptingTest.java
@@ -1674,9 +1674,6 @@ public class ScriptingTest extends StructrTest {
 			assertEquals("Invalid replace() result", "equal", Scripting.replaceVariables(ctx, testOne, "${if(equal(2, 2),\n    (\"equal\"),\n    (\"not equal\")\n)}"));
 			assertEquals("Invalid replace() result", "not equal", Scripting.replaceVariables(ctx, testOne, "${if(equal(2, 3),\n    (\"equal\"),\n    (\"not equal\")\n)}"));
 
-			assertEquals("Invalid keys() / join() result", "createdBy,createdDate,hidden,id,lastModifiedDate,name,owner,type,visibleToAuthenticatedUsers,visibleToPublicUsers", Scripting.replaceVariables(ctx, testOne, "${join(keys(this, 'ui'), ',')}"));
-			assertEquals("Invalid values() / join() result", "A-nice-little-name-for-my-test-object,1,String", Scripting.replaceVariables(ctx, testOne, "${join(values(this, 'protected'), ',')}"));
-
 			// test default values
 			assertEquals("Invalid string default value", "blah", Scripting.replaceVariables(ctx, testOne, "${this.alwaysNull!blah}"));
 			assertEquals("Invalid numeric default value", "12", Scripting.replaceVariables(ctx, testOne, "${this.alwaysNull!12}"));
