816f04f2bff52e64dbc738ec3cff9308fccea396
Kai Schwaiger <vigorouscoding@users.noreply.github.com>
2025-12-04 11:12:45 +0100

    Bugfix: If backend login failed because user has no backend access, do not request /me resource

--- structr-app/src/main/resources/structr/js/websocket.js
+++ structr-app/src/main/resources/structr/js/websocket.js
@@ -180,7 +180,7 @@ let StructrWS = {
 
 		if (command === 'LOGIN' || code === 100) {
 
-			if (command === 'LOGIN' || !StructrWS.userId) {
+			if ((command === 'LOGIN' || !StructrWS.userId) && data.code < 400) {
                 let response = await fetch('/structr/rest/me');
                 let result   = await response.json();
 				StructrWS.userId = result.result.id;
