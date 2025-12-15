c2ba0b750d67ab1d3f77375f2b15e469e4a60148
Christian Morgner <christian.morgner@structr.com>
2025-12-15 14:06:39 +0100

    Adds permission propagation to the POSTER_IMAGE relationship between VideoFile and Image, fixes the relationship endpoints and adds a test.

--- structr-modules/structr-media-module/src/test/java/org/structr/test/media/MediaTest.java
+++ structr-modules/structr-media-module/src/test/java/org/structr/test/media/MediaTest.java
@@ -19,13 +19,23 @@
 package org.structr.test.media;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.structr.core.entity.Group;
+import org.structr.core.entity.SchemaGrant;
+import org.structr.core.entity.SchemaNode;
+import org.structr.core.graph.NodeInterface;
 import org.structr.core.graph.Tx;
+import org.structr.core.graph.attribute.Name;
 import org.structr.core.traits.StructrTraits;
+import org.structr.core.traits.definitions.SchemaGrantTraitDefinition;
 import org.structr.media.AVConv;
+import org.structr.media.traits.definitions.VideoFileTraitDefinition;
 import org.structr.test.web.StructrUiTest;
+import org.structr.web.auth.UiAuthenticator;
 import org.structr.web.common.FileHelper;
+import org.structr.web.entity.User;
 import org.testng.annotations.Test;
 
 import java.io.InputStream;
@@ -66,7 +76,6 @@ public class MediaTest extends StructrUiTest {
 			t.printStackTrace();
 		}
 
-
 		// use RestAssured to check file
 		RestAssured
 			.given()
@@ -88,5 +97,82 @@ public class MediaTest extends StructrUiTest {
 			.when()
 			.get("/VideoFile");
 
+	}
+
+	@Test
+	public void testVideoFilePosterImage() {
+
+		String videoId = null, imageId = null;
+
+		/**
+		 * In this test, we create a video file with a poster image and a test user
+		 * that is only allowed to see the video file, but not the image, and expect
+		 * the image to be visible by propagated permissions via the POSTER_IMAGE
+		 * relationship.
+		 *
+		 * Note: this does not currently work for visibilty flags or anonymous users!
+		 */
+		try (final Tx tx = app.tx()) {
+
+			createAdminUser();
+
+			final SchemaNode videoFile = app.create(StructrTraits.SCHEMA_NODE, new Name("VideoFile")).as(SchemaNode.class);
+			final User tester = app.create(StructrTraits.USER, new Name("tester")).as(User.class);
+			tester.setPassword("test");
+
+			final Group group = app.create(StructrTraits.GROUP, new Name("group")).as(Group.class);
+			group.addMember(securityContext, tester);
+
+			// setup schema grant between group and VideoFile schema node
+			final SchemaGrant schemaGrant = app.create(StructrTraits.SCHEMA_GRANT).as(SchemaGrant.class);
+			schemaGrant.setProperty(schemaGrant.getTraits().key(SchemaGrantTraitDefinition.PRINCIPAL_PROPERTY), group);
+			schemaGrant.setProperty(schemaGrant.getTraits().key(SchemaGrantTraitDefinition.SCHEMA_NODE_PROPERTY), videoFile);
+			schemaGrant.setProperty(schemaGrant.getTraits().key(SchemaGrantTraitDefinition.ALLOW_READ_PROPERTY), true);
+
+			grant("VideoFile", UiAuthenticator.NON_AUTH_USER_GET | UiAuthenticator.AUTH_USER_GET, false);
+			grant("Image",     UiAuthenticator.NON_AUTH_USER_GET | UiAuthenticator.AUTH_USER_GET, false);
+
+			final NodeInterface video = app.create(StructrTraits.VIDEO_FILE, new Name("Testvideo"));
+			final NodeInterface image = app.create(StructrTraits.IMAGE, new Name("Testimage"));
+
+			videoId = video.getUuid();
+			imageId = image.getUuid();
+
+			video.setProperty(video.getTraits().key(VideoFileTraitDefinition.POSTER_IMAGE_PROPERTY), image);
+
+			video.setVisibility(true, true);
+
+			tx.success();
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+
+		// use RestAssured to check file
+		RestAssured
+			.given()
+			.header(X_USER_HEADER, "tester")
+			.header(X_PASSWORD_HEADER, "test")
+			.filter(ResponseLoggingFilter.logResponseTo(System.out))
+			.expect()
+			.statusCode(200)
+			.body("result[0].type",           equalTo(StructrTraits.VIDEO_FILE))
+			.body("result[0].id",             equalTo(videoId))
+			.body("result[0].posterImage.id", equalTo(imageId))
+			.when()
+			.get("/VideoFile");
+
+		// use RestAssured to check file
+		RestAssured
+			.given()
+			.header(X_USER_HEADER, "tester")
+			.header(X_PASSWORD_HEADER, "test")
+			.filter(ResponseLoggingFilter.logResponseTo(System.out))
+			.expect()
+			.statusCode(200)
+			.body("result[0].type", equalTo(StructrTraits.IMAGE))
+			.body("result[0].id",   equalTo(imageId))
+			.when()
+			.get("/Image");
 	}
 }
