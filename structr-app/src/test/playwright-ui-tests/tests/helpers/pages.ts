b32483f8491bb2d87825af748903c1961285ddd2
vigorouscoding <kai.schwaiger@structr.com>
2026-02-11 18:56:43 +0100

    Test: Updates UI tests to play nicer with MacOS and to focus monaco editor better

--- structr-app/src/test/playwright-ui-tests/tests/helpers/pages.ts
+++ structr-app/src/test/playwright-ui-tests/tests/helpers/pages.ts
@@ -25,61 +25,61 @@ import { Page, Locator } from '@playwright/test';
  */
 export class Container {
 
-    locator: Locator;
+	locator: Locator;
 
-    constructor(locator: Locator) {
-        this.locator = locator;
-    }
+	constructor(locator: Locator) {
+		this.locator = locator;
+	}
 
-    /**
-     * Returns the text node of this container (the one with the tag
-     * or text in it), so you can click on it.
-     */
-    getTextNode(): Locator {
-        return this.locator.locator('> div.node-container > span');
-    }
+	/**
+	 * Returns the text node of this container (the one with the tag
+	 * or text in it), so you can click on it.
+	 */
+	getTextNode(): Locator {
+		return this.locator.locator('> div.node-container > span');
+	}
 
-    /**
-     * Returns the tree node of this container (the one that contains
-     * the children), so you can use it to descend the hierarchy.
-     * @param name
-     * @param index
-     */
-    getElement(name: string, index: number = 0): Container {
-        return new Container(this.locator.getByText(name).nth(index).locator('../../..'));
-    }
+	/**
+	 * Returns the tree node of this container (the one that contains
+	 * the children), so you can use it to descend the hierarchy.
+	 * @param name
+	 * @param index
+	 */
+	getElement(name: string, index: number = 0): Container {
+		return new Container(this.locator.getByText(name).nth(index).locator('../../..'));
+	}
 
-    getElements(name: string): Locator {
-        return this.locator.locator('> div.node > div.node-container > span').filter({ hasText: name });
-    }
+	getElements(name: string): Locator {
+		return this.locator.locator('> div.node > div.node-container > span').filter({ hasText: name });
+	}
 
-    async countElements(name: string): Promise<number> {
-        return await this.getElements(name).count();
-    }
+	async countElements(name: string): Promise<number> {
+		return await this.getElements(name).count();
+	}
 
-    /**
-     * Returns the parent container of this node.
-     */
-    getParentContainer() {
-        return new Container(this.locator.locator('..'));
-    }
+	/**
+	 * Returns the parent container of this node.
+	 */
+	getParentContainer() {
+		return new Container(this.locator.locator('..'));
+	}
 }
 
 export async function createAndRenamePage(page: Page, whichTemplate, name, wait = 100) {
 
-    await page.locator('#pages-actions .dropdown-select').click();
-    await page.locator('#create_page').click();
-    await page.waitForTimeout(wait);
-    await page.locator(`#template-tiles .app-tile:nth-child(${whichTemplate})`).click();
-    await page.waitForTimeout(wait);
+	await page.locator('#pages-actions .dropdown-select').click();
+	await page.locator('#create_page').click();
+	await page.waitForTimeout(wait);
+	await page.locator(`#template-tiles .app-tile:nth-child(${whichTemplate})`).click();
+	await page.waitForTimeout(wait);
 
-    await page.getByText('New Page').click();
-    await page.waitForTimeout(wait);
-    await page.getByRole('link', {name: 'General'}).click();
-    await page.waitForTimeout(wait);
-    await page.locator('#name-input').fill(name);
-    await page.locator('#pagesTree').click();
-    await page.waitForTimeout(wait);
+	await page.getByText('New Page').click();
+	await page.waitForTimeout(wait);
+	await page.getByRole('link', {name: 'General'}).click();
+	await page.waitForTimeout(wait);
+	await page.locator('#name-input').fill(name);
+	await page.locator('#pagesTree').click();
+	await page.waitForTimeout(wait);
 }
 
 /**
@@ -89,212 +89,226 @@ export async function createAndRenamePage(page: Page, whichTemplate, name, wait 
  * @param wait
  */
 export function getPageContainer(page: Page, pageName: string, wait = 100): Container {
-    return new Container(page.locator(`#pagesTree > div:has(div:nth-child(1) > span > b[title="${pageName}"])`));
+	return new Container(page.locator(`#pagesTree > div:has(div:nth-child(1) > span > b[title="${pageName}"])`));
 }
 
 export async function expandPageTree(page: Page, pageName, wait = 100) {
 
-    let pageNode = getPageContainer(page, pageName, wait);
+	let pageNode = getPageContainer(page, pageName, wait);
 
-    await expandOrCollapseElement(page, pageNode, 'expand');
+	await expandOrCollapseElement(page, pageNode, 'expand');
 }
 
 export async function collapsePageTree(page: Page, name, wait = 100) {
 
-    let pageNode = getPageContainer(page, name, wait);
+	let pageNode = getPageContainer(page, name, wait);
 
-    await expandOrCollapseElement(page, pageNode, 'collapse');
+	await expandOrCollapseElement(page, pageNode, 'collapse');
 }
 
 export async function expandOrCollapseElement(page: Page, node: Container, action: string, wait = 100) {
 
-    switch (action) {
+	switch (action) {
 
-        case 'expand':
-            await useContextMenu(page, node, 'Expand / Collapse', 'Expand subtree recursively');
-            break;
-        case 'collapse':
-            await useContextMenu(page, node, 'Expand / Collapse', 'Collapse subtree');
-            break;
-    }
+		case 'expand':
+			await useContextMenu(page, node, 'Expand / Collapse', 'Expand subtree recursively');
+			break;
+		case 'collapse':
+			await useContextMenu(page, node, 'Expand / Collapse', 'Collapse subtree');
+			break;
+	}
 }
 
 export async function insertFrontendJs(page: Page, pageName, wait = 100) {
 
-    let pageNode = getPageContainer(page, pageName, wait);
+	let pageNode = getPageContainer(page, pageName, wait);
 
-    let head = pageNode.getElement('head');
+	let head = pageNode.getElement('head');
 
-    await useContextMenu(page, head, 'Suggested HTML element', 'script');
+	await useContextMenu(page, head, 'Suggested HTML element', 'script');
 
-    let script = head.getElement('script');
+	let script = head.getElement('script');
 
-    await page.waitForTimeout(wait);
-    await script.getTextNode().click();
+	await page.waitForTimeout(wait);
+	await script.getTextNode().click();
 
-    await page.waitForTimeout(wait);
-    await page.getByRole('link', {name: 'HTML'}).click();
-    await page.waitForTimeout(wait);
-    await page.locator('input[name="_html_type"]').fill('module');
-    await page.locator('input[name="_html_src"]').fill('/structr/js/frontend/frontend.js');
+	await page.waitForTimeout(wait);
+	await page.getByRole('link', {name: 'HTML'}).click();
+	await page.waitForTimeout(wait);
+	await page.locator('input[name="_html_type"]').fill('module');
+	await page.locator('input[name="_html_src"]').fill('/structr/js/frontend/frontend.js');
 }
 
 export async function resizePagesTree(page: Page, offset: number, wait = 100) {
 
-    let resizer = page.locator('.column-resizer-left');
+	let resizer = page.locator('.column-resizer-left');
 
-    await resizer.isVisible();
+	await resizer.isVisible();
 
-    let box = await resizer.boundingBox();
+	let box = await resizer.boundingBox();
 
-    if (box && box.x) {
+	if (box && box.x) {
 
-        // resize pages tree flyout
-        await resizer.isVisible();
-        await resizer.hover();
-        await page.waitForTimeout(wait);
-        await page.mouse.down();
-        await page.waitForTimeout(wait);
-        await page.mouse.move(box.x + offset, 0, {steps: 20});
-        await page.waitForTimeout(100);
-        await page.waitForTimeout(wait);
-        await page.mouse.up();
-        await page.waitForTimeout(wait);
+		// resize pages tree flyout
+		await resizer.isVisible();
+		await resizer.hover();
+		await page.waitForTimeout(wait);
+		await page.mouse.down();
+		await page.waitForTimeout(wait);
+		await page.mouse.move(box.x + offset, 0, {steps: 20});
+		await page.waitForTimeout(100);
+		await page.waitForTimeout(wait);
+		await page.mouse.up();
+		await page.waitForTimeout(wait);
 
-    } else {
+	} else {
 
-        console.log('box was null!');
-    }
+		console.log('box was null!');
+	}
 
 }
 
 export async function insertInputWithLabel(page: Page, container: Container, name: string, inputOrSelect: string, wait = 100) {
 
-    await useContextMenu(page, container,'Insert HTML element', 'l-m', 'label');
-    await useContextMenu(page, container.getParentContainer(), 'Expand / Collapse', 'Expand subtree recursively');
+	await useContextMenu(page, container,'Insert HTML element', 'l-m', 'label');
+	await useContextMenu(page, container.getParentContainer(), 'Expand / Collapse', 'Expand subtree recursively');
 
-    let count = await container.countElements('label');
-    let labelContainer = container.getElement('label', count - 1);
-    let textContainer = labelContainer.getElement('Initial text for label');
+	let count = await container.countElements('label');
+	let labelContainer = container.getElement('label', count - 1);
+	let textContainer = labelContainer.getElement('Initial text for label');
 
-    await useContextMenu(page, textContainer, 'Wrap element in...', '... HTML element', 's', 'span');
-    await page.waitForTimeout(1000);
-    await setNodeContent(page, textContainer, name);
+	await useContextMenu(page, textContainer, 'Wrap element in...', '... HTML element', 's', 'span');
+	await page.waitForTimeout(1000);
+	await setNodeContent(page, textContainer, name);
 
-    switch (inputOrSelect) {
+	switch (inputOrSelect) {
 
-        case 'input':
-            await useContextMenu(page, labelContainer, 'Insert HTML element', 'i-k', 'input');
-            break;
-        case 'select':
-            await useContextMenu(page, labelContainer, 'Insert HTML element', 's', 'select');
-            break;
-    }
+		case 'input':
+			await useContextMenu(page, labelContainer, 'Insert HTML element', 'i-k', 'input');
+			break;
+		case 'select':
+			await useContextMenu(page, labelContainer, 'Insert HTML element', 's', 'select');
+			break;
+	}
 
-    await labelContainer.getTextNode().click();
+	await labelContainer.getTextNode().click();
 
-    // make label style display: block;
-    await page.getByRole('link', {name: 'HTML'}).click();
-    await page.locator('input[name="_html_style"]').fill('display: block;');
+	// make label style display: block;
+	await page.getByRole('link', {name: 'HTML'}).click();
+	await page.locator('input[name="_html_style"]').fill('display: block;');
 
-    // click on input element to make it configurable
-    await labelContainer.getElement(inputOrSelect).getTextNode().click();
+	// click on input element to make it configurable
+	await labelContainer.getElement(inputOrSelect).getTextNode().click();
 }
 
 export async function configureHTMLAttributes(page: Page, node: Container, data: Object, wait = 100) {
 
-    await node.getTextNode().click();
-    await page.getByRole('link', {name: 'HTML'}).click();
+	await node.getTextNode().click();
+	await page.getByRole('link', {name: 'HTML'}).click();
 
-    for (let key in data) {
-        await page.locator(`input[name="_html_${key}"]`).fill(data[key]);
-    }
+	for (let key in data) {
+		await page.locator(`input[name="_html_${key}"]`).fill(data[key]);
+	}
 
-    await page.waitForTimeout(wait);
+	await page.waitForTimeout(wait);
 
 }
 
 export async function configureGeneralAttributes(page: Page, node: Container, data: Object, wait = 100) {
 
-    await node.getTextNode().click();
-    await page.getByRole('link', {name: 'General'}).click();
+	await node.getTextNode().click();
+	await page.getByRole('link', {name: 'General'}).click();
 
-    for (let key in data) {
-        await page.locator(`input[name="${key}"]`).fill(data[key]);
-    }
+	for (let key in data) {
+		await page.locator(`input[name="${key}"]`).fill(data[key]);
+	}
 
-    await page.waitForTimeout(wait);
+	await page.waitForTimeout(wait);
 
 }
 
 export async function configureFunctionQuery(page: Page, node: Container, query: string, dataKey: string, wait = 100) {
 
-    await node.getTextNode().click();
+	await node.getTextNode().click();
 
-    await page.waitForTimeout(wait);
-    await page.getByRole('link', {name: 'Repeater'}).click();
-    await page.waitForTimeout(wait);
-    await page.getByText('Function Query', { exact: true }).click();
-    await page.waitForTimeout(wait);
-    await page.locator('.monaco-editor').nth(0).click();
-    await page.keyboard.type(query);
-    await page.waitForTimeout(wait);
-    await page.locator('.save-repeater-query').click();
-    await page.locator('.repeater-datakey').fill(dataKey);
-    await page.waitForTimeout(wait);
-    await page.locator('.save-repeater-datakey').click();
+	await page.waitForTimeout(wait);
+	await page.getByRole('link', {name: 'Repeater'}).click();
+	await page.waitForTimeout(wait);
+	await page.getByText('Function Query', { exact: true }).click();
+	await page.waitForTimeout(wait);
+	await page.locator('.monaco-editor').nth(0).click();
+	await page.keyboard.type(query);
+	await page.waitForTimeout(wait);
+	await page.locator('.save-repeater-query').click();
+	await page.locator('.repeater-datakey').fill(dataKey);
+	await page.waitForTimeout(wait);
+	await page.locator('.save-repeater-datakey').click();
 }
 
 export async function setNodeContent(page: Page, node: Container, content: string, wait = 100) {
 
-    await node.getTextNode().click();
-    await page.waitForTimeout(1000);
-    await page.keyboard.down('Control');
-    await page.keyboard.press('A');
-    await page.keyboard.up('Control');
-    await page.keyboard.type(content);
-    await page.waitForTimeout(200);
-    await page.getByRole('button', {name: 'Save'}).click();
-    await page.waitForTimeout(wait);
+	await node.getTextNode().click();
+	await page.waitForTimeout(1000);
+	await sendCtrlPlusA(page);
+	await page.keyboard.type(content);
+	await page.waitForTimeout(200);
+	await page.getByRole('button', {name: 'Save'}).click();
+	await page.waitForTimeout(wait);
 }
 
 export async function useContextMenu(page: Page, container: Container, level1: string, level2?: string, level3?: string, level4?: string) {
 
-    await container.getTextNode().click({button: 'right'});
+	await container.getTextNode().click({button: 'right'});
 
-    let menu = page.locator('#context-menu-dialog');
-    let sub = menu.locator('> ul > li').filter({ has: page.getByText(level1, { exact: true }) });
+	let menu = page.locator('#context-menu-dialog');
+	let sub = menu.locator('> ul > li').filter({ has: page.getByText(level1, { exact: true }) });
 
-    if (level2) {
+	if (level2) {
 
-        await sub.hover();
-        let subsub = sub.locator('> ul > li').filter({ has: page.getByText(level2, { exact: true }) });
+		await sub.hover();
+		let subsub = sub.locator('> ul > li').filter({ has: page.getByText(level2, { exact: true }) });
 
-        if (level3) {
+		if (level3) {
 
-            await subsub.hover();
-            let subsubsub = subsub.locator('> ul > li').filter({ has: page.getByText(level3, { exact: true }) });
+			await subsub.hover();
+			let subsubsub = subsub.locator('> ul > li').filter({ has: page.getByText(level3, { exact: true }) });
 
-            if (level4) {
+			if (level4) {
 
-                await subsubsub.hover();
-                let subsubsubsub = subsubsub.locator('> ul > li').filter({ has: page.getByText(level4, { exact: true }) });
-                await subsubsubsub.click();
+				await subsubsub.hover();
+				let subsubsubsub = subsubsub.locator('> ul > li').filter({ has: page.getByText(level4, { exact: true }) });
+				await subsubsubsub.click();
 
-            } else {
+			} else {
 
-                await subsubsub.click();
-            }
+				await subsubsub.click();
+			}
 
-        } else {
+		} else {
 
-            // direct click on second level
-            await subsub.click();
-        }
+			// direct click on second level
+			await subsub.click();
+		}
 
-    } else {
+	} else {
 
-        // direct click on first level
-        await sub.click();
-    }
+		// direct click on first level
+		await sub.click();
+	}
+}
+
+export async function focusCenterPaneMonacoEditor(page: Page) {
+
+	const monaco = page.locator('#center-pane .monaco-editor');
+	await monaco.click();
+	await monaco.focus();
+}
+
+export async function sendCtrlPlusA(page: Page) {
+
+	const controlKey = (process.platform === 'darwin') ? 'Meta' : 'Control';
+
+	await page.keyboard.down(controlKey);
+	await page.keyboard.press('A');
+	await page.keyboard.up(controlKey);
 }
