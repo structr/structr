8eed4258118453b7f1d6b884f0e17022a2a5b44a
vigorouscoding <kai.schwaiger@structr.com>
2026-03-07 11:38:56 +0100

    Documentation: Adds more documentation to predicate.any

--- structr-base/src/main/java/org/structr/core/function/search/FindAnyFunction.java
+++ structr-base/src/main/java/org/structr/core/function/search/FindAnyFunction.java
@@ -81,7 +81,69 @@ public class FindAnyFunction extends AdvancedScriptingFunction {
 	@Override
 	public String getLongDescription() {
 		return """
-			The function takes a single collection as a parameter. The query returns all nodes that match any of the given values.
+			The function takes a collection as a parameter. The query returns all nodes that match any of the given values.
+
+			The main use case for predicate.any is remote properties but it can also be used for local properties (not array properties at the moment).
+
+			predicate.any is always used in conjunction with `predicate.equals` or `predicate.contains` and the elements of `listOfOptions` must be of the same type as the property that is being searched.
+
+			Examples for different property types:
+			```
+			// String property
+			$.predicate.equals('name', $.predicate.any( [ 'value 1', 'value 2', 'value 3', ... ] ))
+			$.predicate.contains('name', $.predicate.any( [ 'Jack', 'Jane', 'John', ... ] ))
+
+			// Integer Property
+			$.predicate.equals('age', $.predicate.any( [ 1, 50, 30, ... ] ))
+
+			// Date Property
+			// Since dates are stored as long values internally, we use .getTime()
+			$.predicate.equals('createdDate', $.predicate.any( [ date1.getTime(), date2.getTime() ] ))
+
+			// Remote property (cardinality 1)
+			$.predicate.equals('bornIn', $.predicate.any( [ country1, country2, country3, ... ] ))
+
+			// Remote property (cardinality n) - exact search
+			$.predicate.equals('hasSkills', $.predicate.any( [ [skill1, skill2, skill3], [skill2, skill3, skill4], ... ] ))
+
+			// Remote property (cardinality n) - contains search
+			$.predicate.contains('hasSkills', $.predicate.any( [ [skill1, skill2, skill3],  [skill4],  [skill1, skill4], ... ] ))
+			```
+
+			**Remote Attributes / Linked nodes**
+
+			For remote attributes with **cardinality 1**, only predicate.equals can be used:
+
+			```
+			// find all users born in Germany, France or the UK
+			let germany = ...;
+			let france  = ...;
+			let uk      = ...;
+			$.find('User', $.predicate.equals('bornIn', $.predicate.any( [ germany, france, uk ] )));
+			```
+
+			For remote attributes with **cardinality n**, both predicate.equals and predicate.contains can be used.
+
+			For situations where an exact match is required, we use `predicate.equals`. In this example `User.jobSkills` is a list, meaning that the argument passed to predicate.any must be a list of lists:
+
+			```
+			// find all waiters and cooks (users with either of those exact skillsets)
+			let waiterSkills     = [ take_orders, serve_food, serve_drinks ];
+			let pizzaMakerSkills = [ knead_dough, put_toppings, bake_pizza ];
+
+			$.find('User', $.predicate.equals('jobSkills', $.predicate.any( [ waiterSkills, pizzaMakerSkills ] )));
+			```
+
+			The previous example finds users with the **exact** skillset required for a job. This would find perfect matches but it would leave out anyone who has more than the exact skills. For such situations `predicate.contains` can be used. `User.jobSkills` is still a list, meaning that the argument passed to predicate.any must still be a list of lists.
+
+			```
+			// find all users that have the required skills and maybe even more
+			let waiterSkills     = [ take_orders, serve_food, serve_drinks ];
+			let pizzaMakerSkills = [ knead_dough, put_toppings, bake_pizza ];
+
+			$.find('User', $.predicate.contains('jobSkills', $.predicate.any( [ waiterSkills, pizzaMakerSkills ] )));
+			```
+
 			""";
 	}
 
