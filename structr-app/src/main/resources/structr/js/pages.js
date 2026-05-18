2b85f2529d23880217fe42db3137a25567df68d5
vigorouscoding <kai.schwaiger@structr.com>
2026-05-18 16:53:30 +0200

    Bugfix: Fixes bug that prevented showing page template widgets

--- structr-app/src/main/resources/structr/js/pages.js
+++ structr-app/src/main/resources/structr/js/pages.js
@@ -2224,7 +2224,6 @@ let _Pages = {
 			});
 
 			// Display 'Create Page' dialog
-			let smallTransparentGIF = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
 			let createPageButton = document.querySelector('#create_page');
 			createPageButton.addEventListener('click', (e) => {
 				e.stopPropagation();
@@ -2337,6 +2336,7 @@ let _Pages = {
 		for (let widget of pageTemplates) {
 
 			let id = 'create-from-' + widget.id;
+			let smallTransparentGIF = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
 			let tile = _Helpers.createSingleDOMElementFromHTML(`<div id="${id}" class="page-tile"><div class="page-thumbnail-frame"><img src="${widget.newThumbnailPath ?? widget.thumbnailPath ?? smallTransparentGIF}"/><h4>${widget.name}</h4><p>${(widget.description || '')}</p></div></div>`);
 			container.append(tile);
 
