26356a7c7bc1cc9d0a9dbec75a6d9a46d8690ad8
Christian Kramp <ckramp699@gmail.com>
2026-06-30 13:14:37 +0200

    Adds missing license header to the partitioner.

--- structr-app/src/main/resources/build/ModulePathPartitioner.java
+++ structr-app/src/main/resources/build/ModulePathPartitioner.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2010-2026 Structr GmbH
+ *
+ * This file is part of Structr <http://structr.org>.
+ *
+ * Structr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Structr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Structr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Single-file build tool (JEP 330):  java ModulePathPartitioner.java <libDir> <classpathDir> <seedFile>
 //
 // Splits a flat directory of runtime dependency jars (libDir, produced by copy-dependencies) into a
