e2498a77b05c8e96ce17cf92bf4666d1e80801b4
vigorouscoding <kai.schwaiger@structr.com>
2026-07-10 17:50:15 +0200

    Tests: Improves robustness of code test

--- structr-app/src/test/playwright-ui-tests/tests/test-code.spec.js
+++ structr-app/src/test/playwright-ui-tests/tests/test-code.spec.js
@@ -18,8 +18,9 @@
  */
 // @ts-check
 import {expect, test} from '@playwright/test';
-import {goToModule, initialize} from "./helpers/init";
+import {goToModule, initialize, waitForDialogBoxToClose} from "./helpers/init";
 import {login, logout} from "./helpers/auth";
+import {focusMonacoEditorInContainer} from "./helpers/pages";
 
 test.beforeAll(async ({playwright}) => {
 	await initialize(playwright, {
@@ -45,39 +46,40 @@ test('user-defined-functions', async ({page}, testInfo) => {
 	await page.getByText('User-defined functions').first().click();
 	await page.locator('#methods-grid-container .dropdown-menu').first().waitFor({state: 'visible'});
 	await page.locator('#methods-grid-container .dropdown-menu').first().click();
-	await page.waitForTimeout(1000);
 	await page.getByText('Add method').first().click();
-	await page.waitForTimeout(500);
 	await page.locator('#methods-grid-container .property-name').first().click();
 	await page.keyboard.type('testMethod');
-	await page.locator('.monaco-editor').nth(0).click();
+	await focusMonacoEditorInContainer(page, '#code-methods-container');
 	await page.keyboard.type('( log(\'This is testMethod.\'), (\'testMethod return value\') )');
-	await page.waitForTimeout(1000);
 	await page.getByText('Save all').first().click();
+
 	await page.waitForTimeout(1000);
 	await page.screenshot({path: 'screenshots/code_create-user-defined-function.png'});
 
 	// take a screenshot of the OpenAPI root element
 	await page.getByRole('link', {name: 'OpenAPI - Swagger UI'}).click();
+
 	await page.waitForTimeout(2000);
 	await page.screenshot({path: 'screenshots/code_openapi-root.png'});
 
 	// Run function
 	await page.locator('[data-id="globals"] .jstree-ocl').first().click();
-	await page.waitForTimeout(500);
 	await page.getByText('testMethod()', {exact: false}).first().click();
+
+	await page.waitForTimeout(1000);
 	await page.screenshot({path: 'screenshots/code_run-function.png'});
-	await page.waitForTimeout(500);
+
 	await page.getByText('Open run dialog').first().click();
-	await page.waitForTimeout(500);
 	//await page.locator('#run-method').click();
 	await page.getByRole('button', {name: 'Run', exact: true}).click();
 	//await page.getByText('Run', { exact: false }).first().click();
-	await page.waitForTimeout(1000);
 	await expect(page.getByText('"result": "testMethod return value"')).toBeVisible();
 	await page.screenshot({path: 'screenshots/code_run-function-result.png'});
+
 	await page.waitForTimeout(1000);
 	await page.getByRole('button', {name: 'Close', exact: true}).click();
+
+	await waitForDialogBoxToClose(page);
 
 	// Add Milestone and Project to OpenAPI
 	await page.locator('[data-id="root"] .jstree-ocl').first().click();
@@ -85,41 +87,41 @@ test('user-defined-functions', async ({page}, testInfo) => {
 	await page.locator('[data-id="custom"] .jstree-ocl').first().click();
 	await page.getByRole('link', {name: 'Project', exact: true}).waitFor({state: 'visible'});
 	await page.getByRole('link', {name: 'Project', exact: true}).click();
-	await page.waitForTimeout(500);
 	await page.getByText('General').first().click();
-	await page.waitForTimeout(500);
 	await page.getByRole('checkbox', {name: 'Include in OpenAPI output', exact: false}).click();
 	await page.locator('#openapi-options > div > div').filter({hasText: 'Tags'}).getByRole('combobox').click();
 	await page.keyboard.type('default');
 	await page.keyboard.press('Enter');
-	await page.waitForTimeout(500);
 	await page.getByRole('button', {name: 'Save', exact: false}).click();
+	await expect(page.getByRole('button', {name: 'Save', exact: false})).toHaveAttribute('disabled');
+
 	await page.waitForTimeout(1000);
 	await page.screenshot({path: 'screenshots/code_project-to-openapi.png'});
 
 	// Create example method for screenshot
 	await page.locator('li li li .jstree-ocl').first().click();
-	await page.waitForTimeout(100);
 	await page.getByRole('link', {name: 'Methods', exact: true}).click();
 	await page.locator('div.schema-grid button.dropdown-select').click();
-	await page.waitForTimeout(100);
 	await page.getByText('Add method').click();
 	await page.locator('#methods-grid-container .property-name').first().click();
 	await page.keyboard.type('sendEMail');
-	await page.locator('.monaco-editor').nth(0).click();
-	await page.keyboard.type('{');
-	await page.waitForTimeout(100);
-	await page.keyboard.press('Enter');
-	await page.keyboard.type('$.sendPlain');
+	await focusMonacoEditorInContainer(page, '#code-methods-container');
+
+	await page.keyboard.type('{', { delay: 50 });
+	await page.keyboard.press('Enter', { delay: 50 });
+	await page.keyboard.type('$.sendPlain', { delay: 50 });
+
 	await page.waitForTimeout(1000);
 	await page.screenshot({path: 'screenshots/code_project-method-autocomplete.png'});
+
 	await page.keyboard.press('Enter');
 	await page.keyboard.type('();');
 	await page.getByRole('button', {name: 'Save all'}).click();
-	await page.waitForTimeout(1000);
+	await expect(page.getByRole('button', {name: 'Save all'})).toHaveAttribute('disabled');
+
 	await page.getByRole('link', {name: 'Methods', exact: true}).dblclick();
-	await page.waitForTimeout(1000);
 	await page.getByRole('link', {name: 'Custom method sendEMail()'}).click();
+
 	await page.waitForTimeout(200);
 	await page.screenshot({path: 'screenshots/code_project-method.png'});
 
